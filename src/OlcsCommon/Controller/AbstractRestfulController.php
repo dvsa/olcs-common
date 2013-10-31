70a9f37fcd11cd8861701121c5bca1c6b50506f0
Pelle Wessman <pelle.wessman@valtech.se>
2013-10-31 17:08:03 +0100

    -n Backported REST error codes from feature/olcs-442

--- src/OlcsCommon/Controller/AbstractRestfulController.php
+++ src/OlcsCommon/Controller/AbstractRestfulController.php
@@ -11,6 +11,12 @@ namespace OlcsCommon\Controller;
 
 class AbstractRestfulController extends \Zend\Mvc\Controller\AbstractRestfulController
 {
+    const ERROR_METHOD_NOT_ALLOWED = 101;
+    const ERROR_FORBIDDEN = 102;
+    const ERROR_MISSING_PARAMETERS = 103;
+    const ERROR_INVALID_PARAMETER = 104;
+    const ERROR_UNKNOWN = 105;
+
     protected function pickValidKeys($values, $keys)
     {
         return array_intersect_key($values, array_flip($keys));
