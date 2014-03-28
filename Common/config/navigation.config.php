afa7e375cece215bbb201abdd1c88d5c9f7e3a90
adminmwc <adminmwc@192.168.56.1>
2014-03-28 14:19:17 +0000

    added translator stuff

---
+++ Common/config/navigation.config.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+    'label' => 'Home',
+    'route' => 'home',
+    'pages' => array(
+        array(
+            'label' => 'Search',
+            'route' => 'lookup',
+            'action' => 'index',
+            'pages' => array()
+        )     
+    )
+)
+?>
