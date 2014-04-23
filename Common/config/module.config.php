ca2d5e635ec744667ff1f6ef223f2233bea5bafd
Rob Caiger <rob@clocal.co.uk>
2014-04-23 13:11:17 +0100

    Module config

--- Common/config/module.config.php
+++ Common/config/module.config.php
@@ -38,6 +38,7 @@ return array(
             ),
     'forms_path' => __DIR__ .'/../../Common/src/Common/Form/Forms/',
     'tables' => array(
+        'shared' => __DIR__ . '/../src/Common/Table/Tables/',
         'config' => __DIR__ . '/../src/Common/Table/Tables/',
         'partials' => __DIR__ . '/../view/table/'
     ),
