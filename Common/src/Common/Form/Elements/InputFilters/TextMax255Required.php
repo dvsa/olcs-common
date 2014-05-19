916311f61a9531afe31ae46f65ba0efd652c758e
Ian.Lindsay <Ian.Lindsay@valtech.co.uk>
2014-05-19 13:48:11 +0100

    New input filter for a required 255 char text field

---
+++ Common/src/Common/Form/Elements/InputFilters/TextMax255Required.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Text Max 255
+ */
+namespace Common\Form\Elements\InputFilters;
+
+use Zend\InputFilter\InputProviderInterface as InputProviderInterface;
+
+/**
+ * Text Max 255
+ */
+class TextMax255Required extends TextMax255 implements InputProviderInterface
+{
+    protected $required = true;
+}
