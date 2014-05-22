01a02f7968b0e5a57fe4511704f6b0785489115b
Rob Caiger <rob@clocal.co.uk>
2014-05-22 15:38:48 +0100

    Form rendering changes

---
+++ Common/src/Common/Form/Elements/singlecheckbox.element.php
@@ -0,0 +1,17 @@
+<?php
+return ['checkbox' =>
+        [
+            'type' => '\Common\Form\Elements\InputFilters\Checkbox',
+            'name' => '',
+            'options' => [
+                'label' => '',
+                'value_options' => [],
+                'help-block' => 'Please choose',
+                'must_be_checked' => true,
+            ],
+            'attributes' => [
+                'id' => '',
+                'placeholder' => '',
+            ]
+        ]
+    ];
