9ee32e0a6e22ceede7267afb6e02dd35310d845c
Rob Caiger <rob@clocal.co.uk>
2014-05-30 10:28:15 +0100

    Started work

---
+++ Common/src/Common/Service/File/FileUploadInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * File Upload Interface
+ *
+ * @author Rob Caiger <rob@clocal.co.uk>
+ */
+namespace Common\Service\File;
+
+/**
+ * File Upload Interface
+ *
+ * @author Rob Caiger <rob@clocal.co.uk>
+ */
+interface FileUploadInterface
+{
+
+}
