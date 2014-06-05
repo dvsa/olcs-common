b90d3e93c53197162bf9c88a31d2e6a4faa89c8f
Nick Payne <nick@kurai.co.uk>
2014-06-05 13:04:29 +0100

    add unit tests for script service

---
+++ test/Common/src/Common/Service/Script/ScriptFactoryTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace CommonTest\Service\Script;
+
+use Common\Service\Script\ScriptFactory;
+
+class ScriptFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    protected $config = [];
+
+    /**
+     * test before hook
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        $this->config = [
+            'local_scripts_path' => __DIR__ . '/TestResources/'
+        ];
+        $this->service = new ScriptFactory($this->config);
+    }
+
+    public function testLoadFileWithNonExistentPath()
+    {
+        try {
+            $this->service->loadFile('/foo/bar');
+        } catch (\Exception $e) {
+            $this->assertEquals('Attempted to load invalid script file "/foo/bar"', $e->getMessage());
+            return;
+        }
+
+        $this->fail('Expected exception not thrown');
+    }
+
+    public function testLoadFileWithExistentPath()
+    {
+        $result = $this->service->loadFile('stub');
+        $this->assertEquals("alert(\"I am a dummy fixture!\");\n", $result);
+    }
+
+    public function testLoadFilesWhereOneOrMoreDoesNotExist()
+    {
+        try {
+            $this->service->loadFiles(['stub', 'foo']);
+        } catch (\Exception $e) {
+            $this->assertEquals('Attempted to load invalid script file "foo"', $e->getMessage());
+            return;
+        }
+
+        $this->fail('Expected exception not thrown');
+    }
+
+    public function testLoadFilesWhereAllFilesExist()
+    {
+        $scripts = $this->service->loadFiles(['stub', 'another_stub']);
+        $this->assertEquals($scripts, [
+            "alert(\"I am a dummy fixture!\");\n",
+            "alert(\"I am a stub!\");\n"
+        ]);
+    }
+
+}
