427af8495d940a269312f4fcc86566a9d5c7a952
Ian Lindsay <ian@hemera-business-services.co.uk>
2014-07-29 17:52:42 +0100

    Added isWithdrawn and withdrawnDate to form

--- Common/src/Common/Form/Forms/case-stay.form.php
+++ Common/src/Common/Form/Forms/case-stay.form.php
@@ -30,14 +30,13 @@ return [
                         'class' => 'extra-long'
                     ],
                     'isWithdrawn' => [
-                        'type' => 'checkbox',
+                        'type' => 'checkbox-yn',
                         'label' => 'Is withdrawn?',
-                        'required' => false
                     ],
                     'withdrawnDate' => [
                         'type' => 'dateSelectWithEmpty',
                         'label' => 'Withdrawn date',
-                        'required' => false
+                        'filters' => '\Common\Form\Elements\InputFilters\DateNotRequiredNotInFuture'
                     ],
                 ]
             ]
