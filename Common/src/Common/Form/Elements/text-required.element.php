535330ea25b18a49d0cff90dde94f0f26fb5a5b7
Rob Caiger <rob@clocal.co.uk>
2014-07-29 15:48:55 +0100

    Added translations and text-required element

---
+++ Common/src/Common/Form/Elements/text-required.element.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'text-required' => [
+        'name' => '',
+        'type' => '\Common\Form\Elements\InputFilters\TextRequired',
+        'options' => [
+            'label' => ''
+        ],
+        'required' => false,
+        'attributes' => [
+            'class' => '',
+            'id' => ''
+        ]
+    ]
+];
