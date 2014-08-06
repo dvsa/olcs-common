11ea536cd6f73aafd15cb66d17c8398ac3f0d7dc
Nick Payne <nick@kurai.co.uk>
2014-08-06 11:50:27 +0100

    skeleton task identifier formatter and checkbox table input

---
+++ Common/src/Common/Service/Table/Formatter/TaskIdentifier.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * task identifier formatter
+ *
+ * @author nick payne <nick.payne@valtech.co.uk>
+ */
+
+namespace Common\Service\Table\Formatter;
+
+/**
+ * task identifier formatter
+ *
+ * @author nick payne <nick.payne@valtech.co.uk>
+ */
+class TaskIdentifier implements FormatterInterface
+{
+    /**
+     * Format a task identifier
+     *
+     * @param array $data
+     * @param array $column
+     * @param \Zend\ServiceManager\ServiceManager $sm
+     * @return string
+     */
+    public static function format($data, $column, $sm)
+    {
+        return $data['id'] . ' (MLH)';
+    }
+}
