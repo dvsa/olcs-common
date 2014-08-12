88575f07346bcef62ce0b4907faaa69ddefd69cc
Chris Riley <chris.riley@valtech.co.uk>
2014-08-12 09:24:16 +0100

    olcs-3517: Updated form action controller to load forms from annotations when they are found

--- Common/src/Common/Controller/FormActionController.php
+++ Common/src/Common/Controller/FormActionController.php
@@ -9,6 +9,9 @@
 namespace Common\Controller;
 
 use Common\Form\Elements\Types\Address;
+use Zend\Filter\Word\DashToCamelCase;
+use Zend\Form\Annotation\AnnotationBuilder;
+use Zend\Form\Factory;
 use Zend\Mvc\MvcEvent;
 
 /**
@@ -83,6 +86,38 @@ abstract class FormActionController extends AbstractActionController
         $this->fieldValues[$key] = $value;
     }
 
+    protected function normaliseFormName($name, $ucFirst= false)
+    {
+        $name = str_replace([' ', '_'], '-', $name);
+
+        $filter = new DashToCamelCase();
+
+        if (!$ucFirst) {
+            return lcfirst($filter->filter($name));
+        }
+
+        return $filter->filter($name);
+    }
+
+    /**
+     * @param $type
+     * @return \Zend\Form\Form
+     * @TO-DO Turn this into a proper service/factory for forms
+     */
+    protected function getFormClass($type)
+    {
+        $formElementManager = $this->getServiceLocator()->get('FormElementManager');
+        $annotationBuilder = new AnnotationBuilder();
+        $annotationBuilder->setFormFactory(new Factory($formElementManager));
+        foreach (['Olcs', 'SelfServe', 'Common'] as $namespace) {
+            $class = $namespace . '\\Form\\Form\\' . $this->normaliseFormName($type, true);
+            if (class_exists($class)) {
+                return $annotationBuilder->createForm($class);
+            }
+        }
+        return $this->getServiceLocator()->get('OlcsCustomForm')->createForm($type);
+    }
+
     /**
      * Gets a from from either a built or custom form config.
      * @param type $type
@@ -90,7 +125,7 @@ abstract class FormActionController extends AbstractActionController
      */
     protected function getForm($type)
     {
-        $form = $this->getServiceLocator()->get('OlcsCustomForm')->createForm($type);
+        $form = $this->getFormClass($type);
 
         $form = $this->processPostcodeLookup($form);
 
