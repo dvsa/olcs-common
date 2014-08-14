78234064048b3780cc88c760e9461df4edb6796c
Ian Lindsay <ian@hemera-business-services.co.uk>
2014-08-14 14:46:47 +0100

    New withdrawnDate validator

--- Common/src/Common/Form/Elements/InputFilters/WithdrawnDate.php
+++ Common/src/Common/Form/Elements/InputFilters/WithdrawnDate.php
@@ -1,23 +1,55 @@
 <?php
 /**
+ * Checks that if a withdrawn checkbox is ticked then the corresponding date is also filled in
  *
  * @author Ian Lindsay <ian@hemera-business-services.co.uk>
- *
  */
 
 namespace Common\Form\Elements\InputFilters;
 
 use Zend\InputFilter\InputProviderInterface as InputProviderInterface;
+use Zend\Form\Element\DateSelect as ZendDateSelect;
 
 /**
- *
- * @author Ian Lindsay <ian@hemera-business-services.co.uk>
- *
  * Checks that if a withdrawn checkbox is ticked then the corresponding date is also filled in
  *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
  */
-class WithdrawnDate extends DateNotRequiredNotInFuture implements InputProviderInterface
+class WithdrawnDate extends ZendDateSelect implements InputProviderInterface
 {
+
+    /**
+     * Provide default input rules for this element.
+     *
+     * @return array
+     */
+    public function getInputSpecification()
+    {
+        $specification = [
+            'name' => $this->getName(),
+            'continue_if_empty' => true,
+            'filters' => array(
+                array(
+                    'name'    => 'Callback',
+                    'options' => array(
+                        'callback' => function ($date) {
+                            // Convert the date to a specific format
+                            if (!is_array($date) || empty($date['year']) ||
+                                empty($date['month']) || empty($date['day'])) {
+                                return null;
+                            }
+
+                            return $date['year'] . '-' . $date['month'] . '-' . $date['day'];
+                        }
+                    )
+                )
+            ),
+            'validators' => $this->getValidators()
+        ];
+
+        return $specification;
+    }
+
     public function getValidators()
     {
         return array(
--- Common/src/Common/Form/Elements/Validators/WithdrawnDate.php
+++ Common/src/Common/Form/Elements/Validators/WithdrawnDate.php
@@ -1,7 +1,60 @@
 <?php
+
 /**
- * Created by PhpStorm.
- * User: ian
- * Date: 13/08/2014
- * Time: 17:16
- */ 
+ * Custom validator for isWithdrawn.
+ * If the checkbox is ticked, the datefield will be validated
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+namespace Common\Form\Elements\Validators;
+
+use Zend\Validator as ZendValidator;
+
+/**
+ * Custom validator for isWithdrawn.
+ * If the checkbox is ticked, the date field will be validated
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+class WithdrawnDate extends ZendValidator\AbstractValidator
+{
+    const DATE_NOT_VALID = 'dateNotValid';
+    const DATE_IN_FUTURE = 'dateInFuture';
+
+    /**
+     * Validation failure message template definitions
+     *
+     * @var array
+     */
+    protected $messageTemplates = array(
+        self::DATE_NOT_VALID   => "Withdrawn date is not valid",
+        self::DATE_IN_FUTURE    => "Withdrawn date can't be in the future",
+    );
+
+    public function isValid($value, $context = null)
+    {
+        $this->setValue($value);
+
+        if ($context['isWithdrawn'] == 'Y') {
+            $withdrawnDate = $context['withdrawnDate']['year'] .
+                '-' . $context['withdrawnDate']['month'] .
+                '-' . $context['withdrawnDate']['day'];
+
+            $dateValidator = new Date();
+
+            if (!$dateValidator->isValid($withdrawnDate)) {
+                $this->error(self::DATE_NOT_VALID);
+                return false;
+            }
+
+            $notInFutureValidator = new DateNotInFuture();
+
+            if (!$notInFutureValidator->isValid($withdrawnDate)) {
+                $this->error(self::DATE_IN_FUTURE);
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
