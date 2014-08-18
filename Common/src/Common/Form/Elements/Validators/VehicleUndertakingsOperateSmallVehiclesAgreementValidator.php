e3c3147aa0fddaa0389ad90f3e66918039ae67e9
Jess Rowbottom <jess@wrenthorpe.net>
2014-08-18 10:01:33 +0100

    Added in validation for undertakings section.

---
+++ Common/src/Common/Form/Elements/Validators/VehicleUndertakingsOperateSmallVehiclesAgreementValidator.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * VehicleUndertakingsOperateSmallVehiclesAgreementValidator
+ *
+ * @author Jessica Rowbottom <jess.rowbottom@valtech.co.uk>
+ */
+namespace Common\Form\Elements\Validators;
+
+use Zend\Validator\AbstractValidator;
+
+/**
+ * VehicleUndertakingsOperateSmallVehiclesAgreementValidator
+ *
+ * @author Jessica Rowbottom <jess.rowbottom@valtech.co.uk>
+ */
+class VehicleUndertakingsOperateSmallVehiclesAgreementValidator extends AbstractValidator
+{
+    /**
+     * Message templates
+     *
+     * @var array
+     */
+    protected $messageTemplates = array(
+        'required' => 'You must agree'
+    );
+
+    /**
+     * Custom validation for undertakings checkbox field
+     *
+     * @param mixed $value
+     * @param array $context
+     */
+    public function isValid($value, $context = null)
+    {
+        unset($value);
+        var_dump($context);
+
+        // This only gets used if psvOperateSmallVehicles is shown
+        if ( isset($context['psvOperateSmallVehicles']) ) {
+            if ($context['psvOperateSmallVehicles'] === 'N'
+                && $context['psvSmallVehicleConfirmation'] !== 'checked') {
+
+                $this->error('required');
+
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ Common/src/Common/Form/Elements/Validators/VehicleUndertakingsOperateSmallVehiclesValidator.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * VehiclesUndertakingsOperateSmallVehicles
+ *
+ * @author Jessica Rowbottom <jess.rowbottom@valtech.co.uk>
+ */
+namespace Common\Form\Elements\Validators;
+
+use Zend\Validator\AbstractValidator;
+
+/**
+ * VehiclesUndertakingsOperateSmallVehiclesValidator
+ *
+ * @author Jessica Rowbottom <jess.rowbottom@valtech.co.uk>
+ */
+class VehicleUndertakingsOperateSmallVehiclesValidator extends AbstractValidator
+{
+    /**
+     * Message templates
+     *
+     * @var array
+     */
+    protected $messageTemplates = array(
+        'required' => 'Value is required and can\'t be empty'
+    );
+
+    /**
+     * Custom validation for undertakings text field
+     *
+     * @param mixed $value
+     * @param array $context
+     */
+    public function isValid($value, $context = null)
+    {
+        unset($value);
+        
+        if ($context['psvOperateSmallVehicles'] === 'Y'
+            && trim($context['psvSmallVehicleNotes']) === '') {
+
+            $this->error('required');
+
+            return false;
+        }
+
+        return true;
+    }
+}
