40b713b634729449c3d0e7d9c2a51b30c8178371
Nick Payne <nick@kurai.co.uk>
2014-08-28 08:57:03 +0100

    add dateselect tests

---
+++ test/Common/src/Common/Form/Elements/Custom/DateSelectTest.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * DateSelectTest
+ *
+ * @author Nick Payne <nick.payne@valtech.co.uk>
+ */
+namespace CommonTest\Form\Elements\Types;
+
+use Common\Form\Elements\Custom\DateSelect;
+
+/**
+ * DateSelectTest
+ *
+ * @author Nick Payne <nick.payne@valtech.co.uk>
+ */
+class DateSelectTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test the input specification
+     */
+    public function testGetInputSpecificationWithRequiredAndMaxYearDelta()
+    {
+        $element = new DateSelect();
+        $element->setOptions(array(
+            'max_year_delta' => '+11',
+            'required' => true
+        ));
+        $spec = $element->getInputSpecification();
+
+        $targetYear = date('Y', strtotime('+11 years'));
+        $this->assertTrue($spec['required']);
+        $this->assertEquals($targetYear, $element->getMaxYear());
+    }
+
+    public function testGetInputSpecificationNotRequiredStandardMaxYear()
+    {
+        $element = new DateSelect();
+        $spec = $element->getInputSpecification();
+
+        $targetYear = date('Y');
+        $this->assertNull($spec['required']);
+        $this->assertEquals($targetYear, $element->getMaxYear());
+    }
+}
