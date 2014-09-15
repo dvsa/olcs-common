7f7cbae34295070bc6865592f163621656758a91
Rob Caiger <rob@clocal.co.uk>
2014-09-15 10:26:25 +0100

    Renamed delete button

--- Common/src/Common/Table/Tables/application_vehicle-safety_vehicle.table.php
+++ Common/src/Common/Table/Tables/application_vehicle-safety_vehicle.table.php
@@ -11,7 +11,7 @@ return array(
             'actions' => array(
                 'add' => array('class' => 'primary'),
                 'edit' => array('requireRows' => true),
-                'delete' => array('class' => 'warning', 'requireRows' => true)
+                'delete' => array('label' => 'Remove', 'class' => 'warning', 'requireRows' => true)
             )
         ),
         'paginate' => array(
