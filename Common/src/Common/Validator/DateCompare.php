0692b697a180b79f9dba1e00c20b3aa2128ee9c9
Chris Riley <chris.riley@valtech.co.uk>
2014-09-22 17:10:17 +0100

    Adds Date compare validator

---
+++ Common/src/Common/Validator/DateCompare.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace Common\Validator;
+
+use Common\Filter\DateSelectNullifier;
+use Zend\Validator\AbstractValidator;
+
+class DateCompare extends AbstractValidator
+{
+    /**
+     * Error codes
+     * @const string
+     */
+    const NOT_GTE = 'notGreaterThanOrEqual';
+    const NOT_GT = 'notGreaterThan';
+    const NOT_LTE = 'notLessThanOrEqual';
+    const NOT_LT = 'notLessThan';
+
+    /**
+     * Error messages
+     * @var array
+     */
+    protected $messageTemplates = array(
+        self::NOT_GTE => "This date must be after or the same as %compare_to%",
+    );
+
+    /**
+     * @var array
+     */
+    protected $messageVariables = array(
+        'compare_to' => 'compareToLabel'
+    );
+
+    /**
+     * context field against which to validate
+     * @var string
+     */
+    protected $compareTo;
+
+    /**
+     * Type of compare to do
+     * @var string
+     */
+    protected $operator;
+
+    /**
+     * Label of compare to field to use in error message
+     * @var string
+     */
+    protected $compareToLabel;
+
+    /**
+     * @param string $compareTo
+     * @return $this
+     */
+    public function setCompareTo($compareTo)
+    {
+        $this->compareTo = $compareTo;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCompareTo()
+    {
+        return $this->compareTo;
+    }
+
+    /**
+     * @param string $compareToLabel
+     * @return $this
+     */
+    public function setCompareToLabel($compareToLabel)
+    {
+        $this->compareToLabel = $compareToLabel;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCompareToLabel()
+    {
+        return $this->compareToLabel;
+    }
+
+    /**
+     * @param string $operator
+     * @return $this
+     */
+    public function setOperator($operator)
+    {
+        $this->operator = $operator;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOperator()
+    {
+        return $this->operator;
+    }
+
+    public function setOptions($options)
+    {
+        if (isset($options['compare_to'])) {
+            $this->setCompareTo($options['compare_to']);
+        }
+
+        if (isset($option['operator'])) {
+            $this->setOperator($options['operator']);
+        }
+
+        if (isset($options['compare_to_label'])) {
+            $this->setCompareToLabel($options['compare_to_label']);
+        }
+
+        return parent::setOptions($options);
+    }
+
+    /**
+     * Returns true if and only if a token has been set and the provided value
+     * matches that token.
+     *
+     * @param  mixed $value
+     * @param  array $context
+     * @return bool
+     * @throws Exception\RuntimeException if the token doesn't exist in the context array
+     */
+    public function isValid($value, array $context = null)
+    {
+        if (!isset($context[$this->getCompareTo()])) {
+            $this->error('context field not in input'); //@TO~DO~
+            return false;
+        }
+
+        $dateFilter = new DateSelectNullifier();
+
+        $compareToValue = $dateFilter->filter($context[$this->getCompareTo()]);
+
+        switch ($this->getOperator()) {
+            case 'gte':
+                if (!($value >= $compareToValue)) {
+                    $this->error(self::NOT_GTE);
+                    return false;
+                }
+                return true;
+            case 'lte':
+                if (!($value <= $compareToValue)) {
+                    $this->error(self::NOT_LTE);
+                    return false;
+                }
+                return true;
+            case 'gt':
+                if (!($value > $compareToValue)) {
+                    $this->error(self::NOT_GT);
+                    return false;
+                }
+                return true;
+            case 'lt':
+                if (!($value < $compareToValue)) {
+                    $this->error(self::NOT_LT);
+                    return false;
+                }
+                return true;
+            default:
+                $this->error('invalid operator'); //@TODO
+                return false;
+        }
+    }
+}
