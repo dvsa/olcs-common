ba194ff0a5b135458e390a266d2041eb08dfcb02
Nick Payne <nick@kurai.co.uk>
2014-09-22 08:54:29 +0100

    add licence_holder_address

---
+++ Common/src/Common/Service/Document/Bookmark/LicenceHolderAddress.php
@@ -0,0 +1,61 @@
+<?php
+namespace Common\Service\Document\Bookmark;
+
+class LicenceHolderAddress extends DynamicBookmark
+{
+    public function getQuery(array $data)
+    {
+        $query = [
+            'service' => 'Licence',
+            'data' => [
+                'id' => $data['licence']
+            ],
+            'bundle' => [
+                'properties' => ['organisation'],
+                'children' => [
+                    'organisation' => [
+                        'properties' => ['contactDetails'],
+                        'children' => [
+                            'contactDetails' => [
+                                'properties' => ['contactType', 'address'],
+                                'children' => [
+                                    'address' => [
+                                        'properties' => [
+                                            'addressLine1',
+                                            'addressLine2',
+                                            'addressLine3',
+                                            'addressLine4',
+                                            'town',
+                                            'postcode'
+                                        ],
+                                    ],
+                                    'contactType' => [
+                                        'properties' => ['id']
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ];
+
+        return $query;
+    }
+
+    public function format()
+    {
+        foreach ($this->data['organisation']['contactDetails'] as $contactDetail) {
+            if ($contactDetail['contactType']['id'] === 'ct_corr') {
+                $address = $contactDetail['address'];
+
+                return $address['addressLine1']
+                    . "\n" . $address['addressLine2']
+                    . "\n" . $address['addressLine3']
+                    . "\n" . $address['addressLine4']
+                    . "\n" . $address['town']
+                    . "\n" . $address['postcode'];
+            }
+        }
+    }
+}
