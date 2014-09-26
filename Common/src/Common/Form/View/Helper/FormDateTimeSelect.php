2b0ecf5e54a5f10e933c8e38b61e3d6cf414d1fa
Chris Riley <chris.riley@valtech.co.uk>
2014-09-26 15:47:26 +0100

    Updated view helper to use pattern provided by element

--- Common/src/Common/Form/View/Helper/FormDateTimeSelect.php
+++ Common/src/Common/Form/View/Helper/FormDateTimeSelect.php
@@ -26,78 +26,8 @@ class FormDateTimeSelect extends ZendFormDateTimeSelect
      */
     public function render(ElementInterface $element)
     {
-        if (!$element instanceof DateTimeSelectElement) {
-            throw new Exception\InvalidArgumentException(sprintf(
-                '%s requires that the element is of type Zend\Form\Element\DateTimeSelect',
-                __METHOD__
-            ));
-        }
-
-        $name = $element->getName();
-        if ($name === null || $name === '') {
-            throw new Exception\DomainException(sprintf(
-                '%s requires that the element has an assigned name; none discovered',
-                __METHOD__
-            ));
-        }
-
-        $shouldRenderDelimiters = $element->shouldRenderDelimiters();
-        $selectHelper = $this->getSelectElementHelper();
-        $pattern      = $this->parsePattern($shouldRenderDelimiters);
-
-        $daysOptions   = $this->getDaysOptions($pattern['day']);
-        $monthsOptions = $this->getMonthsOptions($pattern['month']);
-        $yearOptions   = $this->getYearsOptions($element->getMinYear(), $element->getMaxYear());
-        $hourOptions   = $this->getHoursOptions($pattern['hour']);
-        $minuteOptions = $this->getMinutesOptions($pattern['minute']);
-        $secondOptions = $this->getSecondsOptions($pattern['second']);
-
-        $dayElement    = $element->getDayElement()->setValueOptions($daysOptions);
-        $monthElement  = $element->getMonthElement()->setValueOptions($monthsOptions);
-        $yearElement   = $element->getYearElement()->setValueOptions($yearOptions);
-        $hourElement   = $element->getHourElement()->setValueOptions($hourOptions);
-        $minuteElement = $element->getMinuteElement()->setValueOptions($minuteOptions);
-        $secondElement = $element->getSecondElement()->setValueOptions($secondOptions);
-
-        if ($element->shouldCreateEmptyOption()) {
-            $dayElement->setEmptyOption('');
-            $yearElement->setEmptyOption('');
-            $monthElement->setEmptyOption('');
-            $hourElement->setEmptyOption('');
-            $minuteElement->setEmptyOption('');
-            $secondElement->setEmptyOption('');
-        }
-
-        $data = array();
-        $data[$pattern['day']]    = $selectHelper->render($dayElement);
-        $data[$pattern['month']]  = $selectHelper->render($monthElement);
-        $data[$pattern['year']]   = $selectHelper->render($yearElement);
-        $data[$pattern['hour']]   = $selectHelper->render($hourElement);
-        $data[$pattern['minute']] = $selectHelper->render($minuteElement);
-
-        if ($element->shouldShowSeconds()) {
-            $data[$pattern['second']]  = $selectHelper->render($secondElement);
-        } else {
-            unset($pattern['second']);
-            if ($shouldRenderDelimiters) {
-                unset($pattern[4]);
-            }
-        }
-
-        $markup = '';
-        foreach ($pattern as $key => $value) {
-            // Delimiter
-            if (is_numeric($key)) {
-                $markup .= $value;
-            } else {
-                if($key == 'hour'){
-                    $markup .= '</div><div class="field"><label>Hearing time</label>';
-                }
-                $markup .= $data[$value];
-            }
-        }
-        $markup = trim($markup);
-        return $markup;
+        $this->pattern = $element->getOption('pattern');
+        return parent::render($element);
     }
 
     /**
