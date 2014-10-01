2054a41efb7711d9fd616f2fda253b2f0b18d20d
Rob Caiger <rob@clocal.co.uk>
2014-10-01 09:33:50 +0100

    Form config

---
+++ Common/src/Common/Form/Forms/application_vehicle-safety_discs-psv.form.php
@@ -0,0 +1,36 @@
+<?php
+
+$translationPrefix = 'application_vehicle-safety_discs-psv';
+
+return array(
+    $translationPrefix => array(
+        'name' => $translationPrefix,
+        'attributes' => array(
+            'method' => 'post',
+        ),
+        'fieldsets' => array(
+            array(
+                'name' => 'data',
+                'options' => array(),
+                'elements' => array(
+                    'validDiscs' => array(
+                        'label' => $translationPrefix . '.validDiscs',
+                        'type' => 'text'
+                    ),
+                    'pending' => array(
+                        'label' => $translationPrefix . '.pending',
+                        'type' => 'text'
+                    )
+                )
+            ),
+            array(
+                'name' => 'table',
+                'options' => array(0),
+                'type' => 'table'
+            ),
+            array(
+                'type' => 'journey-buttons'
+            )
+        )
+    )
+);
