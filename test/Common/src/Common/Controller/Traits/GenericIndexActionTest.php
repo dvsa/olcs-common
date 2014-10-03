0031996fd1dc84483be7cc275588c91ff0347a57
Rob Caiger <rob@clocal.co.uk>
2014-10-03 11:44:53 +0100

    Unit tested generic traits

---
+++ test/Common/src/Common/Controller/Traits/GenericIndexActionTest.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Generic Index Action Test
+ *
+ * @author Rob Caiger <rob@clocal.co.uk>
+ */
+namespace CommonTest\Controller\Traits;
+
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Generic Index Action Test
+ *
+ * @author Rob Caiger <rob@clocal.co.uk>
+ */
+class GenericIndexActionTest extends PHPUnit_Framework_TestCase
+{
+    private $sut;
+
+    protected function setUp()
+    {
+        $this->sut = $this->getMockForTrait(
+            '\Common\Controller\Traits\GenericIndexAction',
+            array(),
+            '',
+            true,
+            true,
+            true,
+            array('renderSection')
+        );
+    }
+
+    /**
+     * @group controller_traits
+     * @group generic_section_controller_traits
+     */
+    public function testIndexAction()
+    {
+        $this->sut->expects($this->once())
+            ->method('renderSection');
+
+        $this->sut->indexAction();
+    }
+}
