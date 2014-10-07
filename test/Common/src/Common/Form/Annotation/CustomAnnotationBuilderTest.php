63a1df4e38a56cb37c51d208238660121f796b7d
Ian Lindsay <ian@hemera-business-services.co.uk>
2014-10-07 13:48:28 +0100

    Speed up form unit tests

---
+++ test/Common/src/Common/Form/Annotation/CustomAnnotationBuilderTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace CommonTest\Form\Annotation;
+
+use Common\Form\Annotation\CustomAnnotationBuilder;
+
+/**
+ * Class CustomAnnotationBuilderTest
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+class CustomAnnotationBuilderTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * We override this Zend method so it always returns false
+     */
+    public function testIsSubclassOfReturnsFalse()
+    {
+        $sut = new CustomAnnotationBuilder();
+        $this->assertEquals(false, $sut::isSubclassOf('', ''));
+    }
+}
