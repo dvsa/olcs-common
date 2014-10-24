2ddbc3f8a94bdd74578cc5feec4889a99b2226fc
Nick Payne <nick@kurai.co.uk>
2014-10-24 15:16:29 +0100

    initial work on edit OC on licence page

--- Common/src/Common/Controller/Lva/Traits/LicenceOperatingCentresControllerTrait.php
+++ Common/src/Common/Controller/Lva/Traits/LicenceOperatingCentresControllerTrait.php
@@ -5,9 +5,49 @@
  */
 namespace Common\Controller\Lva\Traits;
 
+use Common\Form\Elements\Validators\CantIncreaseValidator;
+
 /**
  */
 trait LicenceOperatingCentresControllerTrait
 {
-    // not yet
+    /**
+     * Attach a cant increase validator
+     *
+     * @param Input $input
+     * @param string $messageSuffix
+     * @param int $previousValue
+     */
+    protected function attachCantIncreaseValidator($input, $messageSuffix, $previousValue)
+    {
+        $validatorChain = $input->getValidatorChain();
+
+        $cantIncreaseValidator = new CantIncreaseValidator();
+
+        $message = $this->getServiceLocator()->get('Helper\Translation')->formatTranslation(
+            '%s <a href="#">%s</a>',
+            array(
+                'cant-increase-' . $messageSuffix,
+                $this->url()->fromRoute('create_variation', ['id' => $this->getIdentifier()])
+            )
+        );
+
+        $cantIncreaseValidator->setGenericMessage($message);
+        $cantIncreaseValidator->setPreviousValue($previousValue);
+
+        $validatorChain->attach($cantIncreaseValidator);
+    }
+
+    /**
+     * Get the vehicle auths for the OC (given a licence_operating_Centre_id)
+     *
+     * @param int $id
+     * @return array
+     */
+    protected function getVehicleAuthsForOperatingCentre($id)
+    {
+        return $this->getServiceLocator()
+            ->get('Entity\LicenceOperatingCentre')
+            ->getVehicleAuths($id);
+    }
 }
