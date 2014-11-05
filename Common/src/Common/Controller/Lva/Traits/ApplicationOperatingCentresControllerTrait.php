11c17b1e9922af16bc477a3d4d03c8dd1c0438f9
Nick Payne <nick@kurai.co.uk>
2014-11-05 08:10:09 +0000

    implement traffic area checks when adding/removing OCs

--- Common/src/Common/Controller/Lva/Traits/ApplicationOperatingCentresControllerTrait.php
+++ Common/src/Common/Controller/Lva/Traits/ApplicationOperatingCentresControllerTrait.php
@@ -1,9 +1,16 @@
 <?php
 
+/**
+ * Application Operating Centres Controller Trait
+ *
+ * @author Nick Payne <nick.payne@valtech.co.uk>
+ */
 namespace Common\Controller\Lva\Traits;
 
 /**
  * Application Operating Centres Controller Trait
+ *
+ * @author Nick Payne <nick.payne@valtech.co.uk>
  */
 trait ApplicationOperatingCentresControllerTrait
 {
@@ -15,5 +22,41 @@ trait ApplicationOperatingCentresControllerTrait
         );
     }
 
-    // @TODO port setTrafficAreaAfterCrud
+    /**
+     * Check Traffic Area After Crud Action
+     *
+     * @param array $data
+     */
+    protected function checkTrafficAreaAfterCrudAction($data)
+    {
+        $action = strtolower($data['action']);
+
+        $data = (array)$this->getRequest()->getPost();
+
+        if ($action === 'add' && !$this->getTrafficArea()) {
+            $trafficArea = isset($data['dataTrafficArea']['trafficArea'])
+                ? $data['dataTrafficArea']['trafficArea']
+                : '';
+
+            if (empty($trafficArea) && $this->getOperatingCentresCount()) {
+                $this->getServiceLocator()
+                    ->get('Helper\FlashMessenger')
+                    ->addWarningMessage('select-traffic-area-error');
+
+                return $this->redirect()->toRoute(null, array(), array(), true);
+            }
+        }
+    }
+
+    protected function checkTrafficAreaAfterDelete()
+    {
+        if ($this->getOperatingCentresCount() === 0) {
+            $this->getServiceLocator()
+                ->get('Entity\Licence')
+                ->setTrafficArea(
+                    $this->getLicenceId(),
+                    null
+                );
+        }
+    }
 }
