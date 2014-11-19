d5dbe04217afa83f3b31ed6ca3297c191a3beb49
Ian Lindsay <ian@hemera-business-services.co.uk>
2014-11-19 17:03:30 +0000

    Publication section builder

---
+++ Common/src/Common/Filter/Publication/PublicationSection.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Publication Section Id filter
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+namespace Common\Filter\Publication;
+
+use Common\Exception\ResourceNotFoundException;
+
+/**
+ * Publication Section filter
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+class PublicationSection extends AbstractPublicationFilter
+{
+    const HEARING_SECTION_ID = 13;
+
+    /**
+     * @param \Zend\Stdlib\ArrayObject $publication
+     * @return \Zend\Stdlib\ArrayObject
+     * @throws ResourceNotFoundException
+     */
+    public function filter($publication)
+    {
+        $newData = [
+            'publicationSection' => self::HEARING_SECTION_ID,
+        ];
+
+        $publication = $this->mergeData($publication, $newData);
+
+        return $publication;
+    }
+}
