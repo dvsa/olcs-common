e3f6f6895b8e47bc6eb47e02e79e4fe634e8bc9e
Chris Riley <chris.riley@valtech.co.uk>
2014-11-20 16:45:48 +0000

    Added multi response helper, updated rest client to allow injecting the response helper and added 202 as an allowed response code

---
+++ Common/src/Common/Util/MultiResponseHelper.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Common\Util;
+
+use Zend\Http\Response;
+
+/**
+ * Class MultiResponseHelper
+ * @package Common\Util
+ */
+class MultiResponseHelper extends ResponseHelper
+{
+    /**
+     * @return array|bool
+     */
+    public function handleResponse()
+    {
+        $this->body = $this->response->getBody();
+        $this->checkForValidResponseBody($this->body);
+        $this->checkForInternalServerError($this->body);
+
+        if ($this->method == 'POST' && $this->response->getStatusCode() == Response::STATUS_CODE_207) {
+            //response is a multi status response
+            $responses = $this->responseData['Data'];
+            $return = [];
+            foreach ($responses as $uri => $response) {
+                //could add code to check for bad statuses, but for EBSR the 400 response is used for validation
+                //errors so shouldn't trigger an exception
+                $return[$uri] = $response['Data'];
+            }
+
+            if (count($return)) {
+                return $return;
+            }
+        }
+
+        return false;
+    }
+}
