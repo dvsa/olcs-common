ceaa2fbb2779f849b1ce321f9362c2d9c5668fd0
Ian Lindsay <ian@hemera-business-services.co.uk>
2014-12-04 15:14:40 +0000

    Decision text filter, based on text 1 filter

---
+++ Common/src/Common/Filter/Publication/DecisionText1.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Decision text 1 filter
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+namespace Common\Filter\Publication;
+
+/**
+ * Decision text 1 filter
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+class DecisionText1 extends Text1
+{
+    protected $pi = 'Public Inquiry (%s) held at %s, on %s commencing at %s';
+}
