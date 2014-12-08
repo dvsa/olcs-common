15ddaf02b4ecb598fcdb4948002babff84174cf6
Rob Caiger <rob@clocal.co.uk>
2014-12-08 13:40:32 +0000

    PSV vehicle validation

--- test/Common/src/Common/Service/Entity/ApplicationCompletionEntityServiceTest.php
+++ test/Common/src/Common/Service/Entity/ApplicationCompletionEntityServiceTest.php
@@ -2809,7 +2809,7 @@ class ApplicationCompletionEntityServiceTest extends AbstractEntityServiceTestCa
                 $initialStatus,
                 // Mocked application data
                 array(
-                    // none
+                    'hasEnteredReg' => 'Y'
                 ),
                 // Expected completion data
                 array(
@@ -2845,6 +2845,7 @@ class ApplicationCompletionEntityServiceTest extends AbstractEntityServiceTestCa
                 $initialStatus,
                 // Mocked application data
                 array(
+                    'hasEnteredReg' => 'Y',
                     'totAuthSmallVehicles' => 0,
                     'totAuthMediumVehicles' => 0,
                     'totAuthLargeVehicles' => 0,
@@ -2892,6 +2893,7 @@ class ApplicationCompletionEntityServiceTest extends AbstractEntityServiceTestCa
                 $initialStatus,
                 // Mocked application data
                 array(
+                    'hasEnteredReg' => 'Y',
                     'totAuthSmallVehicles' => 2,
                     'totAuthMediumVehicles' => 1,
                     'totAuthLargeVehicles' => null,
@@ -2942,6 +2944,7 @@ class ApplicationCompletionEntityServiceTest extends AbstractEntityServiceTestCa
                 $initialStatus,
                 // Mocked application data
                 array(
+                    'hasEnteredReg' => 'Y',
                     'totAuthSmallVehicles' => 2,
                     'totAuthMediumVehicles' => 1,
                     'totAuthLargeVehicles' => 0,
@@ -2958,6 +2961,43 @@ class ApplicationCompletionEntityServiceTest extends AbstractEntityServiceTestCa
                             )
                         )
                     )
+                ),
+                // Expected completion data
+                array(
+                    'id' => 3,
+                    'application' => 3,
+                    'typeOfLicenceStatus' => ApplicationCompletionEntityService::STATUS_NOT_STARTED,
+                    'businessTypeStatus' => ApplicationCompletionEntityService::STATUS_NOT_STARTED,
+                    'businessDetailsStatus' => ApplicationCompletionEntityService::STATUS_NOT_STARTED,
+                    'addressesStatus' => ApplicationCompletionEntityService::STATUS_NOT_STARTED,
+                    'peopleStatus' => ApplicationCompletionEntityService::STATUS_NOT_STARTED,
+                    'taxiPhvStatus' => ApplicationCompletionEntityService::STATUS_NOT_STARTED,
+                    'operatingCentresStatus' => ApplicationCompletionEntityService::STATUS_NOT_STARTED,
+                    'financialEvidenceStatus' => ApplicationCompletionEntityService::STATUS_NOT_STARTED,
+                    'transportManagersStatus' => ApplicationCompletionEntityService::STATUS_NOT_STARTED,
+                    'vehiclesStatus' => ApplicationCompletionEntityService::STATUS_NOT_STARTED,
+                    'vehiclesPsvStatus' => ApplicationCompletionEntityService::STATUS_COMPLETE,
+                    'vehiclesDeclarationsStatus' => ApplicationCompletionEntityService::STATUS_NOT_STARTED,
+                    'discsStatus' => ApplicationCompletionEntityService::STATUS_NOT_STARTED,
+                    'communityLicencesStatus' => ApplicationCompletionEntityService::STATUS_NOT_STARTED,
+                    'safetyStatus' => ApplicationCompletionEntityService::STATUS_NOT_STARTED,
+                    'conditionsUndertakingsStatus' => ApplicationCompletionEntityService::STATUS_NOT_STARTED,
+                    'financialHistoryStatus' => ApplicationCompletionEntityService::STATUS_NOT_STARTED,
+                    'licenceHistoryStatus' => ApplicationCompletionEntityService::STATUS_NOT_STARTED,
+                    'convictionsPenaltiesStatus' => ApplicationCompletionEntityService::STATUS_NOT_STARTED,
+                    'undertakingsStatus' => ApplicationCompletionEntityService::STATUS_NOT_STARTED
+                )
+            ),
+
+            // Vehicles PSV complete: not entering reg
+            'vehicles_psv_5' => array(
+                // Current section
+                'vehicles_psv',
+                // Mocked start completion data
+                $initialStatus,
+                // Mocked application data
+                array(
+                    'hasEnteredReg' => 'N'
                 ),
                 // Expected completion data
                 array(
