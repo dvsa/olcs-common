fcad815f59f1e6d09b918ca99f3f29420b56de0b
Ian Lindsay <ian@hemera-business-services.co.uk>
2014-12-11 11:15:20 +0000

    Methods moved to the parent class to allow reuse

--- Common/src/Common/Service/Data/PublicationPolice.php
+++ Common/src/Common/Service/Data/PublicationPolice.php
@@ -9,7 +9,6 @@ namespace Common\Service\Data;
 
 use Zend\ServiceManager\ServiceLocatorAwareInterface;
 use Zend\ServiceManager\ServiceLocatorAwareTrait;
-use Common\Util\RestClient;
 use Common\Data\Object\PublicationPolice as PoliceObject;
 
 /**
@@ -32,67 +31,5 @@ class PublicationPolice extends AbstractData implements ServiceLocatorAwareInter
     public function createEmpty()
     {
         return new PoliceObject();
-    }
-
-    public function createWithData($data)
-    {
-        $police = $this->createEmpty();
-
-        foreach ($data as $key => $value) {
-            $police->offsetSet($key, $value);
-        }
-
-        return $police;
-    }
-
-    /**
-     * @param \Common\Data\Object\PublicationPolice $police
-     * @return mixed
-     */
-    public function save(PoliceObject $police)
-    {
-        $method = 'POST';
-
-        if ($police->offsetGet('id')) {
-            $method = 'PUT';
-        }
-
-        return $this->getServiceLocator()->get('Helper\Rest')->makeRestCall(
-            'PublicationPoliceData',
-            $method,
-            $police->getArrayCopy()
-        );
-    }
-
-    /**
-     * @param \Common\Data\Object\PublicationPolice $police
-     * @param string $service
-     * @return mixed
-     */
-    public function create(PoliceObject $police, $service)
-    {
-        $police = $this->getServiceLocator()->get($service)->filter($police);
-        return $this->save($police);
-    }
-
-    /**
-     * @param array $params
-     * @param null $bundle
-     * @return mixed
-     */
-    public function fetchList($params = [], $bundle = null)
-    {
-        $params['bundle'] = json_encode(empty($bundle) ? $this->getBundle() : $bundle);
-        return $this->getRestClient()->get($params);
-    }
-
-    /**
-     * @return array
-     */
-    private function getBundle()
-    {
-        return [
-            'properties' => 'ALL',
-        ];
     }
 }
