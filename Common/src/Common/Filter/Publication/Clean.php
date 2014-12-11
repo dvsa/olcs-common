fb44a2447033dd2f3e71ea2ccadf364c83a3aef9
Ian Lindsay <ian@hemera-business-services.co.uk>
2014-12-11 11:10:45 +0000

    Filter to clean data from the publication object which is no longer needed

---
+++ Common/src/Common/Filter/Publication/Clean.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Removes extra data used to build up our object
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+namespace Common\Filter\Publication;
+
+/**
+ * Removes extra data used to build up our object
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+class Clean extends AbstractPublicationFilter
+{
+    /**
+     * @param \Zend\Stdlib\ArrayObject $object
+     * @return \Zend\Stdlib\ArrayObject
+     */
+    public function filter($object)
+    {
+        $keys = [
+            'hearingData',
+            'licenceData',
+            'publicationSectionConst'
+        ];
+
+        foreach ($keys as $key) {
+            if ($object->offsetExists($key)) {
+                $object->offsetUnset($key);
+            }
+        }
+
+        return $object;
+    }
+}
