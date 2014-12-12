41ee6cf572502b09823f5e05de2c0e17da839b43
Ian Lindsay <ian@hemera-business-services.co.uk>
2014-12-12 11:33:08 +0000

    Tests the Licence publication filter

---
+++ test/Common/src/Common/Filter/Publication/LicenceTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace CommonTest\Filter\Publication;
+
+use Common\Filter\Publication\Licence;
+use Common\Data\Object\Publication;
+use Mockery as m;
+
+/**
+ * Class LicenceTest
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+class LicenceTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Tests exception thrown if there is no licence
+     *
+     * @expectedException \Common\Exception\ResourceNotFoundException
+     */
+    public function testNoLicenceException()
+    {
+        $input = new Publication();
+        $sut = new Licence();
+
+        $mockLicenceService = m::mock('Common\Service\Data\Licence');
+        $mockLicenceService->shouldReceive('fetchLicenceData')->andReturn(false);
+
+        $mockServiceManager = m::mock('\Zend\ServiceManager\ServiceManager');
+        $mockServiceManager->shouldReceive('get')->with('\Common\Service\Data\Licence')->andReturn($mockLicenceService);
+
+        $sut->setServiceLocator($mockServiceManager);
+
+        $sut->filter($input);
+    }
+
+    /**
+     * @dataProvider filterProvider
+     *
+     * @param string $goodsOrPsv
+     * @param string $expectedPubType
+     */
+    public function testFilter($goodsOrPsv, $expectedPubType)
+    {
+        $trafficArea = 'B';
+        $licenceId = 7;
+
+        $licenceData = [
+            'id' => $licenceId,
+            'trafficArea' => [
+                'id' => $trafficArea
+            ],
+            'goodsOrPsv' => [
+                'id' => $goodsOrPsv
+            ]
+        ];
+
+        $expectedOutput = [
+            'pubType' => $expectedPubType,
+            'licence' => $licenceId,
+            'trafficArea' => $trafficArea,
+            'licenceData' => $licenceData
+        ];
+
+        $input = new Publication();
+        $sut = new Licence();
+
+        $mockLicenceService = m::mock('Common\Service\Data\Licence');
+        $mockLicenceService->shouldReceive('fetchLicenceData')->andReturn($licenceData);
+
+        $mockServiceManager = m::mock('\Zend\ServiceManager\ServiceManager');
+        $mockServiceManager->shouldReceive('get')->with('\Common\Service\Data\Licence')->andReturn($mockLicenceService);
+
+        $sut->setServiceLocator($mockServiceManager);
+
+        $output = $sut->filter($input);
+
+        $this->assertEquals($output->getArrayCopy(), $expectedOutput);
+    }
+
+    public function filterProvider()
+    {
+        return [
+            ['lcat_gv','A&D'],
+            ['lcat_psv','N&P']
+        ];
+    }
+}
