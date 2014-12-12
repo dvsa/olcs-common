79b80d1d449af16b240cb5ff6a2c77c00ee8fb91
Nick Payne <nick@kurai.co.uk>
2014-12-12 07:47:26 +0000

    add entity processing service

---
+++ Common/src/Common/Service/Processing/EntityProcessingService.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Entity Processing Service
+ *
+ * @author Nick Payne <nick.payne@valtech.co.uk>
+ */
+namespace Common\Service\Processing;
+
+use Common\Service\Data\CategoryDataService;
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
+use Zend\ServiceManager\ServiceLocatorAwareTrait;
+
+/**
+ * Entity Processing Service
+ *
+ * @author Nick Payne <nick.payne@valtech.co.uk>
+ */
+class EntityProcessingService implements ServiceLocatorAwareInterface
+{
+    use ServiceLocatorAwareTrait;
+
+    private $serviceMap = [
+        CategoryDataService::CATEGORY_APPLICATION       => 'Licence',
+        CategoryDataService::CATEGORY_BUS_REGISTRATION  => 'BusRegistration',
+        CategoryDataService::CATEGORY_COMPLIANCE        => 'Cases',
+        CategoryDataService::CATEGORY_LICENSING         => 'Licence',
+        CategoryDataService::CATEGORY_ENVIRONMENTAL     => '???',
+        CategoryDataService::CATEGORY_IRFO              => 'IRFO',
+        CategoryDataService::CATEGORY_TRANSPORT_MANAGER => 'TM'
+    ];
+
+    public function findEntityForCategory($category, $identifier)
+    {
+        return $this->getServiceLocator()
+            ->get('Entity\\' . $this->serviceMap[$category])
+            ->findByIdentifier($identifier);
+    }
+}
