00522abfc86e0efd9141e0fcca554de7cc0e1107
Nick Payne <nick@kurai.co.uk>
2014-12-19 11:05:45 +0000

    temporarily short-circuit LVA modal behaviour if not on selfserve

--- Common/src/Common/assets/js/inline/lva-crud.js
+++ Common/src/Common/assets/js/inline/lva-crud.js
@@ -1,4 +1,11 @@
 OLCS.ready(function() {
   "use strict";
+  // @TODO: there is a later story to modalise LVA behaviour
+  // internally. For now, the easiest way to suppress it
+  // is to short circuit based on the app class. Not pretty,
+  // but will be removed early 2015
+  if (document.body.className.search("selfserve") === -1) {
+    return;
+  }
   OLCS.crudTableHandler();
 });
