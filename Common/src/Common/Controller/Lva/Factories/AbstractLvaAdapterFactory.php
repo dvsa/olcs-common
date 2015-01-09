6a8772be82e6f95aabbbacb9b04d0b2d0c6af570
Rob Caiger <rob@clocal.co.uk>
2015-01-09 13:13:45 +0000

    More unit tests

--- Common/src/Common/Controller/Lva/Factories/AbstractLvaAdapterFactory.php
+++ Common/src/Common/Controller/Lva/Factories/AbstractLvaAdapterFactory.php
@@ -9,7 +9,6 @@ namespace Common\Controller\Lva\Factories;
 
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
-use Common\Controller\Lva\AbstractController;
 
 /**
  * Abstract Lva Adapter Factory
@@ -29,10 +28,6 @@ abstract class AbstractLvaAdapterFactory implements FactoryInterface
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
         $serviceClass = 'Common\Controller\Lva\Adapters\\' . $this->adapter;
-
-        if (!class_exists($serviceClass)) {
-            throw new \RuntimeException('Lva adapter not found: ' . $this->adapter);
-        }
 
         return new $serviceClass();
     }
