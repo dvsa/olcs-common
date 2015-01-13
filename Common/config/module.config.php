a9aee24aee2dd9380bee55b7d8c3810c4502c2fe
Shaun Lizzio <shaun@lizzio.co.uk>
2015-01-13 09:10:11 +0000

    add service

--- Common/config/module.config.php
+++ Common/config/module.config.php
@@ -277,6 +277,8 @@ return array(
             'Common\Service\Data\PiVenue' => 'Common\Service\Data\PiVenue',
             'Common\Service\Data\PiHearing' => 'Common\Service\Data\PiHearing',
             'Common\Service\Data\PublicationLink' => 'Common\Service\Data\PublicationLink',
+            'Common\Service\Data\LicenceOperatingCentre' =>
+                'Common\Service\Data\LicenceOperatingCentre',
         ]
     ],
     'tables' => array(
