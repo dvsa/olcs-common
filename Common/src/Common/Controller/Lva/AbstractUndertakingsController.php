7d3fc15da6f65a73d8055f4129e957251c7302b8
Dan Eggleston <dan@stolenegg.com>
2015-01-23 14:01:11 +0000

    OLCS-2236 moved some logic into abstract controller

--- Common/src/Common/Controller/Lva/AbstractUndertakingsController.php
+++ Common/src/Common/Controller/Lva/AbstractUndertakingsController.php
@@ -24,13 +24,27 @@ abstract class AbstractUndertakingsController extends AbstractController
         $request = $this->getRequest();
 
         if ($request->isPost()) {
-            $this->postSave('undertakings');
-            return $this->completeSection('undertakings');
+            $data = (array)$request->getPost();
+        } else {
+            $data = $this->formatDataForForm($this->getUndertakingsData());
         }
 
-        $form = $this->getForm();
+        $form = $this->getForm()->setData($data);
 
-        $this->alterFormForLva($form);
+        if ($request->isPost()) {
+            if ($form->isValid()) {
+                $this->save($this->formatDataForSave($data));
+                $this->postSave('undertakings');
+                return $this->completeSection('undertakings');
+            } else {
+                // validation failed, we need to lookup application data
+                // but use the POSTed checkbox value to render the form again
+                $confirmed = $data['declarationsAndUndertakings']['confirmation'];
+                $data = $this->formatDataForForm($this->getApplicationData());
+                $data['declarationsAndUndertakings']['confirmation'] = $confirmed;
+                $form->setData($data);
+            }
+        }
 
         return $this->render('undertakings', $form);
     }
@@ -43,5 +57,33 @@ abstract class AbstractUndertakingsController extends AbstractController
     protected function getForm()
     {
         return $this->getServiceLocator()->get('Helper\Form')->createForm('Lva\Undertakings');
+    }
+
+    protected function formatDataForSave($data)
+    {
+        $saveData = $data['declarationsAndUndertakings'];
+        return $saveData;
+    }
+
+
+    /**
+     * Get Application Data
+     *
+     * @return array
+     */
+    protected function getUndertakingsData()
+    {
+        return $this->getServiceLocator()->get('Entity\Application')
+            ->getDataForUndertakings($this->getApplicationId());
+    }
+
+    /**
+     * Save the form data
+     *
+     * @param array $data
+     */
+    protected function save($data)
+    {
+        return $this->getServiceLocator()->get('Entity\Application')->save($data);
     }
 }
