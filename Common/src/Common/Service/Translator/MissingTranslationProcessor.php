cf007ad8bfd920dd3820c5243070efc56f5fe2d5
Dan Eggleston <dan@stolenegg.com>
2015-01-28 09:32:31 +0000

    refactor to simplify MissingTranslationProcessor slightly

--- Common/src/Common/Service/Translator/MissingTranslationProcessor.php
+++ Common/src/Common/Service/Translator/MissingTranslationProcessor.php
@@ -51,25 +51,23 @@ class MissingTranslationProcessor
         $message = $params['message'];
 
         if (preg_match_all('/\{([^\}]+)\}/', $message, $matches)) {
-
             // handles text with translation keys inside curly braces {}
             foreach ($matches[0] as $key => $match) {
                 $message = str_replace($match, $translator->translate($matches[1][$key]), $message);
             }
+            return $message;
+        }
 
-        } else {
-            // handles partials as translations. Note we only try to resolve keys
-            // that match a pattern, to avoid having to run the template resolver
-            // against ALL missing translations
-            if (strpos($message, 'markup-') === 0) {
-                $locale    = $params['locale'];
-                $partial   = $locale . '/' . $message; // e.g. en_GB/my-translation-key
-                $foundPath = $this->resolver->resolve($partial);
-                if ($foundPath !== false) {
-                    $message = $this->renderer->render($partial);
-                }
+        // handles partials as translations. Note we only try to resolve keys
+        // that match a pattern, to avoid having to run the template resolver
+        // against ALL missing translations
+        if (strpos($message, 'markup-') === 0) {
+            $locale    = $params['locale'];
+            $partial   = $locale . '/' . $message; // e.g. en_GB/my-translation-key
+            $foundPath = $this->resolver->resolve($partial);
+            if ($foundPath !== false) {
+                $message = $this->renderer->render($partial);
             }
-
         }
 
         return $message;
