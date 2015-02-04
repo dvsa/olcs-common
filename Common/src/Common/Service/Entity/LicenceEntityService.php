b15f07ca810280a669a7b6e71d8120fab933b634
Dan Eggleston <dan@stolenegg.com>
2015-02-04 17:56:07 +0000

    OLCs-2222 remove unused method

--- Common/src/Common/Service/Entity/LicenceEntityService.php
+++ Common/src/Common/Service/Entity/LicenceEntityService.php
@@ -495,9 +495,4 @@ class LicenceEntityService extends AbstractLvaEntityService
 
         return $return;
     }
-
-    public function getTotalVehicleAuthorisationIncLicence($id, $type = '')
-    {
-        return $this->getTotalVehicleAuthorisation($id, $type);
-    }
 }
