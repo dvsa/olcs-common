11e128145ff34e41822d3fb21d599ce6d5609241
Shaun Lizzio <shaun@lizzio.co.uk>
2015-02-09 11:29:12 +0000

    Added Text1, Text2 and TM data filters for TM Hearing Publication

---
+++ Common/src/Common/Filter/Publication/TmHearingText1.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * Transport Manager Hearing text 1 filter
+ *
+ * @author Shaun Lizzio <shaun.lizzio@valtech.co.uk>
+ */
+namespace Common\Filter\Publication;
+
+/**
+ * TM Hearing text 1 filter
+ *
+ * @author Shaun Lizzio <shaun.lizzio@valtech.co.uk>
+ */
+class TmHearingText1 extends Text1
+{
+    protected $pi = 'TM Public Inquiry (Case ID: %s, Public Inquiry ID: %s) for %s to be held at %s,
+    on %s commencing at %s (Previous
+    Publication %s))';
+
+    /**
+     * @param \Common\Data\Object\Publication  $publication
+     * @return \Common\Data\Object\Publication
+     */
+    public function filter($publication)
+    {
+        $hearingData = $publication->offsetGet('hearingData');
+
+        $hearingText = [];
+
+        $hearingText[] = $this->getOpeningText($publication, $hearingData);
+
+        //previous publication
+        if (isset($hearingData['previousPublication'])) {
+            $hearingText[] = $this->getPreviousPublication($hearingData);
+        }
+
+        //previous hearing, only present on hearing publication, not on decision
+        if (isset($hearingData['previousHearing']) && $hearingData['previousHearing']['isAdjourned']) {
+            $hearingText[] = $this->getPreviousHearing($hearingData);
+        }
+
+        $publication->offsetSet('text1', implode(' ', $hearingText));
+
+        return $publication;
+    }
+
+    /**
+     * @param \Common\Data\Object\Publication $publication
+     * @param array $hearingData
+     * @return string
+     */
+    public function getOpeningText($publication, $hearingData)
+    {
+        $case = $publication->offsetGet('case');
+        $transportManager = $publication->offsetGet('transportManager');
+
+        return sprintf(
+            $this->pi,
+            $case['id'],
+            $hearingData['id'],
+            $transportManager['workCd']['person']['title'] . ' '
+                . $transportManager['workCd']['person']['forename'] . ' '
+                . $transportManager['workCd']['person']['familyName'],
+            $hearingData['piVenueOther'],
+            $hearingData['date'],
+            $hearingData['time'],
+            $hearingData['previousPublication']
+        );
+    }
+}
---
+++ Common/src/Common/Filter/Publication/TransportManager.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Case filter
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+namespace Common\Filter\Publication;
+
+use Common\Exception\ResourceNotFoundException;
+
+/**
+ * TransportManager filter
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+class TransportManager extends AbstractPublicationFilter
+{
+
+    /**
+     * @param \Common\Data\Object\Publication $publication
+     * @return \Common\Data\Object\Publication
+     * @throws ResourceNotFoundException
+     */
+    public function filter($publication)
+    {
+        $case = $publication->offsetGet('case');
+        $tmId = $case['transportManager']['id'];
+        $tmData = $this->getServiceLocator()->get('\Common\Service\Data\TransportManager')->fetchTmData($tmId);
+
+        if (!isset($tmData['id'])) {
+            throw new ResourceNotFoundException('No transport manager found');
+        }
+
+        $newData = [
+            'transportManager' => $tmData,
+        ];
+
+        $publication = $this->mergeData($publication, $newData);
+
+        return $publication;
+    }
+}
