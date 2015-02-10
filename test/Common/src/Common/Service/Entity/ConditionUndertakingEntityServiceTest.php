4d88c997eb19884a7372ed95de8626d3029ffa58
Rob Caiger <rob@clocal.co.uk>
2015-02-10 16:49:40 +0000

    Unit tested entity service

--- test/Common/src/Common/Service/Entity/ConditionUndertakingEntityServiceTest.php
+++ test/Common/src/Common/Service/Entity/ConditionUndertakingEntityServiceTest.php
@@ -7,6 +7,7 @@
  */
 namespace CommonTest\Service\Entity;
 
+use Mockery as m;
 use Common\Service\Entity\ConditionUndertakingEntityService;
 
 /**
@@ -33,6 +34,17 @@ class ConditionUndertakingEntityServiceTest extends AbstractEntityServiceTestCas
         $this->assertEquals('RESPONSE', $this->sut->getCondition($id));
     }
 
+    public function testGetConditionForVariation()
+    {
+        $id = 3;
+        $parentId = 4;
+
+        $this->expectOneRestCall('ConditionUndertaking', 'GET', $id)
+            ->will($this->returnValue('RESPONSE'));
+
+        $this->assertEquals('RESPONSE', $this->sut->getConditionForVariation($id, $parentId));
+    }
+
     public function testGetForApplication()
     {
         $id = 3;
@@ -42,6 +54,28 @@ class ConditionUndertakingEntityServiceTest extends AbstractEntityServiceTestCas
             ->will($this->returnValue($reponse));
 
         $this->assertEquals('RESPONSE', $this->sut->getForApplication($id));
+    }
+
+    public function testGetForVariation()
+    {
+        $id = 3;
+        $licenceId = 5;
+
+        $reponse = ['Results' => 'RESPONSE'];
+
+        $mockApplication = m::mock();
+        $this->sm->setService('Entity\Application', $mockApplication);
+
+        $mockApplication->shouldReceive('getLicenceIdForApplication')
+            ->with($id)
+            ->andReturn($licenceId);
+
+        $query = [['application' => $id, 'licence' => $licenceId], 'limit' => 'all'];
+
+        $this->expectOneRestCall('ConditionUndertaking', 'GET', $query)
+            ->will($this->returnValue($reponse));
+
+        $this->assertEquals('RESPONSE', $this->sut->getForVariation($id));
     }
 
     public function testGetForLicence()
