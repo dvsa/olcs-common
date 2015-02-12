58984fadc3cd556957888eb1027fe354385b29ba
Dan Eggleston <dan@stolenegg.com>
2015-02-12 11:23:05 +0000

    OLCS-2222 move static rates to constants

--- Common/src/Common/Controller/Lva/Adapters/AbstractFinancialEvidenceAdapter.php
+++ Common/src/Common/Controller/Lva/Adapters/AbstractFinancialEvidenceAdapter.php
@@ -17,7 +17,19 @@ use Common\Service\Entity\LicenceEntityService as Licence;
  */
 abstract class AbstractFinancialEvidenceAdapter extends AbstractControllerAwareAdapter implements
     FinancialEvidenceAdapterInterface
-{
+    {
+
+    // these values will be retrieved via SystemParameterEntityService
+    // following https://jira.i-env.net/browse/OLCS-6988
+    const RATE_GOODS_FIRST_STANDARD        = 7000;
+    const RATE_GOODS_FIRST_RESTRICTED      = 3100;
+    const RATE_GOODS_ADDITIONAL_STANDARD   = 3900;
+    const RATE_GOODS_ADDITIONAL_RESTRICTED = 1700;
+    const RATE_PSV_FIRST_STANDARD          = 8000;
+    const RATE_PSV_FIRST_RESTRICTED        = 4100;
+    const RATE_PSV_ADDITIONAL_STANDARD     = 4900;
+    const RATE_PSV_ADDITIONAL_RESTRICTED   = 2700;
+
     /**
      * @param int $id
      * @return array
@@ -69,21 +81,21 @@ abstract class AbstractFinancialEvidenceAdapter extends AbstractControllerAwareA
      * @param string $goodsOrPsv
      * @return int
      *
-     * @todo these will become a backend lookup following https://jira.i-env.net/browse/OLCS-6988
+     * @todo these will become a system parameter lookup following https://jira.i-env.net/browse/OLCS-6988
      */
     public function getFirstVehicleRate($licenceType, $goodsOrPsv)
     {
         switch ($licenceType) {
             case Licence::LICENCE_TYPE_RESTRICTED:
                 if ($goodsOrPsv === Licence::LICENCE_CATEGORY_PSV) {
-                    return 4100;
+                    return self::RATE_PSV_FIRST_RESTRICTED;
                 }
-                return 3100;
+                return self::RATE_GOODS_FIRST_RESTRICTED;
             default:
                 if ($goodsOrPsv === Licence::LICENCE_CATEGORY_PSV) {
-                    return 8000;
+                    return self::RATE_PSV_FIRST_STANDARD;
                 }
-                return 7000;
+                return self::RATE_GOODS_FIRST_STANDARD;
         }
     }
 
@@ -97,14 +109,14 @@ abstract class AbstractFinancialEvidenceAdapter extends AbstractControllerAwareA
         switch ($licenceType) {
             case Licence::LICENCE_TYPE_RESTRICTED:
                 if ($goodsOrPsv === Licence::LICENCE_CATEGORY_PSV) {
-                    return 2700;
+                    return self::RATE_PSV_ADDITIONAL_RESTRICTED;
                 }
-                return 1700;
+                return self::RATE_GOODS_ADDITIONAL_RESTRICTED;
             default:
                 if ($goodsOrPsv === Licence::LICENCE_CATEGORY_PSV) {
-                    return 4900;
+                    return self::RATE_PSV_ADDITIONAL_STANDARD;
                 }
-                return 3900;
+                return self::RATE_GOODS_ADDITIONAL_STANDARD;
         }
     }
 }
