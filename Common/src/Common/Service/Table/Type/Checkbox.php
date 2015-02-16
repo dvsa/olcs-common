e445b35bffce2db6c78b6adf850414a35656d7b0
Rob Caiger <rob@clocal.co.uk>
2015-02-16 09:07:13 +0000

    Fixed issued raised in OLCS-6953

--- Common/src/Common/Service/Table/Type/Checkbox.php
+++ Common/src/Common/Service/Table/Type/Checkbox.php
@@ -6,7 +6,6 @@
  * @author Rob Caiger <rob@clocal.co.uk>
  * @author Alex Peshkov <alex.peshkov@valtech.co.uk>
  */
-
 namespace Common\Service\Table\Type;
 
 /**
@@ -17,31 +16,4 @@ namespace Common\Service\Table\Type;
 class Checkbox extends Selector
 {
     protected $format = '<input type="checkbox" name="%s[]" value="%s" %s />';
-
-    /**
-     * Render the checkbox
-     *
-     * @param array $data
-     * @param array $column
-     * @param string $formattedContent
-     *
-     * @return string
-     */
-    public function render($data, $column, $formattedContent = null)
-    {
-        $fieldset = $this->getTable()->getFieldset();
-        $name = 'id';
-
-        if (!empty($fieldset)) {
-            $name = $fieldset . '[id]';
-        }
-
-        $attributes = [];
-
-        if (isset($column['disableIfRowIsDisabled']) && $this->getTable()->isRowDisabled($data)) {
-            $attributes[] = 'disabled="disabled"';
-        }
-
-        return sprintf($this->format, $name, $data['id'], implode(' ', $attributes));
-    }
 }
