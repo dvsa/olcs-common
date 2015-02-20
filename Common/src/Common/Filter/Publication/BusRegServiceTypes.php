191134b1e7613cab479eb4f73f58ca43fea6def1
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-02-20 16:24:41 +0000

    Filter for bus reg service types

---
+++ Common/src/Common/Filter/Publication/BusRegServiceTypes.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Bus Registration Service Types filter
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+namespace Common\Filter\Publication;
+
+/**
+ * Bus Registration Service Types filter
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+class BusRegServiceTypes extends AbstractPublicationFilter
+{
+    /**
+     * @param \Common\Data\Object\Publication $publication
+     * @return \Common\Data\Object\Publication
+     */
+    public function filter($publication)
+    {
+        $busReg = $publication->offsetGet('busRegData');
+
+        $serviceTypes = [];
+
+        if (!empty($busReg['busServiceTypes'])) {
+            foreach($busReg['busServiceTypes'] as $serviceType) {
+                $serviceTypes[] = $serviceType['description'];
+            }
+        }
+
+        $newData = [
+            'busServiceTypes' => implode(', ', $serviceTypes),
+        ];
+
+        $publication = $this->mergeData($publication, $newData);
+
+        return $publication;
+    }
+}
