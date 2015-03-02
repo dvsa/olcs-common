5f750dac91d1f82e117f63152e6ef6b882e86bbc
Joshua Curtis <josh.curtis@valtech.co.uk>
2015-03-02 09:02:10 +0000

    Amended delete message in the language file to ensure the correct message is shown.

--- Common/config/language/en_GB.php
+++ Common/config/language/en_GB.php
@@ -969,8 +969,7 @@ return array(
     'internal.transport-manager.employment.form.position' => 'Position held',
     'internal.transport-manager.employment.save' => 'Save',
     'internal.transport-manager.employments.table' => 'Other employment',
-    'internal.transport-manager.previous-history.delete-question' => 'Are you sure you want to permanently delete this
-        record?',
+    'internal.transport-manager.previous-history.delete-question' => 'Are you sure you want to permanently delete the selected record(s)?',
     'internal.transport-manager.previous-history.deleted-message' => 'Deleted successfully',
     'internal.transport-manager.previous-licences.form.holder-name' => 'Licence holder name',
     'internal.transport-manager.previous-licences.form.lic-no' => 'Licence no.',
