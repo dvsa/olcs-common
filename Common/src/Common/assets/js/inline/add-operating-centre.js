0f86528de803d6df085ea3e5089e243ee0de0f47
Nick Payne <nick@kurai.co.uk>
2015-03-03 16:04:56 +0000

    refactor inline OC JS to avoid stackoverflow issues

--- Common/src/Common/assets/js/inline/add-operating-centre.js
+++ Common/src/Common/assets/js/inline/add-operating-centre.js
@@ -13,6 +13,7 @@ $(function() {
   }
 
   OLCS.cascadeForm({
+    form: "#OperatingCentre",
     cascade: false,
     rulesets: {
       "advertisements": {
@@ -30,11 +31,28 @@ $(function() {
     }
   });
 
-  OLCS.eventEmitter.on("show:advertisements:*", function() {
-    F.selectRadio("advertisements", "adPlaced", "Y");
-  });
+  /**
+   * lo-fi mechanism to detect if we're a variation or not. No point
+   * binding a load of stuff when it's totally irrelevant.
+   */
+  if (vehicles.data("current")) {
+    var showAds = OLCS.eventEmitter.on("show:advertisements:*", function() {
+      F.selectRadio("advertisements", "adPlaced", "Y");
+    });
 
-  OLCS.eventEmitter.on("hide:advertisements:*", function() {
-    F.selectRadio("advertisements", "adPlaced", "N");
-  });
+    var hideAds = OLCS.eventEmitter.on("hide:advertisements:*", function() {
+      F.selectRadio("advertisements", "adPlaced", "N");
+    });
+
+    var showModal = OLCS.eventEmitter.on("show:modal", function() {
+      F.selectRadio("advertisements", "adPlaced", "N");
+    });
+
+    var hideModal = OLCS.eventEmitter.on("hide:modal", function() {
+      OLCS.eventEmitter.off("show:advertisements:*", showAds);
+      OLCS.eventEmitter.off("hide:advertisements:*", hideAds);
+      OLCS.eventEmitter.off("show:modal", showModal);
+      OLCS.eventEmitter.off("hide:modal", hideModal);
+    });
+  }
 });
