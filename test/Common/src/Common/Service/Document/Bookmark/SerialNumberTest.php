58ed35c71bd98978aa5a38f562dd1bff0d78131e
Joshua Curtis <josh.curtis@valtech.co.uk>
2015-03-04 14:02:23 +0000

    Added the serial number bookmark in.

---
+++ test/Common/src/Common/Service/Document/Bookmark/SerialNumberTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace CommonTest\Service\Document\Bookmark;
+
+use Common\Service\Document\Bookmark\SerialNumber;
+
+/**
+ * Class SerialNumberTest
+ *
+ * Test the serial number bookmark.
+ *
+ * @package CommonTest\Service\Document\Bookmark
+ */
+class SerialNumberTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetQueryContainsExpectedKeys()
+    {
+        $bookmark = new SerialNumber();
+        $query = $bookmark->getQuery(['licence' => 123]);
+
+        $this->assertEquals('Licence', $query['service']);
+        $this->assertEquals(['id' => 123], $query['data']);
+    }
+
+    public function testRender()
+    {
+        $bookmark = new SerialNumber();
+        $bookmark->setData(
+            [
+                'licNo' => 123
+            ]
+        );
+
+        // The date function is used here (presumably) because there is no easy way to get
+        // a reference to the service container.
+        $this->assertEquals(
+            '123 ' . date("d/m/Y", strtotime("+0 days")),
+            $bookmark->render()
+        );
+    }
+}
