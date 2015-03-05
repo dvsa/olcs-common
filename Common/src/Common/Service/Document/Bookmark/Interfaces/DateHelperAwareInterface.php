70406f4184bdbdd6fef5eb9e3d177f068bbfe904
Nick Payne <nick@kurai.co.uk>
2015-03-05 15:30:59 +0000

    CS fix

--- Common/src/Common/Service/Document/Bookmark/Interfaces/DateHelperAwareInterface.php
+++ Common/src/Common/Service/Document/Bookmark/Interfaces/DateHelperAwareInterface.php
@@ -4,6 +4,11 @@ namespace Common\Service\Document\Bookmark\Interfaces;
 
 use Common\Service\Helper\DateHelperService;
 
+/**
+ * Date Helper Aware Interface
+ *
+ * @author Nick Payne <nick.payne@valtech.co.uk>
+ */
 interface DateHelperAwareInterface
 {
     public function setDateHelper(DateHelperService $helper);
