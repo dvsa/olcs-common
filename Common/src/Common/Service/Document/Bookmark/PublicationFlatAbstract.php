8bfa4254a13af8a2d45bd83e7966b30572e3747c
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-03-09 12:09:22 +0100

    Publication link section bookmarks

---
+++ Common/src/Common/Service/Document/Bookmark/PublicationFlatAbstract.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * PublicationFlatAbstract
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+namespace Common\Service\Document\Bookmark;
+
+/**
+ * PublicationFlatAbstract
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+abstract class PublicationFlatAbstract extends SingleValueAbstract
+{
+    const SERVICE = 'Publication'; // example
+    const SRCH_VAL_KEY = 'publicationId'; // example
+}
