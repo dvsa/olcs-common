c2ad0a629a416635748ddd4c91f521bc501abb51
Alex P <alex.peshkov@valtech.co.uk>
2015-03-18 16:18:37 +0000

    OLCS-7942 Amend vehicle dates

--- Common/src/Common/Controller/Lva/Adapters/LicenceVehiclesPsvAdapter.php
+++ Common/src/Common/Controller/Lva/Adapters/LicenceVehiclesPsvAdapter.php
@@ -26,4 +26,68 @@ class LicenceVehiclesPsvAdapter extends AbstractAdapter implements VehiclesAdapt
     {
         return $this->getServiceLocator()->get('Entity\Licence')->getVehiclesPsvData($id);
     }
+
+    /**
+     * Format removed and specified dates if needed
+     *
+     * @param array $licenceVehicle
+     * @return array
+     */
+    public function maybeFormatRemovedAndSpecifiedDates($licenceVehicle)
+    {
+        if (isset($licenceVehicle['specifiedDate'])) {
+            if (checkdate(
+                (int)$licenceVehicle['specifiedDate']['month'],
+                (int)$licenceVehicle['specifiedDate']['day'],
+                (int)$licenceVehicle['specifiedDate']['year']
+            )) {
+                $licenceVehicle['specifiedDate'] = sprintf(
+                    '%s-%s-%s',
+                    $licenceVehicle['specifiedDate']['year'],
+                    $licenceVehicle['specifiedDate']['month'],
+                    $licenceVehicle['specifiedDate']['day']
+                );
+            } else {
+                unset($licenceVehicle['specifiedDate']);
+            }
+        }
+        if (isset($licenceVehicle['removalDate'])) {
+            if (checkdate(
+                (int)$licenceVehicle['removalDate']['month'],
+                (int)$licenceVehicle['removalDate']['day'],
+                (int)$licenceVehicle['removalDate']['year']
+            )) {
+                $licenceVehicle['removalDate'] = sprintf(
+                    '%s-%s-%s',
+                    $licenceVehicle['removalDate']['year'],
+                    $licenceVehicle['removalDate']['month'],
+                    $licenceVehicle['removalDate']['day']
+                );
+            } else {
+                unset($licenceVehicle['removalDate']);
+            }
+        }
+        return $licenceVehicle;
+    }
+
+    /**
+     * Disable removed and specified dates if needed
+     *
+     * @param Zend\Form\Form $form
+     * @param Common\Service\Helper\FormHelper
+     */
+    public function maybeDisableRemovedAndSpecifiedDates($form, $formHelper)
+    {
+    }
+
+    /**
+     * Unset specified date if needed
+     *
+     * @param array $data
+     * @return array
+     */
+    public function maybeUnsetSpecifiedDate($data)
+    {
+        return $data;
+    }
 }
