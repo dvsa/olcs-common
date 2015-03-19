9a8f36dd66bd1d4f117b0d4914a89ce45b342227
Dan Eggleston <dan@stolenegg.com>
2015-03-19 17:10:31 +0000

    OLCS-8000 unit test code that is moved into adapter

--- test/Common/src/Common/Controller/Lva/Adapters/ApplicationVehiclesPsvAdapterTest.php
+++ test/Common/src/Common/Controller/Lva/Adapters/ApplicationVehiclesPsvAdapterTest.php
@@ -11,6 +11,7 @@ use CommonTest\Bootstrap;
 use Mockery as m;
 use Mockery\Adapter\Phpunit\MockeryTestCase;
 use Common\Controller\Lva\Adapters\ApplicationVehiclesPsvAdapter;
+use Common\Service\Entity\VehicleEntityService;
 
 /**
  * Application Vehicles Psv Adapter Test
@@ -28,6 +29,24 @@ class ApplicationVehiclesPsvAdapterTest extends MockeryTestCase
 
         $this->sut = new ApplicationVehiclesPsvAdapter();
         $this->sut->setServiceLocator($this->sm);
+
+        // stub the mapping between type and psv type that is now in entity service
+        $map = [
+            'small'  => 'vhl_t_a',
+            'medium' => 'vhl_t_b',
+            'large'  => 'vhl_t_c',
+        ];
+        $this->sm->setService(
+            'Entity\Vehicle',
+            m::mock()
+                ->shouldReceive('getTypeFromPsvType')
+                ->andReturnUsing(
+                    function($type) use ($map) {
+                        return array_search($type, $map);
+                    }
+                )
+                ->getMock()
+        );
     }
 
     public function testGetVehiclesData()
@@ -94,5 +113,176 @@ class ApplicationVehiclesPsvAdapterTest extends MockeryTestCase
             ['licence-vehicle' => []],
             $this->sut->maybeUnsetSpecifiedDate(['licence-vehicle' => ['specifiedDate' => 'date']])
         );
+    }
+
+    /**
+     * Test that flash warning message is added when we render the page and
+     * an authority for individual vehicle type is exceeded. (This can only
+     * happen when vehicles were previously added but then the Operating Centre
+     * authority is decreased)
+     */
+    public function testWarnIfAuthorityExceeded()
+    {
+        $id = 69;
+
+        $entityData = [
+            'id'                    => $id,
+            'hasEnteredReg'         => 'Y',
+            'totAuthVehicles'       => 5,
+            'totAuthSmallVehicles'  => 2,
+            'totAuthMediumVehicles' => 3,
+            'totAuthLargeVehicles'  => 0,
+        ];
+
+        $vehiclesData = [
+            ['vehicle' => ['psvType' => ['id' => VehicleEntityService::PSV_TYPE_SMALL]]],
+            ['vehicle' => ['psvType' => ['id' => VehicleEntityService::PSV_TYPE_SMALL]]],
+            ['vehicle' => ['psvType' => ['id' => VehicleEntityService::PSV_TYPE_MEDIUM]]],
+            ['vehicle' => ['psvType' => ['id' => VehicleEntityService::PSV_TYPE_MEDIUM]]],
+            ['vehicle' => ['psvType' => ['id' => VehicleEntityService::PSV_TYPE_LARGE]]],
+        ];
+
+        $this->sm->setService(
+            'Entity\Application',
+            m::mock()
+                ->shouldReceive('getDataForVehiclesPsv')
+                    ->with($id)
+                    ->once()
+                    ->andReturn($entityData)
+                ->getMock()
+        );
+
+        $this->sm->setService(
+            'Entity\Licence',
+            m::mock()
+                ->shouldReceive('getVehiclesPsvDataForApplication')
+                    ->with($id)
+                    ->once()
+                    ->andReturn($vehiclesData)
+                ->getMock()
+        );
+
+        $this->sm->setService(
+            'Helper\FlashMessenger',
+            m::mock()
+                ->shouldReceive('addCurrentWarningMessage')
+                    ->once()
+                    ->with('more-vehicles-than-large-authorisation')
+                ->shouldReceive('addWarningMessage')
+                    ->never()
+                ->getMock()
+        );
+
+        $psvTypes = [
+            VehicleEntityService::PSV_TYPE_SMALL,
+            VehicleEntityService::PSV_TYPE_MEDIUM,
+            VehicleEntityService::PSV_TYPE_LARGE,
+        ];
+        $this->sut->warnIfAuthorityExceeded($id, $psvTypes, false);
+
+    }
+
+    /**
+     * Test that flash warning messages are not added if user selects they are
+     * not submitting vehicle details
+     */
+    public function testWarnIfAuthorityExceededNotSubmitting()
+    {
+        $id = 69;
+
+        $entityData = [
+            'id'                    => $id,
+            'hasEnteredReg'         => 'N',
+        ];
+
+        $this->sm->setService(
+            'Entity\Application',
+            m::mock()
+                ->shouldReceive('getDataForVehiclesPsv')
+                    ->with($id)
+                    ->once()
+                    ->andReturn($entityData)
+                ->getMock()
+        );
+
+        $this->sm->setService(
+            'Helper\FlashMessenger',
+            m::mock()
+                ->shouldReceive('addWarningMessage')->never()
+                ->shouldReceive('addCurrentWarningMessage')->never()
+                ->getMock()
+        );
+        $psvTypes = [
+            VehicleEntityService::PSV_TYPE_SMALL,
+            VehicleEntityService::PSV_TYPE_MEDIUM,
+            VehicleEntityService::PSV_TYPE_LARGE,
+        ];
+        $this->sut->warnIfAuthorityExceeded($id, $psvTypes, true);
+        $this->sut->warnIfAuthorityExceeded($id, $psvTypes, false);
+    }
+
+    /**
+     * Test that correct flash warning message is added on save when an
+     * authority is exceeded
+     */
+    public function testAddWarningsIfAuthorityExceededRedirect()
+    {
+        $id = 69;
+
+        $entityData = [
+            'id'                    => $id,
+            'hasEnteredReg'         => 'Y',
+            'totAuthVehicles'       => 5,
+            'totAuthSmallVehicles'  => 1,
+            'totAuthMediumVehicles' => 3,
+            'totAuthLargeVehicles'  => 1,
+        ];
+
+        $vehiclesData = [
+            ['vehicle' => ['psvType' => ['id' => VehicleEntityService::PSV_TYPE_SMALL]]],
+            ['vehicle' => ['psvType' => ['id' => VehicleEntityService::PSV_TYPE_SMALL]]],
+            ['vehicle' => ['psvType' => ['id' => VehicleEntityService::PSV_TYPE_LARGE]]],
+            ['vehicle' => ['psvType' => ['id' => VehicleEntityService::PSV_TYPE_LARGE]]],
+        ];
+
+        $this->sm->setService(
+            'Entity\Application',
+            m::mock()
+                ->shouldReceive('getDataForVehiclesPsv')
+                    ->with($id)
+                    ->once()
+                    ->andReturn($entityData)
+                ->getMock()
+        );
+
+        $this->sm->setService(
+            'Entity\Licence',
+            m::mock()
+                ->shouldReceive('getVehiclesPsvDataForApplication')
+                    ->with($id)
+                    ->once()
+                    ->andReturn($vehiclesData)
+                ->getMock()
+        );
+
+        $this->sm->setService(
+            'Helper\FlashMessenger',
+            m::mock()
+                ->shouldReceive('addWarningMessage')
+                    ->once()
+                    ->with('more-vehicles-than-large-authorisation')
+                ->shouldReceive('addWarningMessage')
+                    ->once()
+                    ->with('more-vehicles-than-small-authorisation')
+                ->shouldReceive('addCurrentWarningMessage')->never()
+                ->getMock()
+        );
+
+        $psvTypes = [
+            VehicleEntityService::PSV_TYPE_SMALL,
+            VehicleEntityService::PSV_TYPE_MEDIUM,
+            VehicleEntityService::PSV_TYPE_LARGE,
+        ];
+        $this->sut->warnIfAuthorityExceeded($id, $psvTypes, true);
     }
 }
