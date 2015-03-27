44198660bfea46cb7094b0e3fec2e10cd1e0c47b
Rob Caiger <rob@clocal.co.uk>
2015-03-27 13:34:23 +0000

    Fixed notes tables

--- Common/src/Common/Service/Table/TableBuilder.php
+++ Common/src/Common/Service/Table/TableBuilder.php
@@ -1109,9 +1109,13 @@ class TableBuilder implements ServiceManager\ServiceLocatorAwareInterface
             sort($this->settings['paginate']['limit']['options']);
         }
 
+        /**
+        Temporarily removed this, as if someone has set the limit to be more than the total, they would no longer see
+         the limit options to reduce
         if ($this->total <= min($this->settings['paginate']['limit']['options'])) {
             return '';
         }
+        */
 
         return $this->renderLayout('pagination');
     }
