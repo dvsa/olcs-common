9044b99a1559d606449189f3c5822ae3e8e971a6
Nick Payne <nick@kurai.co.uk>
2015-03-30 08:05:45 +0100

    opt in community licence and OC JS to OLCS.complexPredicate

--- Common/src/Common/assets/js/inline/lva-crud-delta.js
+++ Common/src/Common/assets/js/inline/lva-crud-delta.js
@@ -3,45 +3,29 @@ OLCS.ready(function() {
 
   var tableSelector = "form [data-group*='table']";
 
-  function checkAction(allowedActions, maxLength) {
-
-    if (maxLength === null) {
-      maxLength = Infinity;
-    }
-
-    return function (length, enable, selectedInputs) {
-
-      if (length < 1 || length > maxLength) {
-        return enable(false);
-      }
-
-      var actions = $.map(selectedInputs, function(input) {
-        return $(input).data("action");
-      });
-
-      enable(
-        // as long as we don't have any actions NOT in the allowed list; go for it
-        $(actions).not(allowedActions).length === 0
-      );
-    };
-  }
-
   OLCS.conditionalButton({
     container: tableSelector,
     label: "Edit",
-    predicate: checkAction(["E", "U", "A"], 1)
+    predicate: {
+      max: 1,
+      allow: ["E", "U", "A"]
+    }
   });
 
   OLCS.conditionalButton({
     container: tableSelector,
     label: "Delete",
-    predicate: checkAction(["A", "E", "U"])
+    predicate: {
+      allow: ["A", "E", "U"]
+    }
   });
 
   OLCS.conditionalButton({
     container: tableSelector,
     label: "Restore",
-    predicate: checkAction(["C", "D"])
+    predicate: {
+      allow: ["C", "D"]
+    }
   });
 
   // @TODO: there is a later story to modalise LVA behaviour
