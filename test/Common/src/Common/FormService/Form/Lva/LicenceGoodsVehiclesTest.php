a7b629f7fbba4e6f6be5ab00340a63a6960bbb2a
Rob Caiger <rob@clocal.co.uk>
2015-04-02 11:26:59 +0100

    Added more unit tests

---
+++ test/Common/src/Common/FormService/Form/Lva/LicenceGoodsVehiclesTest.php
@@ -0,0 +1,95 @@
+<?php
+
+/**
+ * Licence Goods Vehicles Test
+ *
+ * @author Rob Caiger <rob@clocal.co.uk>
+ */
+namespace CommonTest\FormService\Form\Lva;
+
+use Mockery as m;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Common\FormService\Form\Lva\LicenceGoodsVehicles;
+use CommonTest\Bootstrap;
+
+/**
+ * Licence Goods Vehicles Test
+ *
+ * @author Rob Caiger <rob@clocal.co.uk>
+ */
+class LicenceGoodsVehiclesTest extends MockeryTestCase
+{
+    protected $sut;
+
+    protected $formHelper;
+
+    protected $formService;
+
+    protected $sm;
+
+    public function setUp()
+    {
+        $this->sm = Bootstrap::getServiceManager();
+        $this->formHelper = m::mock('\Common\Service\Helper\FormHelperService');
+        $this->formService = m::mock('\Common\FormService\FormServiceManager')->makePartial();
+
+        $this->sut = new LicenceGoodsVehicles();
+        $this->sut->setFormHelper($this->formHelper);
+        $this->sut->setServiceLocator($this->sm);
+        $this->sut->setFormServiceLocator($this->formService);
+    }
+
+    public function testGetForm()
+    {
+        // Params
+        $mockTable = m::mock('\Common\Service\Table\TableBuilder');
+        $isCrudPressed = true;
+
+        // Mocks
+        $mockForm = m::mock();
+        $mockTableElement = m::mock('\Zend\Form\Fieldset');
+        $mockValidator = m::mock();
+
+        $this->sm->setService('oneRowInTablesRequired', $mockValidator);
+
+        // Expectations
+        $this->formHelper->shouldReceive('createForm')
+            ->with('Lva\GoodsVehicles')
+            ->andReturn($mockForm)
+            ->shouldReceive('populateFormTable')
+            ->with($mockTableElement, $mockTable);
+
+        $mockForm->shouldReceive('get')
+            ->with('table')
+            ->andReturn($mockTableElement);
+
+        $mockForm->shouldReceive('getInputFilter->get->get->getValidatorChain->attach')
+            ->with($mockValidator);
+
+        // <<--- START SUT::alterForm
+        $mockLicence = m::mock('\Common\FormService\FormServiceInterface');
+        $this->formService->setService('lva-licence', $mockLicence);
+
+        $mockLicenceVariationVehicles = m::mock('\Common\FormService\FormServiceInterface');
+        $this->formService->setService('lva-licence-variation-vehicles', $mockLicenceVariationVehicles);
+
+        $mockLicence->shouldReceive('alterForm')
+            ->with($mockForm);
+
+        $mockLicenceVariationVehicles->shouldReceive('alterForm')
+            ->with($mockForm);
+        // <<--- END SUT::alterForm
+
+        $mockTableElement->shouldReceive('get->getValue')
+            ->andReturn(10);
+
+        $mockValidator->shouldReceive('setRows')
+            ->with([10])
+            ->shouldReceive('setCrud')
+            ->with(true);
+
+        $form = $this->sut->getForm($mockTable, $isCrudPressed);
+
+        $this->assertSame($mockForm, $form);
+    }
+}
