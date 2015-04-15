ac6a074cbbf76e64a7d67be141f565689d18ecb0
Joshua Curtis <josh.curtis@valtech.co.uk>
2015-04-15 09:28:04 +0100

    Updated with comments from Rob

--- Common/src/Common/Service/Table/Formatter/OcComplaints.php
+++ Common/src/Common/Service/Table/Formatter/OcComplaints.php
@@ -17,7 +17,7 @@ namespace Common\Service\Table\Formatter;
 class OcComplaints implements FormatterInterface
 {
     /**
-     * Get the comaplaints for the operating centre and return them.
+     * Get the complaints for the operating centre and return them.
      *
      * @param array $data The row data.
      * @param array $column The column data.
@@ -27,9 +27,6 @@ class OcComplaints implements FormatterInterface
      */
     public static function format($data, $column = array(), $sm = null)
     {
-        $complaints = $sm->get('Entity\OcComplaints')
-            ->getCountComplaintsForOpCentre($data['operatingCentre']['id']);
-
-        return $complaints;
+        return count($data['operatingCentre']['ocComplaints']);
     }
 }
