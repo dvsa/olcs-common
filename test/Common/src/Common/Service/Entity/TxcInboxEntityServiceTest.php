83107b4b50d7b77e267e3465702d2b71351fc390
Shaun Lizzio <shaun@lizzio.co.uk>
2015-04-21 15:35:10 +0100

    Added UT for TxcInbox with LA and Organisation

--- test/Common/src/Common/Service/Entity/TxcInboxEntityServiceTest.php
+++ test/Common/src/Common/Service/Entity/TxcInboxEntityServiceTest.php
@@ -24,13 +24,16 @@ class TxcInboxEntityServiceTest extends AbstractEntityServiceTestCase
     }
 
     /**
+     * Test fetchDocuments for no localAuthority or organisation
      * @group entity_services
      */
-    public function testFetchBusRegDocuments()
+    public function testFetchBusRegDocumentsNoLocalAuthority()
     {
         $params = [
             'busReg' => 123,
-            'localAuthority' => 'NULL'
+            'sort' => 'createdOn',
+            'order' => 'DESC'
+
         ];
 
         $mockDocs = [
@@ -48,6 +51,79 @@ class TxcInboxEntityServiceTest extends AbstractEntityServiceTestCase
             ->will($this->returnValue($mockDocs));
 
         $result = $this->sut->fetchBusRegDocuments($params['busReg']);
+
+        $this->assertCount(3, $result);
+        $this->assertTrue(in_array('RD', $result));
+        $this->assertTrue(in_array('PD', $result));
+        $this->assertTrue(in_array('ZD', $result));
+    }
+
+    /**
+     * Test fetchDocuments for localAuthority set
+     *
+     * @group entity_services
+     */
+    public function testFetchBusRegDocumentsForLocalAuthority()
+    {
+        $params = [
+            'busReg' => 123,
+            'localAuthority' => 4,
+            'sort' => 'createdOn',
+            'order' => 'DESC',
+            'fileRead' => 0
+        ];
+
+        $mockDocs = [
+            'Count' => 3,
+            'Results' => [
+                0 => [
+                    'routeDocument' => 'RD',
+                    'pdfDocument' => 'PD',
+                    'zipDocument' => 'ZD'
+                ]
+            ]
+        ];
+
+        $this->expectOneRestCall('TxcInbox', 'GET', $params)
+            ->will($this->returnValue($mockDocs));
+
+        $result = $this->sut->fetchBusRegDocuments($params['busReg'], ['id' => $params['localAuthority']]);
+
+        $this->assertCount(3, $result);
+        $this->assertTrue(in_array('RD', $result));
+        $this->assertTrue(in_array('PD', $result));
+        $this->assertTrue(in_array('ZD', $result));
+    }
+
+    /**
+     * Test fetchDocuments for orgnaisation set
+     *
+     * @group entity_services
+     */
+    public function testFetchBusRegDocumentsForOrganisation()
+    {
+        $params = [
+            'busReg' => 123,
+            'organisation' => 6,
+            'sort' => 'createdOn',
+            'order' => 'DESC'
+        ];
+
+        $mockDocs = [
+            'Count' => 3,
+            'Results' => [
+                0 => [
+                    'routeDocument' => 'RD',
+                    'pdfDocument' => 'PD',
+                    'zipDocument' => 'ZD'
+                ]
+            ]
+        ];
+
+        $this->expectOneRestCall('TxcInbox', 'GET', $params)
+            ->will($this->returnValue($mockDocs));
+
+        $result = $this->sut->fetchBusRegDocuments($params['busReg'], null, ['id' => $params['organisation']]);
 
         $this->assertCount(3, $result);
         $this->assertTrue(in_array('RD', $result));
