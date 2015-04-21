e6bc183894e24a9af7f53fce4d09e4df6f131b87
Nick Payne <nick@kurai.co.uk>
2015-04-21 14:21:55 +0100

    preserve important parts of a user's filter query when exporting vehicles

--- Common/src/Common/Controller/Lva/AbstractVehiclesPsvController.php
+++ Common/src/Common/Controller/Lva/AbstractVehiclesPsvController.php
@@ -54,16 +54,8 @@ abstract class AbstractVehiclesPsvController extends AbstractVehiclesController
                 continue;
             }
 
-            $rawTableData = $this->getRawTableData();
 
-            $table = $this->getServiceLocator()
-                ->get('Table')
-                ->prepareTable(
-                    'lva-psv-vehicles-' . $tableName,
-                    $this->getTableData($rawTableData, $tableName)
-                );
-
-            $table = $this->alterTable($table);
+            $table = $this->getTable($tableName);
 
             $formHelper->populateFormTable(
                 $form->get($tableName),
@@ -527,5 +519,19 @@ abstract class AbstractVehiclesPsvController extends AbstractVehiclesController
     private function getPsvTypes()
     {
         return $this->getServiceLocator()->get('Entity\Vehicle')->getTypeMap();
+    }
+
+    protected function getTable($tableName)
+    {
+        $rawTableData = $this->getRawTableData();
+
+        $table = $this->getServiceLocator()
+            ->get('Table')
+            ->prepareTable(
+                'lva-psv-vehicles-' . $tableName,
+                $this->getTableData($rawTableData, $tableName)
+            );
+
+        return $this->alterTable($table);
     }
 }
---
+++ Common/src/Common/Form/Model/Form/Lva/Fieldset/VehiclesQuery.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Common\Form\Model\Form\Lva\Fieldset;
+
+use Zend\Form\Annotation as Form;
+
+/**
+ * Vehicle Query Data
+ */
+class VehiclesQuery
+{
+    /**
+     * @Form\Attributes({"value":""})
+     * @Form\Type("Hidden")
+     */
+    public $vrm = null;
+
+    /**
+     * @Form\Attributes({"value":""})
+     * @Form\Type("Hidden")
+     */
+    public $disc = null;
+
+    /**
+     * @Form\Attributes({"value":""})
+     * @Form\Type("Hidden")
+     */
+    public $includeRemoved = null;
+}
