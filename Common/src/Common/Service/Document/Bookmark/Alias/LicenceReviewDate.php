d7c527e6a54460219e3ac071ed89ffe0c3fd0225
Piotr Filak <piotr.filak@valtech.co.uk>
2015-04-22 16:07:41 +0100

    OLCS-5840 - Document Template - Env Complaints letter

---
+++ Common/src/Common/Service/Document/Bookmark/Alias/LicenceReviewDate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Common\Service\Document\Bookmark\Alias;
+
+use Common\Service\Document\Bookmark\ReviewDate;
+
+/**
+ * Alias of ReviewDate
+ */
+class LicenceReviewDate extends ReviewDate
+{
+
+}
---
+++ Common/src/Common/Service/Document/Bookmark/LetterDateAdd21Days.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Common\Service\Document\Bookmark;
+
+use Common\Service\Document\Bookmark\Base\DateDelta;
+
+/**
+ * Letter date + 21 days class
+ */
+class LetterDateAdd21Days extends DateDelta
+{
+    const DELTA = "+21";
+}
