590108dfcb4ececae47306daf0d6ea46f4408a82
Rob Caiger <rob@clocal.co.uk>
2015-04-27 13:29:36 +0100

    Continuation detail filter form, updated TA entity service for getValueOptions

--- Common/src/Common/Service/Entity/ContinuationDetailEntityService.php
+++ Common/src/Common/Service/Entity/ContinuationDetailEntityService.php
@@ -18,6 +18,10 @@ class ContinuationDetailEntityService extends AbstractEntityService
     const STATUS_PRINTING = 'con_det_sts_printing';
     const STATUS_PRINTED = 'con_det_sts_printed';
 
+    const STATUS_UNACCEPTABLE = 'con_det_sts_unacceptable';
+    const STATUS_ACCEPTABLE = 'con_det_sts_acceptable';
+    const STATUS_COMPLETE = 'con_det_sts_complete';
+
     /**
      * Define entity for default behaviour
      *
@@ -29,9 +33,14 @@ class ContinuationDetailEntityService extends AbstractEntityService
         'children' => [
             'status',
             'licence' => [
+                'required' => true,
+                'sort' => 'licNo',
+                'order' => 'ASC',
                 'children' => [
                     'status',
-                    'organisation',
+                    'organisation' => [
+                        'required' => true
+                    ],
                     'licenceType',
                     'goodsOrPsv',
                 ]
@@ -44,12 +53,37 @@ class ContinuationDetailEntityService extends AbstractEntityService
         $this->multiCreate($records);
     }
 
-    public function getListData($continuationId)
+    /**
+     * Filter detail list data
+     */
+    public function getListData($continuationId, array $filters = [])
     {
         $query = [
             'continuation' => $continuationId
         ];
 
-        return $this->getAll($query, $this->listBundle);
+        $bundle = $this->listBundle;
+
+        if (isset($filters['licenceNo']) && !empty($filters['licenceNo'])) {
+            $bundle['children']['licence']['criteria']['licNo'] = $filters['licenceNo'];
+        }
+
+        if (isset($filters['licenceStatus']) && is_array($filters['licenceStatus'])) {
+            $bundle['children']['licence']['criteria']['status'] = $filters['licenceStatus'];
+        }
+
+        if (isset($filters['method']) && in_array($filters['method'], ['post', 'email'])) {
+            if ($filters['method'] === 'post') {
+                $bundle['children']['licence']['children']['organisation']['criteria']['allowEmail'] = 0;
+            } else {
+                $bundle['children']['licence']['children']['organisation']['criteria']['allowEmail'] = 1;
+            }
+        }
+
+        if (isset($filters['status']) && !empty($filters['status'])) {
+            $query['status'] = $filters['status'];
+        }
+
+        return $this->getAll($query, $bundle);
     }
 }
