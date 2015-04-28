83883c40742643df006ccc057f26708553a0f6f4
Nick Payne <nick@kurai.co.uk>
2015-04-28 15:02:33 +0100

    update document filters

--- Common/config/list-data/static-list-data.php
+++ Common/config/list-data/static-list-data.php
@@ -95,8 +95,8 @@ return array(
     ],
     'document_types' => [
         'all' => 'All',
-        'digital' => 'Digital only',
-        'nondigital' => 'Non-digital'
+        'external' => 'External',
+        'internal' => 'Internal'
     ],
     'vehicle_body_types' =>
     [
