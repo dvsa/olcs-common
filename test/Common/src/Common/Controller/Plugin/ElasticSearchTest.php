5815f4efab7b02670756443a904775f610640d15
Shaun Lizzio <shaun@lizzio.co.uk>
2015-05-01 13:25:20 +0100

    More UT

--- test/Common/src/Common/Controller/Plugin/ElasticSearchTest.php
+++ test/Common/src/Common/Controller/Plugin/ElasticSearchTest.php
@@ -6,6 +6,11 @@ use Mockery as m;
 use Mockery\Adapter\Phpunit\MockeryTestCase;
 use \Common\Controller\Plugin\ElasticSearch;
 use Olcs\TestHelpers\ControllerPluginManagerHelper;
+use CommonTest\Bootstrap;
+use Zend\Mvc\MvcEvent;
+use Zend\Mvc\Router\RouteMatch;
+use Zend\View\Helper\Placeholder;
+use Zend\View\Model\ViewModel;
 
 /**
  * Class ElasticSearchPluginTest
@@ -28,31 +33,47 @@ class ElasticSearchTest extends MockeryTestCase
 
     public function setUp()
     {
-        $this->sut = new ElasticSearch();
+        $this->request = m::mock('\Zend\Http\Request');
+
+        $this->routeMatch = new RouteMatch([]);
+        $this->event = new MvcEvent();
+        $this->event->setRouteMatch($this->routeMatch);
+        $this->sm = Bootstrap::getServiceManager();
+
+        $this->pm = m::mock('\Zend\Mvc\Controller\PluginManager[setInvokableClass]')->makePartial();
+        $this->pm->setInvokableClass('ElasticSearch', 'Common\Controller\Plugin\ElasticSearch');
+
+        $this->sut = new TestController();
+        $this->sut->setEvent($this->event);
+        $this->sut->setServiceLocator($this->sm);
+        $this->sut->setPluginManager($this->pm);
     }
 
 
-/*    public function testInvokeOptionsSet()
+    public function testInvokeOptionsSet()
     {
-        $controller = new TestController();
-        $controller->getPluginManager()
-            ->setInvokableClass('ElasticSearch', '\Common\Controller\Plugin\ElasticSearch');
-
         $options = [
             'container_name' => 'testcontainer',
             'layout_template' => 'testlayouttemplate',
             'page_route' => 'testroute'
         ];
 
-        $mockParams = m::mock('Zend/Mvc/Controller/Plugin/Params');
-        $mockParams->shouldReceive('getParams');
-
-        $result = $controller->invokeAction($options);
+        $result = $this->sut->pluginInvoke($options);
 
         $this->assertEquals($result->getContainerName(), $options['container_name']);
+        $this->assertEquals($result->getLayoutTemplate(), $options['layout_template']);
+        $this->assertEquals($result->getPageRoute(), $options['page_route']);
+    }
 
-    }*/
+    public function testInvokeDefaultOptions()
+    {
 
+        $result = $this->sut->pluginInvoke([]);
+
+        $this->assertEquals($result->getContainerName(), 'global_search');
+        $this->assertEquals($result->getLayoutTemplate(), 'main-search-results');
+        $this->assertEquals($result->getPageRoute(), NULL);
+    }
 
     /*
     public function testPostAction()
@@ -117,45 +138,83 @@ class ElasticSearchTest extends MockeryTestCase
     }
     */
 
-    /*
+    private function getMockSearchObjectArray()
+    {
+        return [
+            'index' => 'foo'
+        ];
+    }
+/*
     public function testGenerateResults()
     {
-        $this->sut->generateResults();
-    }
-    */
+
+        $mockForm = m::mock('Zend\Form\Form');
+        $mockForm->shouldReceive('getObject')->andReturn($this->getMockSearchObjectArray());
+
+        $mockContainer = m::mock('Zend\View\Helper\Placeholder\Container');
+        $mockContainer->shouldReceive('getValue')->andReturn($mockForm);
+
+        $mockPlaceholder = m::mock('Zend\View\Helper\Placeholder');
+        $mockPlaceholder->shouldReceive('getContainer')->with('headerSearch')->andReturn($mockContainer);
+
+        $mockViewHelperManager = m::mock('Zend\Mvc\Service\ViewHelperManagerFactory');
+        $mockViewHelperManager->shouldReceive('get')->with('placeholder')->andReturn($mockPlaceholder);
+
+        $this->sm->setService('ViewHelperManager', $mockViewHelperManager);
+
+        $plugin = $this->sut->pluginGenerateResults();
+
+
+    }*/
 
     public function testGetSetContainerName()
     {
-        $this->sut->setContainerName('testContainerName');
-        $this->assertEquals('testContainerName', $this->sut->getContainerName());
+        $plugin = $this->sut->pluginInvoke([]);
+
+        $plugin->setContainerName('testContainerName');
+        $this->assertEquals('testContainerName', $plugin->getContainerName());
     }
 
     public function testGetSetSearchData()
     {
-        $this->sut->setSearchData('testsearchdata');
-        $this->assertEquals('testsearchdata', $this->sut->getSearchData());
+        $plugin = $this->sut->pluginInvoke([]);
+
+        $plugin->setSearchData('testsearchdata');
+        $this->assertEquals('testsearchdata', $plugin->getSearchData());
     }
 
     public function testGetSetLayoutTemplate()
     {
-        $this->sut->setLayoutTemplate('testLayoutTemplate');
-        $this->assertEquals('testLayoutTemplate', $this->sut->getLayoutTemplate());
+        $plugin = $this->sut->pluginInvoke([]);
+
+        $plugin->setLayoutTemplate('testLayoutTemplate');
+        $this->assertEquals('testLayoutTemplate', $plugin->getLayoutTemplate());
     }
 
     public function testGetSetPageRoute()
     {
-        $this->sut->setPageRoute('testpageroute');
-        $this->assertEquals('testpageroute', $this->sut->getPageRoute());
+        $plugin = $this->sut->pluginInvoke([]);
+
+        $plugin->setPageRoute('testpageroute');
+        $this->assertEquals('testpageroute', $plugin->getPageRoute());
     }
 
 }
 
 class testController extends \Common\Controller\AbstractActionController
 {
-    public function invokeAction($options)
+    public function pluginInvoke($options)
     {
         $plugin = $this->ElasticSearch($options);
 
         return $plugin;
+    }
+
+    public function pluginGenerateResults()
+    {
+        $view = new ViewModel();
+        $plugin = $this->ElasticSearch();
+
+        return $plugin->generateResults($view);
     }
 }
