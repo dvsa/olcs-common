b4a29ccc59e23809d68386549112fa33cac95fc6
Dan Eggleston <dan@stolenegg.com>
2015-05-12 14:18:16 +0100

    OLCS-9109 configurable api endpoints

---
+++ Common/src/Common/Service/CompaniesHouse/Api.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Companies House Api Service
+ *
+ * @author Dan Eggleston <dan@stolenegg.com>
+ */
+namespace Common\Service\CompaniesHouse;
+
+use Zend\ServiceManager\ServiceLocatorAwareTrait;
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
+use Zend\View\Model\ViewModel;
+use Common\Util\RestCallTrait;
+
+/**
+ * Companies House Api Service
+ *
+ * @author Dan Eggleston <dan@stolenegg.com>
+ */
+class Api implements ServiceLocatorAwareInterface
+{
+    use RestCallTrait;
+    use ServiceLocatorAwareTrait;
+
+    public function getCompanyProfile($companyNumber)
+    {
+        return $this->sendGet('companies_house\\company', [$companyNumber], true);
+    }
+}
