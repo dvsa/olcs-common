50bf0f5fe1aa8bec4a2c0a79b556d19614dcfca4
Henry Hayes <henryhayesuk@gmail.com>
2015-05-13 16:23:46 +0100

    Moved search types tests

---
+++ test/Common/src/Common/Data/Object/Search/VehicleTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace CommonTest\Data\Object\Search;
+
+/**
+ * Class VehicleTest
+ * @package CommonTest\Data\Object\Search
+ */
+class VehicleTest extends SearchAbstractTest
+{
+    protected $class = 'Common\Data\Object\Search\Vehicle';
+}
