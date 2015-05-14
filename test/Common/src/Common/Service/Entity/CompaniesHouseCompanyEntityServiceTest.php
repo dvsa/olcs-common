d5637736167c82354e5a9f786be375951ba3d69a
Dan Eggleston <dan@stolenegg.com>
2015-05-14 14:13:21 +0100

    OLCS-9109 unit test for new entity service

---
+++ test/Common/src/Common/Service/Entity/CompaniesHouseCompanyEntityServiceTest.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * Companies House Company Entity Service Test
+ *
+ * @author Dan Eggleston <dan@stolenegg.com>
+ */
+namespace CommonTest\Service\Entity;
+
+use Mockery as m;
+use Common\Service\Entity\CompaniesHouseCompanyEntityService;
+
+/**
+ * Companies House Company Entity Service Test
+ *
+ * @author Dan Eggleston <dan@stolenegg.com>
+ */
+class CompaniesHouseCompanyEntityServiceTest extends AbstractEntityServiceTestCase
+{
+    public function setUp()
+    {
+        $this->sut = new CompaniesHouseCompanyEntityService();
+
+        parent::setUp();
+    }
+
+    public function testGetByCompanyNumber()
+    {
+        $companyNumber = '01234567';
+
+        $expectedQuery = [
+            'companyNumber' => $companyNumber,
+        ];
+
+        $expectedBundle = [
+            'children' => [
+                'companyStatus',
+                'country',
+                'officers' => [
+                    'children' => [
+                        'role',
+                    ],
+                ],
+            ],
+        ];
+
+        // expectations
+        $this->expectOneRestCall('CompaniesHouseCompany', 'GET', $expectedQuery, $expectedBundle)
+            ->will($this->returnValue('RESPONSE'));
+
+        // assertions
+        $this->assertEquals('RESPONSE', $this->sut->getByCompanyNumber($companyNumber));
+    }
+
+    public function testSaveNew()
+    {
+        $data = [
+            'companyNumber' => '01234567',
+            'officers' => [
+                ['name' => 'Bob'],
+                ['name' => 'Dave'],
+            ]
+        ];
+
+        $expectedData = [
+            'companyNumber' => '01234567',
+            'officers' => [
+                ['name' => 'Bob'],
+                ['name' => 'Dave'],
+            ],
+            '_OPTIONS_' => [
+                'cascade' => [
+                    'list' => [
+                        'officers' => [
+                            'entity' => 'companiesHouseOfficer',
+                            'parent' => 'company',
+                        ]
+                    ]
+                ]
+            ],
+        ];
+
+        // expectations
+        $this->expectOneRestCall('CompaniesHouseCompany', 'POST', $expectedData)
+            ->will($this->returnValue('RESPONSE'));
+
+        $this->assertEquals('RESPONSE', $this->sut->saveNew($data));
+    }
+}
