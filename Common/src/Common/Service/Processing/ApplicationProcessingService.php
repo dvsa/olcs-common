080bb730a04d3af3cecace330e267f8aeae04eaf
Mat Evans <mat.evans@valtech.co.uk>
2015-05-20 17:19:33 +0100

    OLCS-9061 Create snapshot when refuse, withdraw, NTU application/variation

--- Common/src/Common/Service/Processing/ApplicationProcessingService.php
+++ Common/src/Common/Service/Processing/ApplicationProcessingService.php
@@ -722,6 +722,9 @@ class ApplicationProcessingService implements ServiceLocatorAwareInterface
      */
     public function processWithdrawApplication($id, $reason)
     {
+        $this->getServiceLocator()->get('Processing\ApplicationSnapshot')
+            ->storeSnapshot($id, ApplicationSnapshotProcessingService::ON_WITHDRAW);
+
         $applicationEntityService = $this->getServiceLocator()->get('Entity\Application');
 
         // Set the application status to 'Withdrawn'
@@ -754,6 +757,9 @@ class ApplicationProcessingService implements ServiceLocatorAwareInterface
      */
     public function processRefuseApplication($id)
     {
+        $this->getServiceLocator()->get('Processing\ApplicationSnapshot')
+            ->storeSnapshot($id, ApplicationSnapshotProcessingService::ON_REFUSE);
+
         $applicationEntityService = $this->getServiceLocator()->get('Entity\Application');
 
         // Set the application status to 'Refused'
@@ -785,6 +791,9 @@ class ApplicationProcessingService implements ServiceLocatorAwareInterface
      */
     public function processNotTakenUpApplication($id)
     {
+        $this->getServiceLocator()->get('Processing\ApplicationSnapshot')
+            ->storeSnapshot($id, ApplicationSnapshotProcessingService::ON_NTU);
+
         $licenceId = $this->getLicenceId($id);
 
         // Update the licence and application statuses to NTU
