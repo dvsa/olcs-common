f5a313579afa36f956032c9e3d08c9553def6020
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-05-26 15:57:16 +0100

    Application licence cancelled filter

---
+++ Common/src/Common/Filter/Publication/ApplicationLicenceCancelled.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * Application licence cancelled filter
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+namespace Common\Filter\Publication;
+
+use Common\Exception\ResourceNotFoundException;
+
+/**
+ * Application licence cancelled filter
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+class ApplicationLicenceCancelled extends AbstractPublicationFilter
+{
+    const LIC_TERMINATED = 'Licence terminated WEF ';
+    const LIC_SURRENDERED = 'Licence surrendered WEF ';
+    const LIC_CNS = 'Licence not continued WEF ';
+
+    /**
+     * @var string $date
+     */
+    protected $date;
+
+    /**
+     * @param \Common\Data\Object\Publication $publication
+     * @return \Common\Data\Object\Publication
+     */
+    public function filter($publication)
+    {
+        $publicationSection = $publication->offsetGet('publicationSection');
+
+        $newData = [];
+
+        switch ($publicationSection) {
+            case self::LIC_SURRENDERED_SECTION:
+                $newData['licenceCancelled'] = self::LIC_SURRENDERED . $this->createDate();
+                break;
+            case self::LIC_TERMINATED_SECTION:
+                $newData['licenceCancelled'] = self::LIC_TERMINATED . $this->createDate();
+                break;
+            case self::LIC_CNS_SECTION:
+                $newData['licenceCancelled'] = self::LIC_CNS . $this->createDate();
+                break;
+        }
+
+        $publication = $this->mergeData($publication, $newData);
+
+        return $publication;
+    }
+
+    /**
+     * Allows easier unit testing
+     *
+     * @param string $date
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * Gets the date
+     *
+     * @return string
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    public function createDate()
+    {
+        if ($this->getDate() == null) {
+            $dateTime = new \DateTime();
+            $this->setDate($dateTime->format('j F Y'));
+        }
+
+        return $this->getDate();
+    }
+}
