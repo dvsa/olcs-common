42cbe976ed14b11e1600594979c12d33cbd9a1ca
Rob Caiger <rob@clocal.co.uk>
2015-07-20 16:36:33 +0100

    Unit tests

--- Common/src/Common/Service/Listener/FeeListenerService.php
+++ Common/src/Common/Service/Listener/FeeListenerService.php
@@ -10,7 +10,6 @@ namespace Common\Service\Listener;
 
 use Zend\ServiceManager\ServiceLocatorAwareInterface;
 use Zend\ServiceManager\ServiceLocatorAwareTrait;
-use Common\Service\Entity\ApplicationEntityService;
 use Common\Service\Data\FeeTypeDataService;
 
 /**
@@ -28,44 +27,6 @@ class FeeListenerService implements ServiceLocatorAwareInterface
 
     public function trigger($id, $eventType)
     {
-        $method = 'trigger' . $eventType;
-        if (method_exists($this, $method)) {
-            return $this->$method($id);
-        }
-
-        throw new Exception('Event type not found');
-    }
-
-    protected function triggerPay($id)
-    {
-        $this->maybeProcessGrantingFee($id);
-    }
-
-    protected function triggerWaive($id)
-    {
-        $this->maybeProcessGrantingFee($id);
-    }
-
-    /**
-     * If the fee type is a interim, then check if we do need in-force processing
-     *
-     * @param int $feeId Fee ID
-     *
-     * @return bool Whether the licence was continued
-     */
-    protected function maybeProcessGrantingFee($feeId)
-    {
-        $fee = $this->getServiceLocator()->get('Entity\Fee')->getFeeDetailsForInterim($feeId);
-
-        if ($fee['feeType']['feeType']['id'] !== FeeTypeDataService::FEE_TYPE_GRANTINT) {
-            return false;
-        }
-
-        if (!isset($fee['application']['interimStatus']['id']) ||
-            $fee['application']['interimStatus']['id'] !== ApplicationEntityService::INTERIM_STATUS_GRANTED) {
-            return false;
-        }
-
-        $this->getServiceLocator()->get('Helper\Interim')->grantInterim($fee['application']['id']);
+        // @todo Remove all references to this as it is no longer used
     }
 }
