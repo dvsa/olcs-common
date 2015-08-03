69a08e741762b8be52d53d749f2e48fead6f47ea
Mat Evans <mat.evans@valtech.co.uk>
2015-08-03 10:30:11 +0100

    OLCS-5226 People section guidance message

--- Common/src/Common/Controller/Lva/Adapters/ApplicationPeopleAdapter.php
+++ Common/src/Common/Controller/Lva/Adapters/ApplicationPeopleAdapter.php
@@ -16,21 +16,4 @@ use Zend\Form\Form;
  */
 class ApplicationPeopleAdapter extends VariationPeopleAdapter
 {
-    public function alterFormForOrganisation(Form $form, $table)
-    {
-        if (!$this->hasInforceLicences()) {
-            return;
-        }
-
-        return parent::alterFormForOrganisation($form, $table);
-    }
-
-    public function alterAddOrEditFormForOrganisation(Form $form)
-    {
-        if (!$this->hasInforceLicences()) {
-            return;
-        }
-
-        return parent::alterAddOrEditFormForOrganisation($form);
-    }
 }
