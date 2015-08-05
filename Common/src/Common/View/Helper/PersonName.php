75e82a4bc40f32da9f715498b24f9f0fa645facc
Shaun Lizzio <shaun@lizzio.co.uk>
2015-08-05 14:41:58 +0100

    Added escapeHtml

--- Common/src/Common/View/Helper/PersonName.php
+++ Common/src/Common/View/Helper/PersonName.php
@@ -9,6 +9,7 @@
 namespace Common\View\Helper;
 
 use Zend\View\Helper\AbstractHelper;
+use Zend\View\Helper\EscapeHtml;
 
 /**
  * PersonName view helper
@@ -17,6 +18,13 @@ use Zend\View\Helper\AbstractHelper;
  */
 class PersonName extends AbstractHelper
 {
+    /**
+     * Html escape helper
+     *
+     * @var EscapeHtml
+     */
+    protected $escapeHtmlHelper;
+
     /**
      * Get the HTML to render a persons name
      *
@@ -33,13 +41,36 @@ class PersonName extends AbstractHelper
         ]
     ) {
         $parts = array();
+        $escapeHtml = $this->getEscapeHtmlHelper();
 
         foreach ($fields as $item) {
             if (isset($person[$item]) && !empty($person[$item])) {
-                $parts[] = $person[$item];
+                $parts[] = $escapeHtml($person[$item]);
             }
         }
 
         return implode(' ', $parts);
+    }
+
+    /**
+     * Retrieve the escapeHtml helper
+     *
+     * @return EscapeHtml
+     */
+    private function getEscapeHtmlHelper()
+    {
+        if ($this->escapeHtmlHelper) {
+            return $this->escapeHtmlHelper;
+        }
+
+        if (method_exists($this->getView(), 'plugin')) {
+            $this->escapeHtmlHelper = $this->view->plugin('escapehtml');
+        }
+
+        if (!$this->escapeHtmlHelper instanceof EscapeHtml) {
+            $this->escapeHtmlHelper = new EscapeHtml();
+        }
+
+        return $this->escapeHtmlHelper;
     }
 }
