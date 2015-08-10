de483f6a66dad6b25ec3944f2b866b719c557918
Dan Eggleston <dan@stolenegg.com>
2015-08-10 15:22:04 +0100

    OLCS-9269 add unit tests for form services

---
+++ test/Common/src/Common/FormService/Form/Lva/ConditionsUndertakingsTest.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Conditions & Undertakings Form Service Test
+ *
+ * @author Dan Eggleston <dan@stolenegg.com>
+ */
+namespace CommonTest\FormService\Form\Lva;
+
+use Common\FormService\Form\Lva\ConditionsUndertakings;
+
+/**
+ * Conditions & Undertakings Form Service Test
+ *
+ * @author Dan Eggleston <dan@stolenegg.com>
+ */
+class ConditionsUndertakingsTest extends AbstractLvaFormServiceTestCase
+{
+    protected $classToTest = ConditionsUndertakings::class;
+
+    protected $formName = 'Lva\ConditionsUndertakings';
+}
