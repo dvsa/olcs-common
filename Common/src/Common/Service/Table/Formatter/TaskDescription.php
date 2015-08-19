cb7f018e925e6e782198601ea402e854e428dc53
Dan Eggleston <dan@stolenegg.com>
2015-08-19 13:15:55 +0100

    OLCS-4254 move inline formatter for task descriptions into formatter class

---
+++ Common/src/Common/Service/Table/Formatter/TaskDescription.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * Task description formatter
+ *
+ * @author Dan Eggleston <dan@stolenegg.com>
+ */
+
+namespace Common\Service\Table\Formatter;
+
+/**
+ * Task description formatter
+ *
+ * @author Dan Eggleston <dan@stolenegg.com>
+ */
+class TaskDescription implements FormatterInterface
+{
+    /**
+     * Format a task description
+     *
+     * @param array $data
+     * @param array $column
+     * @param \Zend\ServiceManager\ServiceManager $sm
+     * @return string
+     */
+    public static function format ($row, $column, $serviceLocator) {
+        $router     = $serviceLocator->get('router');
+        $request    = $serviceLocator->get('request');
+        $routeMatch = $router->match($request);
+        $params     = $routeMatch->getParams();
+
+        // the edit URL should pass the context of the page we're on,
+        // rather than the type of each task
+        // (see https://jira.i-env.net/browse/OLCS-6041)
+        switch ($routeMatch->getMatchedRouteName()) {
+            case 'licence/processing/tasks':
+                $routeParams = ['type' => 'licence', 'typeId' => $params['licence']];
+                break;
+            case 'lva-application/processing/tasks':
+                $routeParams = ['type' => 'application', 'typeId' => $params['application']];
+                break;
+            case 'transport-manager/processing/tasks':
+                $routeParams = ['type' => 'tm', 'typeId' => $params['transportManager']];
+                break;
+            case 'licence/bus-processing/tasks':
+                $routeParams = [
+                    'type'    => 'busreg',
+                    'typeId'  => $params['busRegId'],
+                    'licence' => $params['licence']
+                ];
+                break;
+            case 'case_processing_tasks':
+                $routeParams = ['type' => 'case', 'typeId' => $params['case']];
+                break;
+            default:
+                $routeParams = [];
+                break;
+        }
+        $url = $this->generateUrl(
+            array_merge(
+                [
+                    'task' => $row['id'],
+                    'action' => 'edit',
+                ],
+                $routeParams
+            ),
+            'task_action'
+        );
+        return '<a href="'
+            . $url
+            . '" class=js-modal-ajax>'
+            . $row['description']
+            . '</a>';
+    }
+}
