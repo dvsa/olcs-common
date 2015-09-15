e2c0e9f55a429741aad35d04e87a258788118b64
Dan Eggleston <dan@stolenegg.com>
2015-09-15 15:21:46 +0100

    OLCS-4738 received amount form validator

---
+++ Common/src/Common/Form/Elements/Validators/ReceivedAmount.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Received Amount Validator
+ *
+ * @author Dan Eggleston <dan@stolenegg.com>
+ */
+namespace Common\Form\Elements\Validators;
+
+/**
+ * Received Amount Validator
+ *
+ * @author Dan Eggleston <dan@stolenegg.com>
+ */
+class ReceivedAmount extends \Zend\Validator\Between
+{
+    /**
+     * Validation failure message template definitions
+     *
+     * @var array
+     */
+    protected $messageTemplates = array(
+        self::NOT_BETWEEN => "Part payments are permitted but the amount entered is insufficient to
+            allocate any payment to one or more of the selected fees.
+            Please enter a value between %min% and %max%.",
+        self::NOT_BETWEEN_STRICT => "The input is not strictly between '%min%' and '%max%'"
+    );
+
+    public function isValid($value, $context = null)
+    {
+        if (isset($context['minAmountForValidator'])) {
+            $this->setMin($context['minAmountForValidator']);
+        }
+        if (isset($context['maxAmountForValidator'])) {
+            $this->setMax($context['maxAmountForValidator']);
+        }
+        $this->setInclusive(true);
+
+        return parent::isValid($value);
+    }
+}
