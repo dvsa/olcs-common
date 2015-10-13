3d4e93f7ae186cc65eaec94a1f41c3073d57d6a3
Rob Caiger <rob@clocal.co.uk>
2015-10-13 16:08:38 +0100

    Translations

--- Common/config/language/replacements.php
+++ Common/config/language/replacements.php
@@ -3,4 +3,5 @@
 return [
     '{{TM1_FORM_LINK}}' => 'https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/193217/'
         . 'TM1_Form.pdf',
+    '{{OPENING_TIMES_LINK}}' => 'https://www.gov.uk/government/organisations/traffic-commissioners/about/access-and-opening',
 ];
