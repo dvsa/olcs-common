96404f58bc680ab81e2291949235bc467f638117
Dan Eggleston <dan@stolenegg.com>
2015-11-04 13:36:42 +0000

    OLCS-6006 table formatters and translations for reversals

---
+++ Common/src/Common/Service/Table/Formatter/TransactionFeeStatus.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * Transaction Fee Statusformatter
+ *
+ * @author Dan Eggleston <dan@stolenegg.com>
+ */
+
+namespace Common\Service\Table\Formatter;
+
+// need to alias as RefData exists in Formatter namespace
+use Common\RefData as Ref;
+
+/**
+ * Transaction Fee Statusformatter
+ *
+ * @author Dan Eggleston <dan@stolenegg.com>
+ */
+class TransactionFeeStatus extends Money
+{
+    /**
+     * Format a transaction fee allocated amount
+     *
+     * @param array $row
+     * @param array $column
+     * @param \Zend\ServiceManager\ServiceManager $sm
+     * @return string
+     * @inheritdoc
+     */
+    public static function format($row, $column = array(), $serviceLocator = null)
+    {
+        $status = 'Applied';
+
+        if (isset($row['reversingTransaction'])) {
+            $router     = $serviceLocator->get('router');
+            $request    = $serviceLocator->get('request');
+            $urlHelper  = $serviceLocator->get('Helper\Url');
+            $routeMatch = $router->match($request);
+            $matchedRouteName = $routeMatch->getMatchedRouteName();
+
+            $url = $urlHelper->fromRoute(
+                $matchedRouteName,
+                ['transaction' => $row['reversingTransaction']['id']],
+                [],
+                true
+            );
+
+            switch ($row['reversingTransaction']['type']) {
+                case Ref::TRANSACTION_TYPE_REFUND:
+                    $status = 'Refunded';
+                    break;
+                case Ref::TRANSACTION_TYPE_REVERSAL:
+                    $status = 'Reversed';
+                    break;
+                default:
+                    $status = 'Adjusted';
+                    break;
+            }
+
+            return '<a href="'. $url . '">'. $status . '</a>';
+        }
+
+        return $status;
+    }
+}
