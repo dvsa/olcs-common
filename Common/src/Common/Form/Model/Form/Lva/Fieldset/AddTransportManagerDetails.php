7597f051a772215fb670a151a3af7cb7b3fc4dff
Alex P <alex.peshkov@valtech.co.uk>
2015-11-20 10:42:24 +0000

    OLCS-9884 Email address validation is inconsistent across the application.

--- Common/src/Common/Form/Model/Form/Lva/Fieldset/AddTransportManagerDetails.php
+++ Common/src/Common/Form/Model/Form/Lva/Fieldset/AddTransportManagerDetails.php
@@ -46,8 +46,7 @@ class AddTransportManagerDetails
      * @Form\Options({"label":"lva-tm-details-email"})
      * @Form\Type("Text")
      * @Form\Filter({"name":"Zend\Filter\StringTrim"})
-     * @Form\Validator({"name":"Zend\Validator\EmailAddress"})
-     * @Form\Validator({"name":"Zend\Validator\StringLength","options":{"min":5,"max":255}})
+     * @Form\Validator({"name":"Dvsa\Olcs\Transfer\Validators\EmailAddress"})
      */
     public $email = null;
 
