1025a57454cc218aa2dd7443a8bd4667d0ad526c
Alex P <alex.peshkov@valtech.co.uk>
2015-11-25 11:07:00 +0000

    OLCS-11054 address service changed

--- Common/src/Common/Service/Data/AddressDataService.php
+++ Common/src/Common/Service/Data/AddressDataService.php
@@ -4,26 +4,32 @@
  * Address Data Service
  *
  * @author Rob Caiger <rob@clocal.co.uk>
+ * @author Alex Peshkov <alex.peshkov@valtech.co.uk>
  */
 namespace Common\Service\Data;
 
-use Zend\ServiceManager\ServiceLocatorAwareInterface;
-use Zend\ServiceManager\ServiceLocatorAwareTrait;
+use Dvsa\Olcs\Transfer\Query\Address\GetAddress;
+use Dvsa\Olcs\Transfer\Query\Address\GetList;
+use Common\Service\Entity\Exceptions\UnexpectedResponseException;
 
 /**
  * Address Data Service
  *
  * @author Rob Caiger <rob@clocal.co.uk>
+ * @author Alex Peshkov <alex.peshkov@valtech.co.uk>
  */
-class AddressDataService implements ServiceLocatorAwareInterface
+class AddressDataService extends AbstractDataService
 {
-    use ServiceLocatorAwareTrait;
-
     public function getAddressForUprn($uprn)
     {
-        die('uprn: ' . $uprn);
-        return $this->getServiceLocator()->get('Helper\Rest')
-            ->sendGet('postcode\address', array('id' => $uprn), true);
+        $dtoData = GetAddress::create(['uprn' => $uprn]);
+
+        $response = $this->handleQuery($dtoData);
+        if ($response->isServerError() || $response->isClientError() || !$response->isOk() ||
+            !count($response->getResult()['results'])) {
+            throw new UnexpectedResponseException('unknown-error');
+        }
+        return $response->getResult()['results'][0];
     }
 
     public function getAddressesForPostcode($postcode)
