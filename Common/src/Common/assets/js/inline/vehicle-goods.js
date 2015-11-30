273bb4a99765337f1c87e646d85a727e94dd6692
Sam Quayle <sam.quayle@valtech.co.uk>
2015-11-30 11:50:24 +0000

    Fixes issue with double binding

--- Common/src/Common/assets/js/inline/vehicle-goods.js
+++ Common/src/Common/assets/js/inline/vehicle-goods.js
@@ -19,8 +19,15 @@ $(function() {
     }
   });
 
+});
+
+
+OLCS.ready(function() {
+  "use strict";
+
   OLCS.toggleElement({
     triggerSelector: '.more-actions',
     targetSelector: '.more-actions__list'
   });
+
 });
