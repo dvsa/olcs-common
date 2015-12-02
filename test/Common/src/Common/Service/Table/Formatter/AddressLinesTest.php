5f0d9310806ef1b3c1c44493e98f8e6114ff8bb8
Shaun Lizzio <shaun@lizzio.co.uk>
2015-12-02 15:54:14 +0000

    Added AddressLines table formatter

---
+++ test/Common/src/Common/Service/Table/Formatter/AddressLinesTest.php
@@ -0,0 +1,117 @@
+<?php
+
+/**
+ * AddressLines formatter test
+ *
+ * @author Rob Caiger <rob@clocal.co.uk>
+ */
+
+namespace CommonTest\Service\Table\Formatter;
+
+use Common\Service\Table\Formatter\AddressLines;
+
+/**
+ * AddressLines formatter test
+ *
+ * @author Shaun Lizzio <shaun@lizzio.co.uk>
+ */
+class AddressLinesTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Test the format method
+     *
+     * @group Formatters
+     * @group AddressLinesFormatter
+     *
+     * @dataProvider provider
+     */
+    public function testFormat($data, $column, $expected)
+    {
+        $this->assertEquals($expected, AddressLines::format($data, $column));
+    }
+
+    /**
+     * Data provider
+     *
+     * @return array
+     */
+    public function provider()
+    {
+        return array(
+            array(
+                array('addressLine1' => 'foo'), array(), '<p>foo</p>'
+            ),
+            array(
+                array('addressLine1' => 'foo', 'addressLine2' => 'bar'), array(), '<p>foo</p>'
+            ),
+            array(
+                array('addressLine1' => 'foo', 'addressLine2' => 'bar', 'town' => 'cake'), array(), '<p>foo,<br />cake</p>'
+            ),
+            array(
+                array(
+                    'addressLine1' => 'foo',
+                    'addressLine2' => 'bar',
+                    'addressLine3' => 'cake',
+                    'town' => 'fourth'
+                ),
+                array(),
+                '<p>foo,<br />fourth</p>'
+            ),
+            array(
+                array('addressLine1' => 'foo', 'addressLine2' => 'bar', 'addressLine3' => 'cake'),
+                array('addressFields' => array('addressLine1', 'addressLine2')),
+                '<p>foo,<br />bar</p>'
+            ),
+            array(
+                array('addressLine1' => 'foo', 'addressLine2' => 'bar', 'addressLine3' => 'cake'),
+                array('addressFields' => 'FULL'),
+                '<p>foo,<br />bar,<br />cake</p>'
+            ),
+            array(
+                array(
+                    'address' => array(
+                        'addressLine1' => 'foo',
+                        'addressLine2' => 'bar',
+                        'addressLine3' => 'cake',
+                        'town' => 'fourth'
+                    )
+                ),
+                array(
+                    'name' => 'address'
+                ),
+                '<p>foo,<br />fourth</p>'
+            )
+        );
+    }
+
+    /**
+     * Test the format method with nested keys
+     *
+     * @group Formatters
+     * @group AddressLinesFormatter
+     */
+    public function testFormatWithNestedKeys()
+    {
+        $mockHelper = $this->getMock('\stdClass', array('fetchNestedData'));
+
+        $mockHelper->expects($this->once())
+            ->method('fetchNestedData')
+            ->with(['foo' => 'bar'], 'bar->baz')
+            ->willReturn(['addressLine1' => 'address 1']);
+
+        $sm = $this->getMock('\stdClass', array('get'));
+        $sm->expects($this->any())
+            ->method('get')
+            ->with('Helper\Data')
+            ->will($this->returnValue($mockHelper));
+
+        $data = [
+            'foo' => 'bar'
+        ];
+        $columns = [
+            'name' => 'bar->baz'
+        ];
+        $this->assertEquals('<p>address 1</p>', AddressLines::format($data, $columns, $sm));
+    }
+}
