1e7ca905ac99c5782443acbcf9cfba653e750c05
Shaun Lizzio <shaun@lizzio.co.uk>
2016-03-08 11:22:22 +0000

    Added formatter and UT

---
+++ Common/src/Common/Service/Table/Formatter/ConvictionDescription.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * ConvictionDescription formatter
+ * If category is set against a conviction, use the category description text as the description.
+ * If the category is NOT SET OR is set to User defined (new entries only) then this implies a user defined
+ * category and the category.category_text field should be used as the description.
+ *
+ * @author Shaun Lizzio <shaun.lizzio@valtech.co.uk>
+ */
+
+namespace Common\Service\Table\Formatter;
+
+/**
+ * ConvictionDescription formatter
+ *
+ * @author Shaun Lizzio <shaun.lizzio@valtech.co.uk>
+ */
+class ConvictionDescription implements FormatterInterface
+{
+    /**
+     * ConvictionDescription value
+     *
+     * conv_c_cat_1144 is ref data id for a 'User defined' category. However all new convictions with
+     * User defined descriptions have no category. Hence the need for this formatter.
+     *
+     * @param array $data
+     * @param array $column
+     * @param \Zend\ServiceManager\ServiceManager $sm
+     * @return string
+     */
+    public static function format($data, $column = array(), $sm = null)
+    {
+        if (count($data['convictionCategory']) && $data['convictionCategory']['id'] != 'conv_c_cat_1144') {
+            $data['categoryText'] = $data['convictionCategory']['description'];
+        }
+
+        $categoryText = $data['categoryText'];
+
+        $append = strlen($categoryText) > 30 ? '...' : '';
+        return nl2br(substr($categoryText, 0, 30)) . $append;
+    }
+}
