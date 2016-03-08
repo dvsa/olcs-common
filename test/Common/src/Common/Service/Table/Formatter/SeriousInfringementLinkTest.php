fbccc9f95226a9d98119a577d0f9b3a9ad2cfeec
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-03-08 15:09:14 +0000

    Formatter for serious infringement table links

---
+++ test/Common/src/Common/Service/Table/Formatter/SeriousInfringementLinkTest.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * SeriousInfringementLinkTest.php
+ */
+namespace CommonTest\Service\Table\Formatter;
+
+use Mockery as m;
+
+use Common\Service\Table\Formatter\SeriousInfringementLink;
+
+/**
+ * Class SeriousInfringementLinkTest
+ *
+ * @package CommonTest\Service\Table\Formatter
+ */
+class SeriousInfringementLinkTest extends \PHPUnit_Framework_TestCase
+{
+    public function testFormat()
+    {
+        $id = 69;
+        $inputData = ['id' => $id];
+        $route = 'case_penalty_applied';
+        $routeParams = [
+            'si' => $id,
+            'action' => 'index'
+        ];
+
+        $sm = m::mock()
+            ->shouldReceive('get')
+            ->with('Helper\Url')
+            ->andReturn(
+                m::mock()
+                    ->shouldReceive('fromRoute')
+                    ->with($route, $routeParams, [], true)
+                    ->andReturn('URL')
+                    ->getMock()
+            )
+            ->getMock();
+
+        $this->assertEquals(
+            '<a href="URL">' . $id . '</a>',
+            SeriousInfringementLink::format($inputData, [], $sm)
+        );
+    }
+}
