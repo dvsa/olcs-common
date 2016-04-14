16071b287c4bcbced7d934b511696fa3dd0e4a1a
Mat Evans <mat.evans@valtech.co.uk>
2016-04-14 11:33:44 +0100

    OLCS-5157 Task Allocation Rules Admin

---
+++ Common/src/Common/Service/Table/Formatter/TaskAllocationRule/User.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Common\Service\Table\Formatter\TaskAllocationRule;
+
+/**
+ * User value for a task allocation rule
+ *
+ * @author Mat Evans <mat.evans@valtech.co.uk>
+ */
+class User extends \Common\Service\Table\Formatter\Name
+{
+    /**
+     * User value for a task allocation rule
+     *
+     * @param array $data
+     * @param array $column
+     * @param \Zend\ServiceManager\ServiceManager $sm
+     *
+     * @return string
+     */
+    public static function format($data, $column = array(), $sm = null)
+    {
+        $userName = parent::format($data, $column, $sm);
+        if (!empty(trim($userName))) {
+            return $userName;
+        }
+
+        if (is_array($data['taskAlphaSplits']) && count($data['taskAlphaSplits']) > 0) {
+            return '[Alpha split]';
+        }
+
+        return 'Unassigned';
+    }
+}
