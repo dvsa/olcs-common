4de0360791defd37ed29785892925f2ce84de821
Mat Evans <mat.evans@valtech.co.uk>
2016-04-29 12:07:39 +0100

    OLCS-12677 Fix other methods that have incorrect type hint

--- Common/src/Common/Service/Helper/FormHelperService.php
+++ Common/src/Common/Service/Helper/FormHelperService.php
@@ -12,7 +12,6 @@ use Common\Service\Table\TableBuilder;
 use Zend\Form\Element;
 use Zend\Form\Element\Checkbox;
 use Zend\Form\Element\DateSelect;
-use Zend\Form\Element\Select;
 use Zend\Form\Fieldset;
 use Zend\Form\Form;
 use Zend\Form\FormInterface;
@@ -747,7 +746,7 @@ class FormHelperService extends AbstractHelperService
         }
     }
 
-    public function setCurrentOption(Select $element, $index)
+    public function setCurrentOption(Element $element, $index)
     {
         $options = $element->getValueOptions();
 
@@ -884,7 +883,7 @@ class FormHelperService extends AbstractHelperService
         }
     }
 
-    public function removeValueOption(Select $element, $key)
+    public function removeValueOption(Element $element, $key)
     {
         $options = $element->getValueOptions();
 
