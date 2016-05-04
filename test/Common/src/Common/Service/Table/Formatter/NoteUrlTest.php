3919b81936ce244b0330fa86b197e0cf1485c9d9
Alex P <alex.peshkov@valtech.co.uk>
2016-05-04 12:22:41 +0100

    OLCS-12427 unit test added

---
+++ test/Common/src/Common/Service/Table/Formatter/NoteUrlTest.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Note Url formatter test
+ *
+ * @author Alex.Peshkov <alex.peshkov@valtech.co.uk>
+ */
+namespace CommonTest\Service\Table\Formatter;
+
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Mockery as m;
+use Common\Service\Table\Formatter\NoteUrl;
+use CommonTest\Bootstrap;
+
+/**
+ * Note Url formatter test
+ *
+ * @author Alex.Peshkov <alex.peshkov@valtech.co.uk>
+ */
+class NoteUrlTest extends MockeryTestCase
+{
+    /**
+     * Test the format method
+     */
+    public function testFormat()
+    {
+        $data = [
+            'id' => 1,
+            'createdOn' => '2015-01-01 10:10'
+        ];
+        $sm = Bootstrap::getServiceManager();
+
+        $mockUrlHelper = m::mock()
+            ->shouldReceive('fromRoute')
+            ->with(
+                null,
+                ['action' => 'edit', 'id' => $data['id']],
+                ['query' => ['foo' => 'bar']],
+                true
+            )
+            ->andReturn('the_url')
+            ->getMock();
+
+        $mockRequest = m::mock('\Zend\Stdlib\RequestInterface')
+            ->shouldReceive('getQuery')
+            ->andReturn(
+                m::mock()
+                    ->shouldReceive('toArray')
+                    ->once()
+                    ->andReturn(['foo' => 'bar'])
+                    ->getMock()
+            )
+            ->once()
+            ->getMock();
+
+        $sm->setService('request', $mockRequest);
+        $sm->setService('Helper\Url', $mockUrlHelper);
+
+        $expectedLink = '<a class="js-modal-ajax" href="the_url">'
+            . (new \DateTime($data['createdOn']))->format(\DATE_FORMAT) . '</a>';
+
+        $this->assertEquals($expectedLink, NoteUrl::format($data, [], $sm));
+    }
+}
