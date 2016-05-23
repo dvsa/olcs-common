2261da424acf98f625e32534e7f496db8fa4b2fc
Alex P <alex.peshkov@valtech.co.uk>
2016-05-23 11:08:19 +0100

    OLCS-12670 unused code removed

--- test/Common/src/Common/Service/Cqrs/Query/QuerySenderTest.php
+++ test/Common/src/Common/Service/Cqrs/Query/QuerySenderTest.php
@@ -37,7 +37,6 @@ class QuerySenderTest extends MockeryTestCase
     /**
      * @param QueryInterface $query
      * @return \Common\Service\Cqrs\Response
-     * @group test123
      */
     public function testSend()
     {
