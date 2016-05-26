4acc655145a8ddee302e9e03a348b64428207aea
Mat Evans <mat.evans@valtech.co.uk>
2016-05-26 16:31:11 +0100

    OLCS-12869 Max email length increased to 254

--- Common/config/language/en_GB.php
+++ Common/config/language/en_GB.php
@@ -890,7 +890,7 @@ return [
     'edit-condition' => 'Edit condition',
     'edit-conviction' => 'Edit Conviction',
     'edit-undertaking' => 'Edit undertaking',
-    'email-validator.error-message' => 'Email address should be less than 60 characters',
+    'email-validator.error-message' => 'Email address should be less than 254 characters',
     'end-date' => 'End date',
     'entity-details' => 'Entity details',
     'entity-details-nav-back-to-operator-search' => 'Back to operator search',
