9095415478e30a4f0e4fe71a1f261f75d307c48e
Shaun Lizzio <shaun@lizzio.co.uk>
2016-06-06 11:18:06 +0100

    Added UT for filters and date ranges

--- test/Common/src/Common/Data/Object/Search/PublicationSelfserveTest.php
+++ test/Common/src/Common/Data/Object/Search/PublicationSelfserveTest.php
@@ -9,4 +9,37 @@ namespace CommonTest\Data\Object\Search;
 class PublicationSelfserveTest extends SearchAbstractTest
 {
     protected $class = 'Common\Data\Object\Search\PublicationSelfserve';
+
+    private $sut;
+
+    public function setUp()
+    {
+        $this->sut = new $this->class();
+
+        parent::setUp();
+    }
+
+    public function testGetDateRanges()
+    {
+        $dateRanges = $this->sut->getDateRanges();
+
+        $this->assertCount(2, $dateRanges);
+
+        $this->assertInstanceOf('Common\Data\Object\Search\Aggregations\DateRange\PublishedDateFrom', $dateRanges[0]);
+        $this->assertInstanceOf('Common\Data\Object\Search\Aggregations\DateRange\PublishedDateTo', $dateRanges[1]);
+    }
+
+    public function testGetFilters()
+    {
+        $filters = $this->sut->getFilters();
+
+        $this->assertCount(5, $filters);
+
+        $this->assertInstanceOf('Common\Data\Object\Search\Aggregations\Terms\LicenceType', $filters[0]);
+        $this->assertInstanceOf('Common\Data\Object\Search\Aggregations\Terms\TrafficArea', $filters[1]);
+        $this->assertInstanceOf('Common\Data\Object\Search\Aggregations\Terms\PublicationType', $filters[2]);
+        $this->assertInstanceOf('Common\Data\Object\Search\Aggregations\Terms\DocumentStatus', $filters[3]);
+        $this->assertInstanceOf('Common\Data\Object\Search\Aggregations\Terms\PublicationSection', $filters[4]);
+
+    }
 }
