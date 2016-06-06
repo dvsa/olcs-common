2d59c0708d457dedbb6bba7dc2e807f1a3e5296d
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-06-06 12:01:39 +0100

    Form changes to facilitate changes to EBSR uploads

---
+++ Common/src/Common/Form/Model/Fieldset/SubmitButton.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Common\Form\Model\Fieldset;
+
+use Zend\Form\Annotation as Form;
+
+/**
+ * @Form\Name("form-actions")
+ * @Form\Attributes({"class":"actions-container"})
+ */
+class SubmitButton
+{
+    /**
+     * @Form\Attributes({"type":"submit","class":"action--primary large"})
+     * @Form\Options({
+     *     "label": "Submit"
+     * })
+     * @Form\Type("\Common\Form\Elements\InputFilters\ActionButton")
+     */
+    public $submit = null;
+}
