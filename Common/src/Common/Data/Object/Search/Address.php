1a777b2371c8eb40c0d37944ca1093002cc0f035
Alex P <alex.peshkov@valtech.co.uk>
2016-07-26 11:45:59 +0100

    OLCS-13090 code improvements, escaping added

--- Common/src/Common/Data/Object/Search/Address.php
+++ Common/src/Common/Data/Object/Search/Address.php
@@ -3,6 +3,7 @@
 namespace Common\Data\Object\Search;
 
 use Common\Data\Object\Search\Aggregations\Terms as Filter;
+use Common\Util\Escape;
 
 /**
  * Class Address
@@ -86,7 +87,7 @@ class Address extends InternalSearchAbstract
                     return sprintf(
                         '<a href="%s">%s</a>',
                         $urlHelper->fromRoute('operator/business-details', ['organisation' => $data['orgId']]),
-                        $data['orgName']
+                        Escape::html($data['orgName'])
                     );
                 }
             ],
