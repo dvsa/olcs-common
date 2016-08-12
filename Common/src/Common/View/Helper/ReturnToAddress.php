601d62a7c56de003bbf4bbc84c71d6ca5e87bb03
Dmitrij Golubev <dgolubev@DGolubev-pc.local>
2016-08-12 17:59:20 +0100

    OLCS-13435/Fix1: move helper to module Common;

---
+++ Common/src/Common/View/Helper/ReturnToAddress.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Common\View\Helper;
+
+use Zend\View\Helper\AbstractHelper;
+
+/**
+ * Return To Address
+ *
+ * @author Rob Caiger <rob@clocal.co.uk>
+ */
+class ReturnToAddress extends AbstractHelper
+{
+    protected static $addresses = [
+        'ni' => [
+            'Department for Infrastructure',
+            'The Central Licensing Office',
+            'PO Box 180',
+            'Leeds',
+            'LS9 1BU'
+        ],
+        'gb' => [
+            'Office of the Traffic Commissioner',
+            'The Central Licensing Office',
+            'Hillcrest House',
+            '386 Harehills Lane',
+            'Leeds',
+            'LS9 6NF'
+        ]
+    ];
+
+    /**
+     * Print out address
+     *
+     * @param bool   $isNi      Is NI
+     * @param string $separator Default line separator
+     *
+     * @return string
+     */
+    public function __invoke($isNi = false, $separator = ', ')
+    {
+        return self::getAddress($isNi, $separator);
+    }
+
+    /**
+     * Print out address
+     *
+     * @param bool   $isNi      Is NI
+     * @param string $separator Default line separator
+     *
+     * @return string
+     */
+    public static function getAddress($isNi = false, $separator = ', ')
+    {
+        if ($isNi) {
+            return implode($separator, self::$addresses['ni']);
+        }
+
+        return implode($separator, self::$addresses['gb']);
+    }
+}
---
+++ test/Common/src/Common/View/Helper/ReturnToAddressTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace CommonTest\View\Helper;
+
+use Common\View\Helper\ReturnToAddress;
+use Mockery as m;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+
+/**
+ * @covers Common\View\Helper\ReturnToAddress
+ */
+class ReturnToAddressTest extends MockeryTestCase
+{
+    /**
+     * @dataProvider dpTestInvoke
+     */
+    public function testInvoke($isNi, $separator, $expect)
+    {
+        $sut = new ReturnToAddress();
+
+        if ($separator !== null) {
+            $actual = $sut($isNi, $separator);
+            $actualStatic = $sut::getAddress($isNi, $separator);
+        } else {
+            $actual = $sut($isNi);
+            $actualStatic = $sut::getAddress($isNi);
+        }
+
+        static::assertEquals($expect, $actual);
+        static::assertEquals($actual, $actualStatic);
+    }
+
+    public function dpTestInvoke()
+    {
+        return [
+            [
+                'isNi' => false,
+                'separator' => null,
+                'expect' => 'Office of the Traffic Commissioner, The Central Licensing Office, Hillcrest House, ' .
+                    '386 Harehills Lane, Leeds, LS9 6NF',
+            ],
+            [
+                'isNi' => false,
+                'separator' => '</br>',
+                'expect' => 'Office of the Traffic Commissioner</br>' .
+                    'The Central Licensing Office</br>Hillcrest House</br>' .
+                    '386 Harehills Lane</br>Leeds</br>LS9 6NF',
+            ],
+            [
+                'isNi' => true,
+                'separator' => null,
+                'expect' => 'Department for Infrastructure, The Central Licensing Office, PO Box 180, Leeds, LS9 1BU',
+            ],
+            [
+                'isNi' => true,
+                'separator' => '<br />',
+                'expect' => 'Department for Infrastructure<br />' .
+                    'The Central Licensing Office<br />PO Box 180<br />Leeds<br />LS9 1BU',
+            ],
+        ];
+    }
+}
