acc232684867621db6ac484e93c86bf14d54401b
Edmund Reed <esr360@live.com>
2016-11-07 16:21:23 +0000

    ensuring modalised behaviour on internal

--- Common/src/Common/assets/js/inline/lva-crud-delta.js
+++ Common/src/Common/assets/js/inline/lva-crud-delta.js
@@ -28,14 +28,6 @@ OLCS.ready(function() {
     }
   });
 
-  // @TODO: there is a later story to modalise LVA behaviour
-  // internally. For now, the easiest way to suppress it
-  // is to short circuit based on the app class. Not pretty,
-  // but will be removed early 2015
-  if (document.body.className.search("selfserve") === -1) {
-    return;
-  }
-
   OLCS.crudTableHandler({
       selector : '.trigger-modal'
   });
--- Common/src/Common/assets/js/inline/lva-crud.js
+++ Common/src/Common/assets/js/inline/lva-crud.js
@@ -1,12 +1,6 @@
 OLCS.ready(function() {
+    
   "use strict";
-  // @TODO: there is a later story to modalise LVA behaviour
-  // internally. For now, the easiest way to suppress it
-  // is to short circuit based on the app class. Not pretty,
-  // but will be removed early 2015
-  if (document.body.className.search("selfserve") === -1) {
-    return;
-  }
 
   var tableSelector = "form [data-group*='table']";
 
