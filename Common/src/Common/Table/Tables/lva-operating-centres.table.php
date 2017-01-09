573ca0d110e3ffadef86b30caf1552abea90688c
Edmund Reed <esr360@live.com>
2017-01-09 12:13:05 +0000

    removing table titles

--- Common/src/Common/Table/Tables/lva-operating-centres.table.php
+++ Common/src/Common/Table/Tables/lva-operating-centres.table.php
@@ -2,7 +2,7 @@
 
 return array(
     'variables' => array(
-        'title' => 'application_operating-centres_authorisation.table.title',
+        'title' => '',
         'empty_message' => 'application_operating-centres_authorisation-tableEmptyMessage',
         'within_form' => true,
     ),
