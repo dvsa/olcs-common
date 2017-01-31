352356c863f65e76a31c39c00c11d676e50f0ead
Mat Evans <mat.evans@valtech.co.uk>
2017-01-31 14:51:50 +0000

    Revert "adding people validation"
    This reverts commit 152c22f995fba31c263859e132ebdd79844fc67a.

--- Common/src/Common/Form/Model/Form/Lva/Fieldset/Person.php
+++ Common/src/Common/Form/Model/Form/Lva/Fieldset/Person.php
@@ -10,88 +10,86 @@ use Zend\Form\Annotation as Form;
 class Person
 {
     /**
-     * @Form\Type("Hidden")
      * @Form\Attributes({"value":""})
+     * @Form\Type("Hidden")
      */
     public $id = null;
 
     /**
-     * @Form\Type("Hidden")
      * @Form\Attributes({"value":""})
+     * @Form\Type("Hidden")
      */
     public $version = null;
 
     /**
-     * @Form\Type("DynamicSelect")
-     * @Form\Required(false)
      * @Form\Attributes({"id":"title","placeholder":""})
      * @Form\Options({
-     *     "empty_option":"Please Select",
-     *     "label":"application_your-business_people-sub-action-formTitle",
-     *     "label_attributes":{"aria-label": "Select title"},
+     *     "empty_option": "Please Select",
+     *     "label": "application_your-business_people-sub-action-formTitle",
+     *     "label_attributes": {
+     *         "aria-label": "Select title"
+     *     },
      *     "category":"person_title",
      * })
+     * @Form\Type("DynamicSelect")
+     * @Form\Required(false)
      */
     public $title = null;
 
     /**
-     * @Form\Type("Text")
      * @Form\Attributes({"class":"long","id":"forename"})
      * @Form\Options({
      *     "label":"application_your-business_people-sub-action-formFirstName",
-     *     "label_attributes": {"aria-label": "Enter first names"},
+     *     "label_attributes": {
+     *         "aria-label": "Enter first names"
+     *     },
      *     "error-message": "person_forename-error"
      * })
+     * @Form\Type("Text")
      * @Form\Filter({"name":"Zend\Filter\StringTrim"})
      * @Form\Validator({"name":"Zend\Validator\StringLength","options":{"min":0,"max":35}})
-     * @Form\Validator({"name":"regex", 
-     *     "options":{"pattern":"/^[a-z ,.'-]+$/i","messages":{"regexNotMatch":"error.characters.not-allowed"}}
-     * })
      */
     public $forename = null;
 
     /**
-     * @Form\Type("Text")
      * @Form\Attributes({"class":"long","id":"familyname"})
      * @Form\Options({
-     *     "label":"application_your-business_people-sub-action-formSurname",
-     *     "label_attributes": {"aria-label": "Enter last name"},
+     *    "label":"application_your-business_people-sub-action-formSurname",
+     *     "label_attributes": {
+     *         "aria-label": "Enter last name"
+     *     },
      *     "error-message": "person_familyName-error"
      * })
+     * @Form\Type("Text")
      * @Form\Filter({"name":"Zend\Filter\StringTrim"})
      * @Form\Validator({"name":"Zend\Validator\StringLength","options":{"min":0,"max":35}})
-     * @Form\Validator({"name":"regex", 
-     *     "options":{"pattern":"/^[a-z ,.'-]+$/i","messages":{"regexNotMatch":"error.characters.not-allowed"}}
-     * })
      */
     public $familyName = null;
 
     /**
-     * @Form\Type("Text")
-     * @Form\Required(false)
      * @Form\Attributes({"class":"long","id":""})
      * @Form\Options({
-     *     "label":"application_your-business_people-sub-action-formOtherNames",
-     *     "label_attributes": {"aria-label": "Enter other names (optional)"}
+     *    "label":"application_your-business_people-sub-action-formOtherNames",
+     *     "label_attributes": {
+     *         "aria-label": "Enter other names (optional)"
+     *     }
      * })
+     * @Form\Required(false)
+     * @Form\Type("Text")
      * @Form\Validator({"name":"Zend\Validator\StringLength","options":{"min":0,"max":35}})
-     * @Form\Validator({"name":"regex", 
-     *     "options":{"pattern":"/^[a-z ,.'-]+$/i","messages":{"regexNotMatch":"error.characters.not-allowed"}}
-     * })
      */
     public $otherName = null;
 
     /**
-     * @Form\Type("Text")
      * @Form\Attributes({"class":"long","id":""})
      * @Form\Options({"label":"application_your-business_people-sub-action-formPosition"})
+     * @Form\Type("Text")
      * @Form\Filter({"name":"Zend\Filter\StringTrim"})
      * @Form\Validator({"name":"Zend\Validator\StringLength","options":{"min":0,"max":45}})
      */
     public $position = null;
 
     /**
-     * @Form\Type("DateSelect")
      * @Form\Attributes({"id":"dob"})
      * @Form\Options({
      *     "label": "dob",
@@ -100,6 +98,7 @@ class Person
      *     "error-message": "person_birthDate-error",
      *     "fieldset-attributes": {"id":"dob_day"}
      * })
+     * @Form\Type("DateSelect")
      * @Form\Filter({"name": "DateSelectNullifier"})
      * @Form\Validator({"name": "\Common\Validator\Date"})
      * @Form\Validator({"name":"Date","options":{"format":"Y-m-d"}})
