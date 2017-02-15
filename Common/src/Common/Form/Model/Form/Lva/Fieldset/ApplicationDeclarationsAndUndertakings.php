211ad79022ebeeca536ba6d8b49bdbcc9dbb8c2a
Alex P <alex.peshkov@valtech.co.uk>
2017-02-15 10:43:14 +0000

    OLCS-15299 Adapt checkbox within the declaration page due to verify

--- Common/src/Common/Form/Model/Form/Lva/Fieldset/ApplicationDeclarationsAndUndertakings.php
+++ Common/src/Common/Form/Model/Form/Lva/Fieldset/ApplicationDeclarationsAndUndertakings.php
@@ -51,20 +51,6 @@ class ApplicationDeclarationsAndUndertakings
     public $declarationDownload = null;
 
     /**
-     * @Form\Options({
-     *     "checked_value": "Y",
-     *     "unchecked_value": "N",
-     *     "must_be_value": "Y",
-     *     "label": "application.review-declarations.confirm-verify-text",
-     *     "short-label": "application.review-declarations.confirm-short-label",
-     *     "label_attributes": {"id": "label-declarationConfirmation"}
-     * })
-     * @Form\Attributes({"data-container-class": "confirm checkbox"})
-     * @Form\Type("\Common\Form\Elements\InputFilters\SingleCheckbox")
-     */
-    public $declarationConfirmation = null;
-
-    /**
      * @Form\Attributes({"value":""})
      * @Form\Type("Hidden")
      */
