3ff5f2c52eb0e7889e3ff2987c927d2ec0bccb31
Piotr Filak <piotr.filak@valtech.co.uk>
2017-02-15 13:30:26 +0000

    OLCS-15317 - Script to generate missing form unit tests

---
+++ Scripts/CreateFormUnitTests.php
@@ -0,0 +1,134 @@
+<?php
+// @codingStandardsIgnoreFile
+
+/**
+ * Class to create form unit test files
+ */
+class CreateFromUnitTests
+{
+    const UT_TEMPLATE_FILE_NAME = 'FormUnitTestTemplate.php';
+
+    private $path;
+
+    /**
+     * Set path to dir with forms
+     *
+     * @param string $path Path to dir with forms
+     *
+     * @return $this
+     */
+    public function setSourcePath($path)
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    /**
+     * Create missing form unit tests
+     *
+     * @return void
+     */
+    public function exec()
+    {
+        // load the template
+        $template = file_get_contents(__DIR__ . '/'. self::UT_TEMPLATE_FILE_NAME);
+
+        // get list of existing forms
+        $pathToForms = realpath(__DIR__ . '/'. $this->path);
+        $formsList = $this->getDirContents($pathToForms);
+
+        foreach($formsList as $formPath) {
+            print "Form: $formPath\n";
+
+            // set test path
+            $testPath = strtr(
+                $formPath,
+                [
+                    '/module/' => '/test/',
+                    '/Common/src/Common/' => '/test/Common/src/Common/',
+                    '.php' => 'Test.php',
+                ]
+            );
+            print "Test: $testPath\n";
+
+            // set test dir path
+            $testDirPath = dirname($testPath);
+
+            if (!is_dir($testDirPath)) {
+                // create the folder
+                mkdir($testDirPath, 0777, true);
+            }
+
+            if (!file_exists($testPath)) {
+                // create the test
+                $testParts = [];
+                preg_match('/.+\/test\/([^\/]+)\/src\/(.+)/', $testDirPath, $testParts);
+                $testNameSpace = str_replace(
+                    ['/', 'CommonTest\Common'],
+                    ['\\', 'CommonTest'],
+                    $testParts[1].'Test/'.$testParts[2]
+                );
+                $formClassName = str_replace(
+                    ['/', 'Common\Common'],
+                    ['\\', 'Common'],
+                    '/'.$testParts[1].'/'.$testParts[2].'/'.basename($formPath, '.php')
+                );
+
+                file_put_contents(
+                    $testPath,
+                    strtr(
+                        $template,
+                        [
+                            'R_NAMESPACE' => $testNameSpace,
+                            'R_TEST_CLASS_NAME' => basename($testPath, '.php'),
+                            'R_FORM_CLASS_NAME' => $formClassName,
+                        ]
+                    )
+                );
+                print "Created\n";
+            } else {
+                print "Already exists\n";
+            }
+
+            print "\n";
+        }
+    }
+
+    /**
+     * Get dir contents
+     *
+     * @param string $dir     Path to dir
+     * @param array  $results Results
+     *
+     * @return array
+     */
+    private function getDirContents($dir, &$results = [])
+    {
+        $files = scandir($dir);
+
+        foreach ($files as $value) {
+            $path = realpath($dir.DIRECTORY_SEPARATOR.$value);
+
+            if(!is_dir($path)) {
+                $results[] = $path;
+            } elseif($value != "." && $value != "..") {
+                $this->getDirContents($path, $results);
+            }
+        }
+
+        return $results;
+    }
+}
+
+/*
+ * Call php CreateFormUnitTests.php <path-to-dir-with-forms>
+ */
+if (empty($argv[1])) {
+    print "usage: php CreateFormUnitTests.php <path-to-dir-with-forms>\n";
+    exit;
+}
+
+(new CreateFromUnitTests)
+    ->setSourcePath($argv[1])
+    ->exec();
---
+++ Scripts/FormUnitTestTemplate.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace R_NAMESPACE;
+
+use Olcs\TestHelpers\FormTester\AbstractFormValidationTestCase;
+
+/**
+ * Class R_TEST_CLASS_NAME
+ *
+ * @group FormTests
+ */
+class R_TEST_CLASS_NAME extends AbstractFormValidationTestCase
+{
+    /**
+     * @var string The class name of the form being tested
+     */
+    protected $formName = R_FORM_CLASS_NAME::class;
+}
