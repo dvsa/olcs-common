a0fa0cfbbe37f55fb98abe3e64127308e535990e
Mat Evans <mat.evans@valtech.co.uk>
2017-02-17 15:06:44 +0000

    OLCS-15295 Log missing Welsh translation

--- Common/src/Common/Util/TranslatorDelegator.php
+++ Common/src/Common/Util/TranslatorDelegator.php
@@ -7,11 +7,8 @@
  */
 namespace Common\Util;
 
-use Zend\EventManager\EventManagerInterface;
-use Zend\I18n\Translator\Translator as ZendTranslator;
 use Zend\I18n\Translator\TranslatorInterface;
 use Zend\Mvc\I18n\Translator;
-use Zend\ServiceManager\ServiceLocatorInterface;
 
 /**
  * Translator Delegator
@@ -21,7 +18,7 @@ use Zend\ServiceManager\ServiceLocatorInterface;
 class TranslatorDelegator extends Translator
 {
     /**
-     * @var ZendTranslator
+     * @var TranslatorInterface
      */
     protected $translator;
 
@@ -31,49 +28,53 @@ class TranslatorDelegator extends Translator
     private $replacements;
 
     /**
-     * @var TranslatorLogger
+     * TranslatorDelegator constructor.
+     *
+     * @param TranslatorInterface $translator   Transloator
+     * @param array               $replacements Array of tokens that can be replaced in translations
      */
-    private $translationLogger;
-
     public function __construct(TranslatorInterface $translator, array $replacements)
     {
         $this->translator = $translator;
         $this->replacements = $replacements;
     }
 
+    /**
+     * Proxy to any translator methods
+     *
+     * @param string $method Method to call
+     * @param array  $args   Methods arguments
+     *
+     * @return mixed
+     */
     public function __call($method, array $args)
     {
         return call_user_func_array([$this->translator, $method], $args);
     }
 
+    /**
+     * Translate a message
+     *
+     * @param string $message    Message to be translated
+     * @param string $textDomain Domain for translations
+     * @param string $locale     Locale to be translated to
+     *
+     * @return string
+     */
     public function translate($message, $textDomain = 'default', $locale = null)
     {
-        if ($this->translationLogger !== null) {
-            $this->translationLogger->logTranslations(
-                $message,
-                $this->translator->translate($message, $textDomain, 'en_GB'),
-                $this->translator->translate($message, $textDomain, 'cy_GB')
-            );
-        }
-
         return $this->replaceVariables($this->translator->translate($message, $textDomain, $locale));
     }
 
+    /**
+     * Replace token in translated string
+     *
+     * @param string $message Message being translated
+     *
+     * @return string
+     */
     protected function replaceVariables($message)
     {
         return str_replace(array_keys($this->replacements), array_values($this->replacements), $message);
-    }
-
-    /**
-     * Enable logging translations
-     *
-     * @param string                            $logFileName File name to write to
-     * @param \Zend\Http\PhpEnvironment\Request $request     Request
-     *
-     * @return void
-     */
-    public function enabledLogging($logFileName, \Zend\Http\PhpEnvironment\Request $request)
-    {
-        $this->translationLogger = new TranslatorLogger($logFileName, $request);
     }
 }
