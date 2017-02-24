0799fdb9e8d449209090e12c22b6e3d541adf273
Piotr Filak <piotr.filak@valtech.co.uk>
2017-02-24 15:48:50 +0000

    OLCS-15440 - Implement UT form validation for Common

--- test/Common/src/Common/Form/Model/Form/GenerateContinuationTest.php
+++ test/Common/src/Common/Form/Model/Form/GenerateContinuationTest.php
@@ -15,4 +15,34 @@ class GenerateContinuationTest extends AbstractFormValidationTestCase
      * @var string The class name of the form being tested
      */
     protected $formName = \Common\Form\Model\Form\GenerateContinuation::class;
+
+    public function testType()
+    {
+        $element = ['details', 'type'];
+        $this->assertFormElementRequired($element, true);
+        $this->assertFormElementAllowEmpty($element, false);
+        $this->assertFormElementValid($element, 'operator');
+        $this->assertFormElementValid($element, 'irfo');
+    }
+
+    public function testDate()
+    {
+        $element = ['details', 'date'];
+        $this->assertFormElementRequired($element, false);
+        $this->assertFormElementMonthSelect($element);
+    }
+
+    public function testTrafficArea()
+    {
+        $element = ['details', 'trafficArea'];
+        $this->assertFormElementRequired($element, true);
+        $this->assertFormElementAllowEmpty($element, false);
+        $this->assertFormElementDynamicSelect($element);
+    }
+
+    public function testGenerate()
+    {
+        $element = ['form-actions', 'generate'];
+        $this->assertFormElementActionButton($element);
+    }
 }
--- test/Common/src/Common/Form/Model/Form/Lva/CommunityLicencesAddTest.php
+++ test/Common/src/Common/Form/Model/Form/Lva/CommunityLicencesAddTest.php
@@ -15,4 +15,24 @@ class CommunityLicencesAddTest extends AbstractFormValidationTestCase
      * @var string The class name of the form being tested
      */
     protected $formName = \Common\Form\Model\Form\Lva\CommunityLicencesAdd::class;
+
+    public function testTotal()
+    {
+        $element = ['data', 'total'];
+        $this->assertFormElementRequired($element, true);
+        $this->assertFormElementAllowEmpty($element, false);
+        $this->assertFormElementNumber($element, 1, null, \Zend\Validator\GreaterThan::NOT_GREATER);
+    }
+
+    public function testSubmit()
+    {
+        $element = ['form-actions', 'submit'];
+        $this->assertFormElementActionButton($element);
+    }
+
+    public function testCancel()
+    {
+        $element = ['form-actions', 'cancel'];
+        $this->assertFormElementActionButton($element);
+    }
 }
