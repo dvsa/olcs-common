356bdcecc0df2fc14dadd56bae485c4f7e11b15e
Gaurav Malhotra <gaurav.malhotra@valtech.co.uk>
2017-03-13 12:59:05 +0000

    Community licences restore test

--- test/Common/src/Common/Form/Model/Form/Lva/CommunityLicencesRestoreTest.php
+++ test/Common/src/Common/Form/Model/Form/Lva/CommunityLicencesRestoreTest.php
@@ -15,4 +15,20 @@ class CommunityLicencesRestoreTest extends AbstractFormValidationTestCase
      * @var string The class name of the form being tested
      */
     protected $formName = \Common\Form\Model\Form\Lva\CommunityLicencesRestore::class;
+
+    public function testConfirm()
+    {
+        $element = [ 'data', 'confirm' ];
+        $this->assertFormElementHtml($element);
+    }
+
+    public function testActionButtons()
+    {
+        $element = [ 'form-actions', 'submit' ];
+        $this->assertFormElementActionButton($element);
+
+        $element = [ 'form-actions', 'cancel' ];
+        $this->assertFormElementActionButton($element);
+    }
+
 }
