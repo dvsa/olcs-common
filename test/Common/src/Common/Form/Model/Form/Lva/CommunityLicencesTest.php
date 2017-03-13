7831aa5bd0dea5cf3050e30bb5dc54ae2b6ed53b
Gaurav Malhotra <gaurav.malhotra@valtech.co.uk>
2017-03-13 13:13:39 +0000

    OLCS-15862 community licences

--- test/Common/src/Common/Form/Model/Form/Lva/CommunityLicencesTest.php
+++ test/Common/src/Common/Form/Model/Form/Lva/CommunityLicencesTest.php
@@ -15,4 +15,49 @@ class CommunityLicencesTest extends AbstractFormValidationTestCase
      * @var string The class name of the form being tested
      */
     protected $formName = \Common\Form\Model\Form\Lva\CommunityLicences::class;
+
+    public function testTotalCommunityLicences()
+    {
+        $element = [ 'data', 'totalCommunityLicences' ];
+        $this->assertFormElementText($element);
+        $this->assertFormElementAllowEmpty($element, true);
+    }
+
+    public function testTable()
+    {
+        $element = [ 'table', 'table' ];
+        $this->assertFormElementRequired($element, false);
+        $this->assertFormElementAllowEmpty($element, true);
+        $this->assertFormElementTable($element);
+    }
+
+    public function testRows()
+    {
+        $element = [ 'table', 'rows' ];
+        $this->assertFormElementHidden($element);
+    }
+
+    public function testTableId()
+    {
+        $element = [ 'table', 'id' ];
+        $this->assertFormElementHidden($element);
+    }
+
+    public function testTableAction()
+    {
+        $element = [ 'table', 'action' ];
+        $this->assertFormElementHidden($element);
+    }
+
+    public function testActionButtons()
+    {
+        $element = [ 'form-actions', 'saveAndContinue' ];
+        $this->assertFormElementActionButton($element);
+
+        $element = [ 'form-actions', 'save' ];
+        $this->assertFormElementActionButton($element);
+
+        $element = [ 'form-actions', 'cancel' ];
+        $this->assertFormElementActionButton($element);
+    }
 }
