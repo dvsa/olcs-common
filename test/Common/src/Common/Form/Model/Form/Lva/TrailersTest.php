2729ca35da54d28f1d5167ddd6c10bc8167e044d
Gaurav Malhotra <gaurav.malhotra@valtech.co.uk>
2017-03-14 13:39:25 +0000

    OLCS-16046 trailers form test

--- test/Common/src/Common/Form/Model/Form/Lva/TrailersTest.php
+++ test/Common/src/Common/Form/Model/Form/Lva/TrailersTest.php
@@ -3,6 +3,7 @@
 namespace CommonTest\Form\Model\Form\Lva;
 
 use Olcs\TestHelpers\FormTester\AbstractFormValidationTestCase;
+use Common\Form\Elements\InputFilters\SingleCheckbox;
 
 /**
  * Class TrailersTest
@@ -15,4 +16,56 @@ class TrailersTest extends AbstractFormValidationTestCase
      * @var string The class name of the form being tested
      */
     protected $formName = \Common\Form\Model\Form\Lva\Trailers::class;
+
+    public function testTableTable()
+    {
+        $element = ['table', 'table'];
+        $this->assertFormElementRequired($element, false);
+        $this->assertFormElementAllowEmpty($element, true);
+        $this->assertFormElementTable($element);
+    }
+
+    public function testTableAction()
+    {
+        $element = ['table', 'action'];
+        $this->assertFormElementNoRender($element);
+    }
+
+    public function testTableRows()
+    {
+        $element = ['table', 'rows'];
+        $this->assertFormElementHidden($element);
+    }
+
+    public function testTableId()
+    {
+        $element = ['table', 'id'];
+        $this->assertFormElementNoRender($element);
+    }
+
+    public function testTrailersShareInfo()
+    {
+        $element = ['trailers', 'shareInfo'];
+        $this->assertFormElementType($element, SingleCheckbox::class);
+        $this->assertFormElementValid($element, 'Y');
+        $this->assertFormElementValid($element, 'N');
+    }
+
+    public function testSaveAndContinue()
+    {
+        $element = ['form-actions', 'saveAndContinue'];
+        $this->assertFormElementActionButton($element);
+    }
+
+    public function testSave()
+    {
+        $element = ['form-actions', 'save'];
+        $this->assertFormElementActionButton($element);
+    }
+
+    public function testCancel()
+    {
+        $element = ['form-actions', 'cancel'];
+        $this->assertFormElementActionButton($element);
+    }
 }
