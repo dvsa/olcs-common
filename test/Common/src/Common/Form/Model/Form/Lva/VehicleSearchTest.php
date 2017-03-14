d0a196882efe19fa4171d12a7dc3b76ad31bce01
Gaurav Malhotra <gaurav.malhotra@valtech.co.uk>
2017-03-14 14:20:35 +0000

    OLCS-16047 vehicle search filter form test

--- test/Common/src/Common/Form/Model/Form/Lva/VehicleSearchTest.php
+++ test/Common/src/Common/Form/Model/Form/Lva/VehicleSearchTest.php
@@ -15,4 +15,35 @@ class VehicleSearchTest extends AbstractFormValidationTestCase
      * @var string The class name of the form being tested
      */
     protected $formName = \Common\Form\Model\Form\Lva\VehicleSearch::class;
+
+    public function testIncludeRemoved()
+    {
+        $element = ['includeRemoved'];
+        $this->assertFormElementHidden($element);
+    }
+
+    public function testLimit()
+    {
+        $element = ['limit'];
+        $this->assertFormElementHidden($element);
+    }
+
+    public function testVehicleVrm()
+    {
+        $element = ['vehicleSearch', 'vrm'];
+        $this->assertFormElementText($element);
+        $this->assertFormElementRequired($element, true);
+    }
+
+    public function testFilterButton()
+    {
+        $element = ['vehicleSearch', 'filter'];
+        $this->assertFormElementActionButton($element);
+    }
+
+    public function testClearSearchButton()
+    {
+        $element = ['vehicleSearch', 'clearSearch'];
+        $this->assertFormElementActionButton($element);
+    }
 }
