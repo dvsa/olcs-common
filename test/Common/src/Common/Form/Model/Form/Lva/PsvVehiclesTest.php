215358d2a4147142b4ea80df51b5037ab568bfd6
Gaurav Malhotra <gaurav.malhotra@valtech.co.uk>
2017-03-14 11:25:41 +0000

    OLCS-16039 psv vehicles form test

--- test/Common/src/Common/Form/Model/Form/Lva/PsvVehiclesTest.php
+++ test/Common/src/Common/Form/Model/Form/Lva/PsvVehiclesTest.php
@@ -3,6 +3,8 @@
 namespace CommonTest\Form\Model\Form\Lva;
 
 use Olcs\TestHelpers\FormTester\AbstractFormValidationTestCase;
+use Zend\Form\Element\Radio;
+use Zend\Form\Element\Checkbox;
 
 /**
  * Class PsvVehiclesTest
@@ -15,4 +17,60 @@ class PsvVehiclesTest extends AbstractFormValidationTestCase
      * @var string The class name of the form being tested
      */
     protected $formName = \Common\Form\Model\Form\Lva\PsvVehicles::class;
+
+    public function testVersion()
+    {
+        $element = [ 'data', 'version' ];
+        $this->assertFormElementHidden($element);
+    }
+
+    public function testHasEnteredReg()
+    {
+        $element = [ 'data', 'hasEnteredReg' ];
+        $this->assertFormElementType($element, Radio::class);
+        $this->assertFormElementAllowEmpty($element, false);
+    }
+
+    public function testVehiclesTable()
+    {
+        $element = [ 'vehicles', 'table' ];
+        $this->assertFormElementTable($element);
+        $this->assertFormElementRequired($element, false);
+        $this->assertFormElementAllowEmpty($element, true);
+
+        $element = [ 'vehicles', 'action' ];
+        $this->assertFormElementHidden($element);
+
+        $element = [ 'vehicles', 'rows' ];
+        $this->assertFormElementHidden($element);
+
+        $element = [ 'vehicles', 'id' ];
+        $this->assertFormElementHidden($element);
+    }
+
+    public function testShareInfo()
+    {
+        $element = [ 'shareInfo', 'shareInfo' ];
+        $this->assertFormElementType($element, Checkbox::class);
+        $this->assertFormElementValid($element, 'Y');
+        $this->assertFormElementValid($element, 'N');
+    }
+
+    public function testSaveAndContinue()
+    {
+        $element = ['form-actions', 'saveAndContinue'];
+        $this->assertFormElementActionButton($element);
+    }
+
+    public function testSave()
+    {
+        $element = ['form-actions', 'save'];
+        $this->assertFormElementActionButton($element);
+    }
+
+    public function testCancel()
+    {
+        $element = ['form-actions', 'cancel'];
+        $this->assertFormElementActionButton($element);
+    }
 }
