1d4fab9a88614dd38f351bf51fae85fea53f3fba
Gaurav Malhotra <gaurav.malhotra@valtech.co.uk>
2017-03-14 13:58:51 +0000

    OLCS-16047 type of licence form test

--- test/Common/src/Common/Form/Model/Form/Lva/TypeOfLicenceTest.php
+++ test/Common/src/Common/Form/Model/Form/Lva/TypeOfLicenceTest.php
@@ -3,6 +3,7 @@
 namespace CommonTest\Form\Model\Form\Lva;
 
 use Olcs\TestHelpers\FormTester\AbstractFormValidationTestCase;
+use Zend\Form\Element\Radio;
 
 /**
  * Class TypeOfLicenceTest
@@ -15,4 +16,49 @@ class TypeOfLicenceTest extends AbstractFormValidationTestCase
      * @var string The class name of the form being tested
      */
     protected $formName = \Common\Form\Model\Form\Lva\TypeOfLicence::class;
+
+    public function testVersion()
+    {
+        $element = ['version'];
+        $this->assertFormElementHidden($element);
+    }
+
+    public function testOperatorLocation()
+    {
+        $element = ['type-of-licence', 'operator-location'];
+        $this->assertFormElementType($element, Radio::class);
+        $this->assertFormElementRequired($element, true);
+    }
+
+    public function testOperatorType()
+    {
+        $element = ['type-of-licence', 'operator-type'];
+        $this->assertFormElementType($element, Radio::class);
+        $this->assertFormElementRequired($element, true);
+    }
+
+    public function testLicenceType()
+    {
+        $element = ['type-of-licence', 'licence-type'];
+        $this->assertFormElementType($element, Radio::class);
+        $this->assertFormElementRequired($element, true);
+    }
+
+    public function testSaveAndContinue()
+    {
+        $element = ['form-actions', 'saveAndContinue'];
+        $this->assertFormElementActionButton($element);
+    }
+
+    public function testSave()
+    {
+        $element = ['form-actions', 'save'];
+        $this->assertFormElementActionButton($element);
+    }
+
+    public function testCancel()
+    {
+        $element = ['form-actions', 'cancel'];
+        $this->assertFormElementActionButton($element);
+    }
 }
