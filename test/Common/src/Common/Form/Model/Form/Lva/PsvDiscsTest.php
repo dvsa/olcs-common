cf443c3d7b226493c46b1ea906e460d8b01bf1aa
Gaurav Malhotra <gaurav.malhotra@valtech.co.uk>
2017-03-14 11:12:42 +0000

    OLCS-16039 psv discs form test

--- test/Common/src/Common/Form/Model/Form/Lva/PsvDiscsTest.php
+++ test/Common/src/Common/Form/Model/Form/Lva/PsvDiscsTest.php
@@ -15,4 +15,20 @@ class PsvDiscsTest extends AbstractFormValidationTestCase
      * @var string The class name of the form being tested
      */
     protected $formName = \Common\Form\Model\Form\Lva\PsvDiscs::class;
+
+    public function testTable()
+    {
+        $element = [ 'table', 'table' ];
+        $this->assertFormElementTable($element);
+        $this->assertFormElementNotValid($element, null, [ 'required' ]);
+
+        $element = [ 'table', 'action' ];
+        $this->assertFormElementHidden($element);
+
+        $element = [ 'table', 'rows' ];
+        $this->assertFormElementHidden($element);
+
+        $element = [ 'table', 'id' ];
+        $this->assertFormElementHidden($element);
+    }
 }
