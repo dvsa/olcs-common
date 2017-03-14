91e885e4f79a4ea5bb84727b211aebfcb5f034f3
Gaurav Malhotra <gaurav.malhotra@valtech.co.uk>
2017-03-14 09:39:07 +0000

    OLCS-16037 people form test

--- test/Common/src/Common/Form/Model/Form/Lva/PeopleTest.php
+++ test/Common/src/Common/Form/Model/Form/Lva/PeopleTest.php
@@ -15,4 +15,48 @@ class PeopleTest extends AbstractFormValidationTestCase
      * @var string The class name of the form being tested
      */
     protected $formName = \Common\Form\Model\Form\Lva\People::class;
+
+    public function testTable()
+    {
+        $element = ['table', 'table'];
+        $this->assertFormElementRequired($element, false);
+        $this->assertFormElementAllowEmpty($element, true);
+        $this->assertFormElementTable($element);
+    }
+
+    public function testTableAction()
+    {
+        $element = ['table', 'action'];
+        $this->assertFormElementNoRender($element);
+    }
+
+    public function testTableRows()
+    {
+        $element = ['table', 'rows'];
+        $this->assertFormElementHidden($element);
+    }
+
+    public function testTableId()
+    {
+        $element = ['table', 'id'];
+        $this->assertFormElementNoRender($element);
+    }
+
+    public function testSaveAndContinue()
+    {
+        $element = ['form-actions', 'saveAndContinue'];
+        $this->assertFormElementActionButton($element);
+    }
+
+    public function testSave()
+    {
+        $element = ['form-actions', 'save'];
+        $this->assertFormElementActionButton($element);
+    }
+
+    public function testCancel()
+    {
+        $element = ['form-actions', 'cancel'];
+        $this->assertFormElementActionButton($element);
+    }
 }
