db7bd50c78c27d25f98c7dd11926b67ef4af370d
Gaurav Malhotra <gaurav.malhotra@valtech.co.uk>
2017-03-14 13:14:12 +0000

    OLCS-16046 taxi phv form test

--- test/Common/src/Common/Form/Model/Form/Lva/TaxiPhvTest.php
+++ test/Common/src/Common/Form/Model/Form/Lva/TaxiPhvTest.php
@@ -3,6 +3,8 @@
 namespace CommonTest\Form\Model\Form\Lva;
 
 use Olcs\TestHelpers\FormTester\AbstractFormValidationTestCase;
+use Zend\Form\Element\Select;
+use Common\Form\Elements\Types\TrafficAreaSet;
 
 /**
  * Class TaxiPhvTest
@@ -15,4 +17,68 @@ class TaxiPhvTest extends AbstractFormValidationTestCase
      * @var string The class name of the form being tested
      */
     protected $formName = \Common\Form\Model\Form\Lva\TaxiPhv::class;
+
+    public function testTable()
+    {
+        $element = ['table', 'table'];
+        $this->assertFormElementTable($element);
+        $this->assertFormElementNotValid($element, null, ['required']);
+    }
+
+    public function testTableAction()
+    {
+        $element = ['table', 'action'];
+        $this->assertFormElementNoRender($element);
+    }
+
+    public function testTableRows()
+    {
+        $element = ['table', 'rows'];
+        $this->assertFormElementHidden($element);
+    }
+
+    public function testTableId()
+    {
+        $element = ['table', 'id'];
+        $this->assertFormElementNoRender($element);
+    }
+
+    public function testTrafficArea()
+    {
+        $element = ['dataTrafficArea', 'trafficArea'];
+        $this->assertFormElementType($element, Select::class);
+        $this->assertFormElementRequired($element, true);
+    }
+
+    public function testTrafficAreaSet()
+    {
+        $element = ['dataTrafficArea', 'trafficAreaSet'];
+        $this->assertFormElementType($element, TrafficAreaSet::class);
+        $this->assertFormElementRequired($element, false);
+    }
+
+    public function testEnforcementArea()
+    {
+        $element = ['dataTrafficArea', 'enforcementArea'];
+        $this->assertFormElementType($element, Select::class);
+        $this->assertFormElementRequired($element, false);
+    }
+
+    public function testSaveAndContinue()
+    {
+        $element = ['form-actions', 'saveAndContinue'];
+        $this->assertFormElementActionButton($element);
+    }
+
+    public function testSave()
+    {
+        $element = ['form-actions', 'save'];
+        $this->assertFormElementActionButton($element);
+    }
+
+    public function testCancel()
+    {
+        $element = ['form-actions', 'cancel'];
+        $this->assertFormElementActionButton($element);
+    }
 }
