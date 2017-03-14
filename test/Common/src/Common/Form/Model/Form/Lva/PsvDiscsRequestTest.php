15b9efa82685a949b8a4dcf998dca763dd788abd
Gaurav Malhotra <gaurav.malhotra@valtech.co.uk>
2017-03-14 10:53:12 +0000

    OLCS-16039 psv disc request form

--- test/Common/src/Common/Form/Model/Form/Lva/PsvDiscsRequestTest.php
+++ test/Common/src/Common/Form/Model/Form/Lva/PsvDiscsRequestTest.php
@@ -15,4 +15,28 @@ class PsvDiscsRequestTest extends AbstractFormValidationTestCase
      * @var string The class name of the form being tested
      */
     protected $formName = \Common\Form\Model\Form\Lva\PsvDiscsRequest::class;
+
+    public function testAdditionalDiscs()
+    {
+        $element = [ 'data', 'additionalDiscs' ];
+        $this->assertFormElementAllowEmpty($element, true);
+    }
+
+    public function testCancel()
+    {
+        $element = ['form-actions', 'cancel'];
+        $this->assertFormElementActionButton($element);
+    }
+
+    public function testSubmit()
+    {
+        $element = ['form-actions', 'submit'];
+        $this->assertFormElementActionButton($element);
+    }
+
+    public function testAddAnother()
+    {
+        $element = ['form-actions', 'addAnother'];
+        $this->assertFormElementActionButton($element);
+    }
 }
