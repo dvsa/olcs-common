580f1059098037a86feb0167cac1ecd51f6ab541
Alex P <alex.peshkov@valtech.co.uk>
2017-03-20 12:50:02 +0000

    OLCS-16016 Unable to disqualify a person

---
+++ Common/src/Common/Service/Table/Formatter/DisqualifyUrl.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Common\Service\Table\Formatter;
+
+/**
+ * Disqualify URL formatter
+ *
+ * @author Alex Peshkov <alex.peshkov@valtech.co.uk>
+ */
+class DisqualifyUrl implements FormatterInterface
+{
+    /**
+     * Format a disqualify URL
+     *
+     * @param array                               $row            row
+     * @param array                               $column         column
+     * @param \Zend\ServiceManager\ServiceManager $serviceLocator service locator
+     *
+     * @return string
+     */
+    public static function format($row, $column = [], $serviceLocator = null)
+    {
+        $router           = $serviceLocator->get('router');
+        $request          = $serviceLocator->get('request');
+        $urlHelper        = $serviceLocator->get('Helper\Url');
+        $routeMatch       = $router->match($request);
+        $matchedRouteName = $routeMatch->getMatchedRouteName();
+        $query            = $request->getQuery()->toArray();
+        $params           = $routeMatch->getParams();
+
+        $url = '';
+        switch ($matchedRouteName) {
+            case 'lva-variation/people':
+                $url = $urlHelper->fromRoute(
+                    'disqualify-person',
+                    [
+                        'action'       => 'index',
+                        'variation'    => $params['variation'],
+                        'person'       => $row['id'],
+                    ],
+                    ['query' => $query],
+                    true
+                );
+                break;
+            case 'lva-licence/people':
+                $url = $urlHelper->fromRoute(
+                    'disqualify-person',
+                    [
+                        'action'       => 'index',
+                        'licence'      => $params['licence'],
+                        'person'       => $row['id'],
+                    ],
+                    ['query' => $query],
+                    true
+                );
+                break;
+            case 'lva-application/people':
+                $url = $urlHelper->fromRoute(
+                    'disqualify-person',
+                    [
+                        'action'       => 'index',
+                        'application'  => $params['application'],
+                        'person'       => $row['id'],
+                    ],
+                    ['query' => $query],
+                    true
+                );
+                break;
+            default:
+                break;
+        }
+        return sprintf(
+            '<a href="%s" class="js-modal-ajax">%s</a>',
+            $url,
+            $row['disqualificationStatus']
+        );
+    }
+}
---
+++ test/Common/src/Common/Service/Table/Formatter/DisqualifyUrlTest.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace CommonTest\Service\Table\Formatter;
+
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Mockery as m;
+use Common\Service\Table\Formatter\DisqualifyUrl;
+use CommonTest\Bootstrap;
+
+/**
+ * Disqualify Url formatter test
+ *
+ * @author Alex Peshkov <alex.peshkov@valtech.co.uk>
+ */
+class DisqualifyUrlTest extends MockeryTestCase
+{
+    protected $sm;
+
+    protected $mockRouteMatch;
+
+    protected $mockUrlHelper;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->sm = Bootstrap::getServiceManager();
+
+        $this->mockRouteMatch = m::mock(\Zend\Mvc\Router\RouteMatch::class);
+        $this->mockUrlHelper = m::mock();
+        $mockRequest = m::mock(\Zend\Stdlib\RequestInterface::class)
+            ->shouldReceive('getQuery')
+            ->andReturn(
+                m::mock()
+                ->shouldReceive('toArray')
+                ->once()
+                ->andReturn(['foo' => 'bar'])
+                ->getMock()
+            )
+            ->once()
+            ->getMock();
+
+        $mockRouter = m::mock()
+            ->shouldReceive('match')
+            ->with($mockRequest)
+            ->andReturn($this->mockRouteMatch)
+            ->getMock();
+
+        $this->sm->setService('router', $mockRouter);
+        $this->sm->setService('request', $mockRequest);
+        $this->sm->setService('Helper\Url', $this->mockUrlHelper);
+    }
+
+    /**
+     * @dataProvider provider
+     */
+    public function testFormat($data, $routeMatch, $expectedRoute, $expectedRouteParams, $params, $expectedLink)
+    {
+        $this->mockRouteMatch
+            ->shouldReceive('getMatchedRouteName')
+            ->andReturn($routeMatch)
+            ->once()
+            ->shouldReceive('getParams')
+            ->andReturn($params)
+            ->once();
+
+        if ($expectedRoute !== null) {
+            $this->mockUrlHelper
+                ->shouldReceive('fromRoute')
+                ->with($expectedRoute, $expectedRouteParams, ['query' => ['foo' => 'bar']], true)
+                ->andReturn('the_url');
+        }
+
+        $this->assertEquals($expectedLink, DisqualifyUrl::format($data, [], $this->sm));
+    }
+
+    public function provider()
+    {
+        return [
+            'licence' => [
+                [
+                    'id' => '99',
+                    'disqualificationStatus' => 'foo',
+                ],
+                'lva-licence/people',
+                'disqualify-person',
+                ['action' => 'index', 'person' => '99', 'licence' => 1],
+                ['licence' => 1],
+                '<a href="the_url" class="js-modal-ajax">foo</a>',
+            ],
+            'application' => [
+                [
+                    'id' => '99',
+                    'disqualificationStatus' => 'foo',
+                ],
+                'lva-application/people',
+                'disqualify-person',
+                ['action' => 'index', 'person' => '99', 'application' => 2],
+                ['application' => 2],
+                '<a href="the_url" class="js-modal-ajax">foo</a>',
+            ],
+            'variation' => [
+                [
+                    'id' => '99',
+                    'disqualificationStatus' => 'foo',
+                ],
+                'lva-variation/people',
+                'disqualify-person',
+                ['action' => 'index', 'person' => '99', 'variation' => 3],
+                ['variation' => 3],
+                '<a href="the_url" class="js-modal-ajax">foo</a>',
+            ],
+            'unknown' => [
+                [
+                    'id' => '99',
+                    'disqualificationStatus' => 'foo',
+                ],
+                'bar',
+                null,
+                null,
+                null,
+                '<a href="" class="js-modal-ajax">foo</a>',
+            ],
+        ];
+    }
+}
