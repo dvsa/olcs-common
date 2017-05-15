7e2095a82d29b063a920239ceb89d747d9b2659b
Dmitrij Golubev <dgolubev@DGolubev-pc.local>
2017-05-15 15:21:00 +0100

    OLCS-16580: get hasInforceLicences, ExcepionalOrgs, IsSole from organisation istead of api response; unit tests; annotations;

--- test/Common/src/Common/Controller/Lva/Adapters/AbstractPeopleAdapterTest.php
+++ test/Common/src/Common/Controller/Lva/Adapters/AbstractPeopleAdapterTest.php
@@ -2,6 +2,8 @@
 
 namespace CommonTest\Controller\Lva\Adapters;
 
+use Common\Controller\Lva\AbstractController;
+use Common\RefData;
 use Mockery as m;
 use Mockery\Adapter\Phpunit\MockeryTestCase;
 use Zend\Form\Form;
@@ -9,20 +11,122 @@ use Common\Service\Table\TableBuilder;
 use Common\Controller\Lva\Adapters\AbstractPeopleAdapter;
 
 /**
- * Abstract People Adapter Test
- *
  * @author Alex Peshkov <alex.peshkov@valtech.co.uk>
+ * @covers \Common\Controller\Lva\Adapters\AbstractPeopleAdapter
  */
 class AbstractPeopleAdapterTest extends MockeryTestCase
 {
+    const ID = 9001;
+
+    /** @var  m\MockInterface | AbstractPeopleAdapter */
+    private $sut;
+
+    public function setUp()
+    {
+        $this->sut = m::mock(AbstractPeopleAdapter::class)
+            ->makePartial()
+            ->shouldAllowMockingProtectedMethods();
+    }
+
+    public function testLoadPeopleDataLic()
+    {
+        $this->sut->shouldReceive('loadPeopleDataForLicence')->with(self::ID);
+
+        static::assertTrue($this->sut->loadPeopleData(AbstractController::LVA_LIC, self::ID));
+    }
+
+    public function testLoadPeopleDataOth()
+    {
+        $this->sut->shouldReceive('loadPeopleDataForApplication')->with(self::ID);
+
+        static::assertTrue($this->sut->loadPeopleData(AbstractController::LVA_VAR, self::ID));
+        static::assertTrue($this->sut->loadPeopleData(AbstractController::LVA_APP, self::ID));
+    }
+
+    /** @dataProvider dpTestHasInforceLicences */
+    public function testHasInforceLicences($data, $expect)
+    {
+        $this->sut->shouldReceive('getOrganisation')->once()->andReturn($data);
+
+        static::assertEquals($expect, $this->sut->hasInforceLicences());
+    }
+
+    public function dpTestHasInforceLicences()
+    {
+        return[
+            [
+                'data'=> [
+                    'hasInforceLicences' => false,
+                ],
+                'expect'=> false,
+            ],
+            [
+                'data'=> [
+                    'hasInforceLicences' => true,
+                ],
+                'expect'=> true,
+            ],
+            [
+                'data'=> [],
+                'expect'=> false,
+            ],
+        ];
+    }
+
+    /** @dataProvider dpTestIsExceptionalOrganisation */
+    public function testIsExceptionalOrganisation($type, $expect)
+    {
+        $this->sut->shouldReceive('getOrganisationType')->once()->andReturn($type);
+
+        static::assertEquals($expect, $this->sut->isExceptionalOrganisation());
+    }
+
+    public function dpTestIsExceptionalOrganisation()
+    {
+        return[
+            [
+                'type'=> RefData::ORG_TYPE_REGISTERED_COMPANY,
+                'expect'=> false,
+            ],
+            [
+                'type'=> RefData::ORG_TYPE_PARTNERSHIP,
+                'expect'=> true,
+            ],
+            [
+                'type'=> RefData::ORG_TYPE_SOLE_TRADER,
+                'expect'=> true,
+            ],
+        ];
+    }
+
     /**
-     * @dataProvider dataProvider
+     * @dataProvider dpTestIsSoleTrader
+     */
+    public function testIsSoleTrader($type, $expect)
+    {
+        $this->sut->shouldReceive('getOrganisationType')->once()->andReturn($type);
+
+        static::assertEquals($expect, $this->sut->isSoleTrader());
+    }
+
+    public function dpTestIsSoleTrader()
+    {
+        return[
+            [
+                'type'=> RefData::ORG_TYPE_REGISTERED_COMPANY,
+                'expect'=> false,
+            ],
+            [
+                'type'=> RefData::ORG_TYPE_SOLE_TRADER,
+                'expect'=> true,
+            ],
+        ];
+    }
+    /**
+     * @dataProvider dpTestAlterFormForOrganisation
      */
     public function testAlterFormForOrganisation($type, $expected)
     {
-        $sut = m::mock(AbstractPeopleAdapter::class)
-            ->makePartial();
-
         $mockTable = m::mock(TableBuilder::class)
             ->shouldReceive('getAction')
             ->with('add')
@@ -36,15 +140,15 @@ class AbstractPeopleAdapterTest extends MockeryTestCase
             ->once()
             ->getMock();
 
-        $sut->shouldReceive('getOrganisationType')
+        $this->sut->shouldReceive('getOrganisationType')
             ->andReturn($type)
             ->twice()
             ->getMock();
 
-        $sut->alterFormForOrganisation(m::mock(Form::class), $mockTable);
+        $this->sut->alterFormForOrganisation(m::mock(Form::class), $mockTable);
     }
 
-    public function dataProvider()
+    public function dpTestAlterFormForOrganisation()
     {
         return [
             'ltd' => [
