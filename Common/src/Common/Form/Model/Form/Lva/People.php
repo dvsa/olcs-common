6a8235b59f982ce6c35494654466d2b897383219
Alex P <alex.peshkov@valtech.co.uk>
2017-06-15 17:53:57 +0100

    OLCS-14375 some code added back

--- Common/src/Common/Form/Model/Form/Lva/People.php
+++ Common/src/Common/Form/Model/Form/Lva/People.php
@@ -5,6 +5,7 @@ namespace Common\Form\Model\Form\Lva;
 use Zend\Form\Annotation as Form;
 
 /**
+ * @Form\Options({"prefer_form_input_filter":true})
  * @Form\Name("lva-people")
  * @Form\Attributes({"method":"post"})
  * @Form\Type("Common\Form\Form")
