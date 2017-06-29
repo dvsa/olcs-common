6b9994bc143dd77d417712259aac05809ff164a8
lindsayi <ian.lindsay@valtech.co.uk>
2017-06-29 16:25:11 +0100

    olcs-16871 added new translation key for removing the final tm from a licence (welsh still needs to be translated)

--- Common/config/language/en_GB.php
+++ Common/config/language/en_GB.php
@@ -900,6 +900,7 @@ return [
     'definition' => 'Definition',
     'delete' => 'Delete',
     'delete.confirmation.text' => 'This action is permanent and cannot be undone.',
+    'delete.final-tm.confirmation.text' => 'You are removing your last Transport Manager. If you haven\'t yet made an application to appoint a replacement, you must contact us on {{OPERATOR_LICENSING_PHONE}} or at <a href="mailto:{{OPERATOR_LICENSING_EMAIL}}">{{OPERATOR_LICENSING_EMAIL}}</a>',
     'deleting.all.vehicles.message' => 'If you are removing all the vehicles from your licence you must be aware that standard licence holders must be able to prove they have a formal arrangement for access at all times to at least one vehicle registered or in circulation.',
     'delta_action_links.remove' => 'Remove',
     'delta_action_links.restore' => 'Restore',
--- Common/config/language/replacements.php
+++ Common/config/language/replacements.php
@@ -21,4 +21,6 @@ return [
     '{{EBSR_SUPPORT_EMAIL}}' => 'ebsr@vosa.gov.uk',
     '{{EBSR_PAPER_FORMS_LINK}}' => 'https://www.gov.uk/government/collections/local-bus-service-registration-forms',
     '{{EBSR_TRANSXCHANGE_LINK}}' => 'https://www.gov.uk/government/publications/transxchange-downloads-and-schema',
+    '{{OPERATOR_LICENSING_EMAIL}}' => 'operatorlicensing@otc.gsi.gov.uk',
+    '{{OPERATOR_LICENSING_PHONE}}' => '0300 123 9000',
 ];
