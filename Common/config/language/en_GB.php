05855e2ccb1d09dc9b713ae379cb2d6be1bba08e
Alex Peshkov <alex.peshkov@valtech.co.uk>
2017-08-03 15:57:55 +0100

    OLCS-16976 translation fixed

--- Common/config/language/en_GB.php
+++ Common/config/language/en_GB.php
@@ -3235,7 +3235,7 @@ return [
     'continuation.declaration.print.licence_number' => 'Licence number',
     'continuation.declaration.print.licence_holder' => 'Licence holder',
     'continuation.declaration.print.return_to' => 'Return to',
-    'continuation.declaration.print.gv_title' => 'Continue an existing goods Vehicle Operator\'s Licence',
+    'continuation.declaration.print.gv_title' => 'Continue an existing Goods Vehicle Operator\'s Licence',
     'continuation.declaration.print.psv_title' => 'Continue an existing Public Service Vehicle Operator\'s Licence',
     'continuation.declaration.print.psv_sr_title' => 'Continue an existing Special Restricted Public Service Vehicle Operator\'s Licence',
     'continuation.declaration.print.full_name_in_block-capitals' => 'Full name in block capitals',
