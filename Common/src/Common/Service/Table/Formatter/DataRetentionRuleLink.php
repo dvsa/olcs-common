ec03ff41f9f1b995fce2b8bf01cfd50eace32a33
Gaurav Malhotra <gaurav.malhotra@valtech.co.uk>
2017-08-15 11:23:38 +0100

    OLCS-17658 table formatter for displaying data retention link on data retention rules table

---
+++ Common/src/Common/Service/Table/Formatter/DataRetentionRuleLink.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Common\Service\Table\Formatter;
+
+use Zend\ServiceManager\ServiceManager;
+
+/**
+ * Data Retention Rule link formatter
+ */
+class DataRetentionRuleLink implements FormatterInterface
+{
+    /**
+     * Format
+     *
+     * @param array          $data   Data of current row
+     * @param array          $column Column
+     * @param ServiceManager $sm     Service manager
+     *
+     * @return string
+     */
+    public static function format($data, $column = array(), $sm = null)
+    {
+        $urlHelper = $sm->get('Helper\Url');
+        $url = $urlHelper->fromRoute(
+            'admin-dashboard/admin-data-retention/records',
+            ['dataRetentionRuleId' => $data['id']]
+        );
+
+        return '<a href="' . $url . '" target="_self">' . $data['description'] . '</a>';
+    }
+}
