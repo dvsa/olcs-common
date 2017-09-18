23c7dd9f8899ee4ee551f41cd58633aa0bfe6d25
jamie crisp <james.crisp@valtech.co.uk>
2017-09-18 14:40:23 +0100

    OLCS-17930: changed replace button on licence disks.

--- Common/src/Common/Service/Table/Type/ActionLinks.php
+++ Common/src/Common/Service/Table/Type/ActionLinks.php
@@ -111,7 +111,7 @@ class ActionLinks extends Selector
         if ($this->isLinkVisible($data, $column, 'Replace', false)) {
             $inputName = sprintf($this->getInputName($column, 'replaceInputName'), $data['id']);
             $content .= sprintf(
-                ' <input type="submit" class="right-aligned trigger-modal" name="%s" value="' . $replace . '">',
+                ' <input type="submit" class="action--secondary right-aligned trigger-modal" name="%s" value="' . $replace . '">',
                 $inputName
             );
         }
