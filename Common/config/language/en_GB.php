6e2ef747b946f0de079521ae250e0d22c0b010b5
jamie crisp <james.crisp@valtech.co.uk>
2017-09-18 13:54:05 +0100

    OLCS-17744: added class for operating centre text

--- Common/config/language/en_GB.php
+++ Common/config/language/en_GB.php
@@ -3274,6 +3274,6 @@ return [
     'continuations.step.header' => 'Step %s of %s',
     'lva-operating-centre-radio-hint' => 'Uploading your financial information as soon as possible will help us process your application quickly.',
     'lva-operating-centre-radio-label' => '<p class="legend">How would you like to send your advert?</p>',
-    'lva-operating-centre-newspaper-advert' => '<p>Newspaper advert</p>',
+    'lva-operating-centre-newspaper-advert' => '<p class="label">Newspaper advert</p>',
     // Potentially unused translations
 ];
