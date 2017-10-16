4ba8c983386067d19c9271484bfb7dcde60bb088
Igor <igorstimoli@gmail.com>
2017-10-16 14:44:15 +0100

    OLCS-18106: add DataRetentionRuleAdminLink formatter

---
+++ Common/src/Common/Service/Table/Formatter/DataRetentionRuleAdminLink.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Common\Service\Table\Formatter;
+
+use Zend\ServiceManager\ServiceManager;
+
+/**
+ * Data Retention Rule admin link formatter
+ */
+class DataRetentionRuleAdminLink implements FormatterInterface
+{
+    /**
+     * Format
+     *
+     * @param array          $data   Data of current row
+     * @param array          $column Column
+     * @param ServiceManager $sm     Service manager
+     *
+     * @return string
+     */
+    public static function format($data, $column = array(), $sm = null)
+    {
+        $urlHelper = $sm->get('Helper\Url');
+        $url = $urlHelper->fromRoute(
+            'admin-dashboard/admin-data-retention/rule-admin-form',
+            ['dataRetentionRuleId' => $data['id']]
+        );
+
+        return '<a href="' . $url . '" target="_self">' .
+            ucwords($data['description']) .
+            '</a>';
+    }
+}
