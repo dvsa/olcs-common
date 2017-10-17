2d4247fe4eed787088cfdd9c514ae8b6b8e35c7e
Mat Evans <mat.evans@valtech.co.uk>
2017-10-17 12:22:41 +0100

    OLCS-18084 Fix issue with insolvency checkbox not being populated

--- Common/src/Common/Data/Mapper/Lva/FinancialHistory.php
+++ Common/src/Common/Data/Mapper/Lva/FinancialHistory.php
@@ -14,8 +14,16 @@ namespace Common\Data\Mapper\Lva;
  */
 class FinancialHistory
 {
+    /**
+     * Map from API data to form data
+     *
+     * @param array $data API data
+     *
+     * @return array
+     */
     public static function mapFromResult(array $data)
     {
+        $data['financialHistoryConfirmation']['insolvencyConfirmation'] = $data['insolvencyConfirmation'];
         return [
             'data' => $data
         ];
