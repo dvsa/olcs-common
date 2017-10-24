b7624e117b5d69fb9d09a17a3d0e4988f481b628
Shaun Hare <shaun.hare@dvsa.gov.uk>
2017-10-24 17:09:22 +0100

    translations for section heading and subtitle

--- Common/config/language/en_GB.php
+++ Common/config/language/en_GB.php
@@ -1624,8 +1624,18 @@ return [
 
     'lva.section.title.add_director' => 'Add director',
     'lva.section.title.add_partner' => 'Add partner',
-    'lva.section.title.add_person' => 'Add person', 
-    
+    'lva.section.title.add_person' => 'Add person',
+
+    'lva.section.title.add_person_org_t_rc' => 'Add a Director',
+    'lva.section.title.add_person_org_t_p' => 'Add a Partner',
+    'lva.section.title.add_person_org_t_pa' => 'Add a Responsible person',
+    'lva.section.title.add_person_org_t_st' => 'Add a Owner',
+
+    'licence_add-Person-PersonType-org_t_rc'=>'Director',
+    'licence_add-Person-PersonType-org_t_p'=>'Partner',
+    'licence_add-Person-PersonType-org_t_pa'=>'Responsible person',
+    'licence_add-Person-PersonType-org_t_st'=>'Owner',
+
     'continue.finance.history.button' => 'Continue to financial history',
 
     'list-of-convictions' => 'List of convictions',
