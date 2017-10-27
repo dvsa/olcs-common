b6afe9e093184516e9f2039eded069b5f6d08cdb
Richard Ward <richard.ward@bjss.com>
2017-10-27 15:53:38 +0100

    Financial History form modifications for director-chahnge journey
    Hide hint field as it doesn't apply
    Wording change for intro based on organisation type
    
    OLCS-17955

--- test/Common/src/Common/FormService/Form/Lva/FinancialHistoryTest.php
+++ test/Common/src/Common/FormService/Form/Lva/FinancialHistoryTest.php
@@ -5,16 +5,21 @@
  *
  * @author Dan Eggleston <dan@stolenegg.com>
  */
+
 namespace CommonTest\FormService\Form\Lva;
 
 use Common\Form\Elements\InputFilters\SingleCheckbox;
+use Common\Form\Elements\Types\HtmlTranslated;
 use Common\Form\Form;
 use Common\FormService\Form\Lva\FinancialHistory;
+use Common\FormService\FormServiceManager;
+use Common\RefData;
+use Common\Service\Helper\FormHelperService;
 use Mockery as m;
 use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Zend\Form\ElementInterface;
 use Zend\Form\Fieldset;
 use Zend\Http\Request;
-use Zend\InputFilter\Input;
 
 /**
  * Financial History Form Service Test
@@ -23,16 +28,19 @@ use Zend\InputFilter\Input;
  */
 class FinancialHistoryTest extends MockeryTestCase
 {
+    /** @var  FinancialHistory */
     protected $sut;
 
+    /** @var FormHelperService|m\Mock */
     protected $formHelper;
 
+    /** @var FormServiceManager|m\Mock */
     protected $fsm;
 
     public function setUp()
     {
-        $this->formHelper = m::mock('\Common\Service\Helper\FormHelperService');
-        $this->fsm = m::mock('\Common\FormService\FormServiceManager')->makePartial();
+        $this->formHelper = m::mock(FormHelperService::class);
+        $this->fsm = m::mock(FormServiceManager::class)->makePartial();
 
         $this->sut = new FinancialHistory();
         $this->sut->setFormHelper($this->formHelper);
@@ -41,6 +49,7 @@ class FinancialHistoryTest extends MockeryTestCase
 
     public function testGetForm()
     {
+        /** @var Request|m\Mock $request */
         $request = m::mock(Request::class);
 
         // Mocks
@@ -57,9 +66,12 @@ class FinancialHistoryTest extends MockeryTestCase
 
     /**
      * @dataProvider lvaDataProvider
+     *
+     * @param $lva
      */
     public function testGetFormWithNiFlagSetToY($lva)
     {
+        /** @var Request|m\Mock $request */
         $request = m::mock(Request::class);
 
         // Mocks
@@ -102,6 +114,61 @@ class FinancialHistoryTest extends MockeryTestCase
         return [
             ['variation'],
             ['application'],
+        ];
+    }
+
+    /**
+     * @dataProvider provideDirectorChangeWordingVariations
+     *
+     * @param string $organisationType one of RefData::ORG_TYPE_*
+     * @param string $personDescription the type of person ("Person", "Director", "Partner")
+     */
+    public function testGetFormForDirectorChange($organisationType, $personDescription)
+    {
+        /** @var Request|m\Mock $request */
+        $request = m::mock(Request::class);
+
+        /** @var Form|m\Mock $mockForm */
+        $mockForm = m::mock(Form::class);
+
+        /** @var Fieldset|m\Mock $mockDataFieldset */
+        $mockDataFieldset = m::mock(Fieldset::class);
+
+        /** @var HtmlTranslated|m\Mock $mockHasAnyPersonElement */
+        $mockHasAnyPersonElement = m::mock(ElementInterface::class);
+
+        $this->formHelper->shouldReceive('createFormWithRequest')
+            ->with('Lva\FinancialHistory', $request)
+            ->andReturn($mockForm);
+
+        $this->formHelper->shouldReceive('remove')->once()->with($mockForm, 'data->financeHint');
+
+        $mockForm->shouldReceive('get')->with('data')->andReturn($mockDataFieldset);
+        $mockDataFieldset->shouldReceive('get')->with('hasAnyPerson')->andReturn($mockHasAnyPersonElement);
+
+        $mockHasAnyPersonElement
+            ->shouldReceive('setTokens')
+            ->with([sprintf('Have any of the new %s been:', $personDescription)])
+            ->once();
+
+        $form = $this->sut->getForm(
+            $request,
+            ['variationType' => RefData::VARIATION_TYPE_DIRECTOR_CHANGE, 'organisationType' => $organisationType]
+        );
+
+        $this->assertSame($mockForm, $form);
+    }
+
+    public function provideDirectorChangeWordingVariations()
+    {
+        return [
+            [RefData::ORG_TYPE_REGISTERED_COMPANY, 'directors'],
+            [RefData::ORG_TYPE_SOLE_TRADER, 'people'],
+            [RefData::ORG_TYPE_LLP, 'partners'],
+            [RefData::ORG_TYPE_PARTNERSHIP, 'partners'],
+            [RefData::ORG_TYPE_OTHER, 'people'],
+            [RefData::ORG_TYPE_IRFO, 'people'],
+            ['anything-else', 'people'],
         ];
     }
 }
