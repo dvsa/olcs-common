5406575881ce2df814a3ed3526e7ecc037245228
Igor <igorstimoli@gmail.com>
2017-11-21 17:08:23 +0000

    OLCS-14221: UserTest

---
+++ test/Common/src/Common/Rbac/UserTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace CommonTest\Rbac;
+
+use Common\Rbac\User;
+use Mockery\Adapter\Phpunit\MockeryTestCase as TestCase;
+
+/**
+ * Class User
+ * @package CommonTest\Rbac
+ */
+class UserTest extends TestCase
+{
+    private $sut;
+
+
+    public function setUp()
+    {
+        $this->sut = new User();
+    }
+
+    public function testIsNotIdentified()
+    {
+
+        $this->sut->setUserType(User::USER_TYPE_NOT_IDENTIFIED);
+
+        $this->assertTrue($this->sut->isNotIdentified());
+
+    }
+
+    public function testIsNotIdentifiedFalse()
+    {
+
+        $this->sut->setUserType(User::USER_TYPE_ANON);
+
+        $this->assertFalse($this->sut->isNotIdentified());
+
+    }
+
+}
