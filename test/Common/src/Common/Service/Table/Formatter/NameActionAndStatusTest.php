e226958551081d9ee7c2ee520f839111fd645503
Chris Emerson <Chris.Emerson@bjss.com>
2017-11-21 15:45:07 +0000

    Show 'new' label against people who are just added using the Flash Messenger

---
+++ test/Common/src/Common/Service/Table/Formatter/NameActionAndStatusTest.php
@@ -0,0 +1,69 @@
+<?php
+namespace CommonTest\Service\Table\Formatter;
+
+use Common\Service\Table\Formatter\NameActionAndStatus;
+use PHPUnit_Framework_TestCase;
+
+class NameActionAndStatusTest extends PHPUnit_Framework_TestCase
+{
+    const TEST_ID = 12345;
+    const TEST_TITLE = 'TEST_TITLE';
+    const TEST_FORENAME = '';
+    const TEST_FAMILY_NAME = '';
+
+    /**
+     * @dataProvider provider
+     */
+    public function testFormat($data, $expected)
+    {
+        $this->assertEquals($expected, NameActionAndStatus::format($data));
+    }
+
+    public function provider()
+    {
+        return [
+            [
+                [
+                    'id' => self::TEST_ID,
+                    'forename' => self::TEST_FORENAME,
+                    'familyName' => self::TEST_FAMILY_NAME,
+                    'title' => [
+                        'description' => ''
+                    ],
+                    'status' => null
+                ],
+                '<input class="" name="table[action][edit][' . self::TEST_ID . ']" value="'
+                . self::TEST_FORENAME . ' ' . self::TEST_FAMILY_NAME
+                . '" type="submit">'
+            ],
+            [
+                [
+                    'id' => self::TEST_ID,
+                    'forename' => self::TEST_FORENAME,
+                    'familyName' => self::TEST_FAMILY_NAME,
+                    'title' => [
+                        'description' => self::TEST_TITLE
+                    ],
+                    'status' => null
+                ],
+                '<input class="" name="table[action][edit][' . self::TEST_ID . ']" value="'
+                . self::TEST_TITLE . ' ' . self::TEST_FORENAME . ' ' . self::TEST_FAMILY_NAME
+                . '" type="submit">'
+            ],
+            [
+                [
+                    'id' => self::TEST_ID,
+                    'forename' => self::TEST_FORENAME,
+                    'familyName' => self::TEST_FAMILY_NAME,
+                    'title' => [
+                        'description' => self::TEST_TITLE
+                    ],
+                    'status' => 'new'
+                ],
+                '<input class="" name="table[action][edit][' . self::TEST_ID . ']" value="'
+                . self::TEST_TITLE . ' ' . self::TEST_FORENAME . ' ' . self::TEST_FAMILY_NAME
+                . '" type="submit"> <span class="overview__status green">New</span>'
+            ]
+        ];
+    }
+}
