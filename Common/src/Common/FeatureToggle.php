827fcf6943bcfa0623a89b2e832f1f587f2a3932
Ian Lindsay <ian@hemera-business-services.co.uk>
2018-07-05 10:37:27 +0100

    olcs-20415 add test for features enabled plugin, add new ref data for backend feature toggle

--- Common/src/Common/FeatureToggle.php
+++ Common/src/Common/FeatureToggle.php
@@ -12,4 +12,6 @@ class FeatureToggle
     const INTERNAL_PERMITS = 'internal_permits';
     const SELFSERVE_ECMT = 'ss_ecmt';
     const SELFSERVE_PERMITS = 'ss_permits';
+    const BACKEND_ECMT = 'back_ecmt';
+    const BACKEND_PERMITS = 'back_permits';
 }
---
+++ test/Common/src/Common/Controller/Plugin/FeaturesEnabledTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace CommonTest\Controller\Plugin;
+
+use Common\Controller\Plugin\FeaturesEnabled;
+use Common\Service\Cqrs\Query\QuerySender;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Mockery as m;
+use Zend\Mvc\MvcEvent;
+
+/**
+ * FeaturesEnabled Test
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+class FeaturesEnabledTest extends MockeryTestCase
+{
+    protected $action = 'action';
+    protected $mvcEvent;
+    protected $querySender;
+
+    protected function setUp()
+    {
+        $this->mvcEvent = m::mock(MvcEvent::class);
+        $this->mvcEvent->shouldReceive('getRouteMatch->getParam')->with('action')->andReturn($this->action);
+        $this->querySender = m::mock(QuerySender::class);
+    }
+
+    public function testInvokeWithEmptyConfig()
+    {
+        $this->querySender->shouldNotReceive('featuresEnabled');
+        $sut = new FeaturesEnabled($this->querySender);
+        $this->assertEquals(false, $sut->__invoke([], $this->mvcEvent));
+    }
+
+    /**
+     * @dataProvider dpTestInvoke
+     */
+    public function testInvoke($config, $checkedToggles, $expectedResult)
+    {
+        $this->querySender->shouldReceive('featuresEnabled')->once()->with($checkedToggles)->andReturn($expectedResult);
+        $sut = new FeaturesEnabled($this->querySender);
+        $this->assertEquals($expectedResult, $sut->__invoke($config, $this->mvcEvent));
+    }
+
+    public function dpTestInvoke()
+    {
+        $defaultConfig = ['default toggle 1', 'default toggle 2'];
+        $actionConfig = ['action toggle 1', 'action toggle 2'];
+
+        $bothConfigs = [
+            'default' => $defaultConfig,
+            $this->action => $actionConfig
+        ];
+
+        $defaultConfigOnly = [
+            'default' => $defaultConfig
+        ];
+
+        //action config only
+        $actionConfigOnly = [
+            $this->action => $actionConfig
+        ];
+
+        return [
+            [$bothConfigs, $actionConfig, true],
+            [$defaultConfigOnly, $defaultConfig, false],
+            [$actionConfigOnly, $actionConfig, true],
+        ];
+    }
+}
