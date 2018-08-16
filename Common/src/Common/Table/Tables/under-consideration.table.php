b9332fe2aa0577510c255d814b06e5380f569eb4
Jason de Jonge <dejongej@ip-192-168-101-147.eu-west-1.compute.internal>
2018-08-16 20:51:14 +0100

    [OLCS-21155] Initial draft of table config for Under Consideration page

---
+++ Common/src/Common/Table/Tables/under-consideration.table.php
@@ -0,0 +1,19 @@
+<?php
+//This table is unusual because the headers are down the side of the table instead of across the top
+return array(
+    'variables' => array(
+        'title' => 'Application Details',
+        'empty_message' => 'empty message',
+        'required_label' => '',
+        'within_form' => true,
+    ),
+    'settings' => array(),
+    'columns' => array(
+        array(
+            'name' => 'applicationDetailsTitle',
+        ),
+        array(
+            'name' => 'applicationDetailsAnswer',
+        ),
+    )
+);
