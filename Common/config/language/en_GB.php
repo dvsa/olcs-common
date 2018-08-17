ac41f56c46cdc630ed9cf39023439959b567e609
Jason de Jonge <dejongej@ip-192-168-1-4.eu-west-1.compute.internal>
2018-08-17 10:43:09 +0100

    transaltion for new licence question

--- Common/config/language/en_GB.php
+++ Common/config/language/en_GB.php
@@ -3383,6 +3383,7 @@ return [
     'permits.page.cabotage.question' => 'ECMT permits do not allow you to carry out cabotage',
     'permits.page.ecmt.licence.title' => 'Permits Home - ',
     'permits.page.ecmt.licence.question' => 'Select a licence you would like your ECMT permit to be applied to',
+    'permits.page.ecmt.licence.question.one.licence' => 'Permit application for licence %s',
     'permits.page.ecmt.licence.info' => 'ECMT permits will be valid for use from 1 January 2019 to 31 December 2019.',
     'permits.page.international.journey.title' => 'Percentage of International Journey - ',
     'permits.page.international.journey.question' => 'In the last 12 months what portion of your journeys were international for this licence?',
