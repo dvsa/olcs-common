568a8e99e6104b521513332f563b7ceebc410fe7
Parth <parth.vyas@dvsa.gov.uk>
2018-08-28 10:50:58 +0100

    Create markup for confirmation pages. OLCS-19792

--- Common/src/Common/Service/Data/SectionConfig.php
+++ Common/src/Common/Service/Data/SectionConfig.php
@@ -381,7 +381,7 @@ class SectionConfig implements ServiceLocatorAwareInterface
                 'identifier' => 'application'
             ),
             'transport_manager' => [
-                'identifier' =>'application'
+                'identifier' => 'application'
             ]
         );
 
