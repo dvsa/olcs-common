835557a46eae3f276f671c138a768c73f7e6a1e1
Shaun Hare <shaun.hare@dvsa.gov.uk>
2018-09-11 06:58:07 +0100

    WIP - formating data for OLCS-21373

--- Common/src/Common/Data/Mapper/Lva/TransportManagerApplication.php
+++ Common/src/Common/Data/Mapper/Lva/TransportManagerApplication.php
@@ -31,8 +31,97 @@ class TransportManagerApplication
         return $errors;
     }
 
-    public static function mapForSections(array $transportManagerApplication): array
+    public static function mapForSections(array $transportManagerApplication, array $postData): array
     {
+        $sections = [];
+        $count = 0;
+        foreach ($postData as $key => $value) {
+            if (!in_array($key, ['security', 'form-actions'])) {
+                $sectionName = $key;
+                $sections[$count]['sectionHeading'] = 'lva-checkanswers-section-' . $sectionName;
+                $sections[$count]['change'] = ['sectionName' => $sectionName, 'backText' => ''];
+                foreach ($value as $label => $answer) {
+                    if (is_array($answer)) {
+                        switch ($label) {
+                            case "hoursOfWeek":
+                                $days = json_encode([
+                                    ['label' => 'Monday', 'answer' => 'hoursMon'],
+                                    ['label' => 'Tuesday', 'answer' => 'hoursTue'],
+                                    ['label' => 'Wednesday', 'answer' => 'hoursWed'],
+                                    ['label' => 'Thursday', 'answer' => 'hoursThu'],
+                                    ['label' => 'Friday', 'answer' => 'hoursFri'],
+                                    ['label' => 'Saturday', 'answer' => 'hoursSat'],
+                                    ['label' => 'Sunday', 'answer' => 'hoursSun']
+                                ]);
+
+                                foreach ($answer as $key => $value) {
+                                    foreach ($value as $weekDay => $hrs) {
+                                        $days = str_replace($weekDay, $hrs, $days);
+                                    }
+                                }
+                                $sections[$count + 10]['sectionHeading'] = 'Hours of the Week';
+                                $sections[$count + 10]['change'] = ['sectionName' => $sectionName, 'backText' => ''];
+                                $sections[$count + 10]['questions'] = json_decode($days, JSON_OBJECT_AS_ARRAY);
+
+                                break;
+
+                            case "birthDate":
+                                $answer = (!empty($answer['year']) && !empty($answer['month']) && !empty($answer['day']))
+                                    ? sprintf('%04d-%02d-%02d', $answer['year'], $answer['month'], $answer['day'])
+                                    : null;
+                                $sections[0]['questions'][] = ["label" => "birthDate", "answer" => $answer];
+                                break;
+
+                            case "otherLicences":
+                                if (empty($sections[$count]['convictions']['rows'])) {
+                                    $sections[$count]['questions'][] = ["label"=>"tma-checkanswers-label-".$label, "answer"=>self::getDefaultText()];
+
+                                }
+                                break;
+                            case "file":
+                            case "certificate":
+                                if (empty($sections[$sectionName]['fileCount'])) {
+                                    $sections[$count]['questions'][] = ["label"=>"tma-checkanswers-label-".$label, "answer"=>self::getDefaultText()];
+                                }
+                                break;
+                            case "convictions":
+                                if (empty($sections[$count]['convictions']['rows'])) {
+                                    $sections[$count]['questions'][] = ["label"=>"tma-checkanswers-label-".$label, "answer"=>self::getDefaultText()];
+
+                                }
+
+                                break;
+                            case "previousLicences":
+                                if (empty($sections[$count]['previousLicences']['rows'])) {
+                                    $sections[$count]['questions'][] = ["label"=>"tma-checkanswers-label-".$label, "answer"=>self::getDefaultText()];
+
+                                }
+                                break;
+                            default:
+                                $sections[$count]['questions'] = [];
+                                if ($sectionName === 'homeAddress' || $sectionName === 'workAddress') {
+                                    $label = 'tm-checkanswers-label-' . $sectionName;
+                                    $answers = implode('<br />', $answer);
+                                    $sections[0]['questions'][] = ["label" => $label, "answer" => $answers];
+                                }
+                        }
+                    } else {
+                        $sections[$count]['questions'][] = ['label' => $label, 'answer' => $answer];
+                    }
+                }
+            }
+
+            $count++;
+            if ($key === 'details') {
+                $label= "name";
+                $nameAnswer =['label' => $label, 'answer' => $transportManagerApplication['transportManager']['homeCd']['person']['forename']. " ".$transportManagerApplication['transportManager']['person']['familyName'] ];
+                array_unshift($sections[$count]['questions'], $nameAnswer);
+            }
+        }
+        unset($sections[1]);
+        unset($sections[2]);
+        return $sections;
+        /*
         return [
             [
                 'sectionHeading' => "Your Details",
@@ -113,5 +202,11 @@ class TransportManagerApplication
         ];
 
         //return $transportManagerApplication;
+        */
+    }
+
+    public static function getDefaultText()
+    {
+        return 'None added';
     }
 }
