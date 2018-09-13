cd307dbf274c296d71a8666cfaa930dac7c2c001
Shaun Hare <shaun.hare@dvsa.gov.uk>
2018-09-13 08:03:22 +0100

    WIP- section info otherEmployment

---
+++ Common/src/Common/Data/Mapper/Lva/TransportManager/Sections/OtherEmployment.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Common\Data\Mapper\Lva\TransportManager\Sections;
+
+
+class OtherEmployment extends AbstractSection implements TransportManagerSectionInterface
+{
+
+    use SectionSerializeTrait;
+
+    public function populate(array $transportManagerApplication)
+    {
+        // TODO: Implement populate() method.
+    }
+}
