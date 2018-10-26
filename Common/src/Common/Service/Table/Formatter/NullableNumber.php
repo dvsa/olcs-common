57d91705634bd0b11861141d39a6aa783dd32691
Jason de Jonge <dejongej@ip-192-168-179-72.eu-west-1.compute.internal>
2018-10-26 16:00:52 +0100

    added new formatter and unit test

---
+++ Common/src/Common/Service/Table/Formatter/NullableNumber.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Nullable Number formatter
+ *
+ * @author Jason de Jonge <jason.de-jonge@capgemini.co.uk>
+ */
+
+namespace Common\Service\Table\Formatter;
+
+/**
+ * Nullable Number formatter
+ *
+ * @author Jason de Jonge <jason.de-jonge@capgemini.co.uk>
+ */
+class NullableNumber implements FormatterInterface
+{
+    /**
+     * Comment value
+     *
+     * @param array $data
+     * @param array $column
+     * @param \Zend\ServiceManager\ServiceManager $sm
+     * @return int either the input number or 0 for null values
+     */
+    public static function format($data, $column = array(), $sm = null)
+    {
+        if (!is_null($data[$column['name']])) {
+            return $data[$column['name']];
+        }
+        return 0;
+    }
+}
