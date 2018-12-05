a385d2f90ff191a558ad4ac9b795d604a930a95b
Henry White <henry.white@capgemini.com>
2018-12-05 16:05:09 +0000

    OLCS-21703: Updated restricted licence text content

--- Common/config/language/cy_GB.php
+++ Common/config/language/cy_GB.php
@@ -1499,11 +1499,7 @@ return [
     'permits.form.cancel_application.label' => 'I confirm that I would like to cancel my application.',
     'permits.form.cancel_application.button' => 'Cancel application',
     'permits.form.cancel_application.error_message' => 'You must select the checkbox to continue',
-    'permits.form.ecmt-licence.restricted-licence.hint' => 'Please note, you only need an ECMT permit for a restricted licence if you intend to travel to: Austria, Belarus, Bulgaria, the Czech Republic, Estonia, Finland, France, Hungary, Italy, Lithuania, Poland, Russia, Sweden and Turkey.',
-
-    'permits.form.change_licence.label' => 'I confirm that I would like to change the licence to %s',
-    'permits.form.change_licence.button' => 'Confirm Licence Change',
-    'permits.form.change_licence.error_message' => 'You must select the checkbox to continue',
+    'permits.form.ecmt-licence.restricted-licence.hint' => 'You only need an ECMT permit for a restricted licence if you intend to travel to: Austria, Belarus, Bulgaria, the Czech Republic, Estonia, Finland, France, Hungary, Italy, Lithuania, Poland, Russia, Sweden and Turkey.',
 
     'error.messages.restricted.countries.list' => 'You must select at least one country',
     'error.messages.sector' => 'You must answer the question to continue',
