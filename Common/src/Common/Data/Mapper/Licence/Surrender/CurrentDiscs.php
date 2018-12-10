37acb840c89087d71f6d2173aa2d2dba229be461
James Hodgson <james@tdchodgy.co.uk>
2018-12-10 15:06:18 +0000

    Populate checkboxes if needed

--- Common/src/Common/Data/Mapper/Licence/Surrender/CurrentDiscs.php
+++ Common/src/Common/Data/Mapper/Licence/Surrender/CurrentDiscs.php
@@ -9,20 +9,28 @@ class CurrentDiscs implements MapperInterface
 
     public static function mapFromResult(array $data): array
     {
+
+        $inPossession = isset($data['discDestroyed']) ? 'Y' : 'N';
+        $lost = isset($data['discLost']) ? 'Y' : isset($data['discLostInfo']) ? 'Y' : 'N';
+        $stolen = isset($data['discStolen']) ? 'Y' : isset($data['discStolenInfo']) ? 'Y' : 'N';
+
         return [
             'version' => $data['version'],
             'possessionSection' => [
+                'inPossession' => $inPossession,
                 'possessionInfo' => [
                     'discDestroyed' => $data['discDestroyed'] ?? null
                 ]
             ],
             'lostSection' => [
+                'lost' => $lost,
                 'lostInfo' => [
                     'discLost' => $data['discLost'] ?? null,
                     'lostInfo' => $data['discLostInfo'] ?? null
                 ]
             ],
             'stolenSection' => [
+                'stolen' => $stolen,
                 'stolenInfo' => [
                     'discStolen' => $data['discStolen'] ?? null,
                     'stolenInfo' => $data['discStolenInfo'] ?? null
