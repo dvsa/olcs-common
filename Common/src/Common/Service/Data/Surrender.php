d3f23239ac701296be77c71f00bda1ad431e662f
Shaun Hare <shaun.hare@dvsa.gov.uk>
2019-01-25 13:17:45 +0000

    added unit tests

--- Common/src/Common/Service/Data/Surrender.php
+++ Common/src/Common/Service/Data/Surrender.php
@@ -8,6 +8,12 @@ use Dvsa\Olcs\Transfer\Query\Surrender\ByLicence;
 
 class Surrender extends AbstractDataService
 {
+    /**
+     * @param int $licenceId
+     *
+     * @return array
+     * @throws UnexpectedResponseException
+     */
     public function fetchSurrenderData(int $licenceId): array
     {
         $surrenderQuery = ByLicence::create(
