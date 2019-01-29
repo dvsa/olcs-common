9b44fe1f7601bc280f0c8f448e9a50613ff5134c
Piotr Filak <piotr.filak@capgemini.com>
2019-01-29 10:44:07 +0000

    OLCS-23283 - SS - IRHP app link on permit dashboard

--- Common/src/Common/Service/Table/Formatter/LicencePermitReference.php
+++ Common/src/Common/Service/Table/Formatter/LicencePermitReference.php
@@ -29,24 +29,31 @@ class LicencePermitReference implements FormatterInterface
      */
     public static function format($row, $column = null, $serviceLocator = null)
     {
-        $urlHelper = $serviceLocator->get('Helper\Url');
-        $route = 'application-overview';
+        if (isset($row['irhpPermitType'])) {
+            // IRHP app
+            if ($row['isNotYetSubmitted']) {
+                $route = 'application';
+            }
+        } else {
+            // ECMT app
+            $route = 'application-overview';
 
-        if ($row['isValid']) {
-            $route = 'ecmt-valid-permits';
-        } elseif ($row['isFeePaid'] || $row['isIssueInProgress']) {
-            $route = null;
-        } elseif ($row['isAwaitingFee']) {
-            $route = 'ecmt-awaiting-fee';
-        } elseif ($row['isUnderConsideration']) {
-            $route = 'ecmt-under-consideration';
+            if ($row['isValid']) {
+                $route = 'ecmt-valid-permits';
+            } elseif ($row['isFeePaid'] || $row['isIssueInProgress']) {
+                $route = null;
+            } elseif ($row['isAwaitingFee']) {
+                $route = 'ecmt-awaiting-fee';
+            } elseif ($row['isUnderConsideration']) {
+                $route = 'ecmt-under-consideration';
+            }
         }
 
         return isset($route)
             ? vsprintf(
                 '<a class="overview__link" href="%s"><span class="overview__link--underline">%s</span></a>',
                 [
-                    $urlHelper->fromRoute('permits/' . $route, ['id' => $row['id']]),
+                    $serviceLocator->get('Helper\Url')->fromRoute('permits/' . $route, ['id' => $row['id']]),
                     Escape::html($row['applicationRef'])
                 ]
             )
