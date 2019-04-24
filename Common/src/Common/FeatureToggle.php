bc5ec034cf709901d4feadddab50550659cb7492
Jonathan Thomas <jonathan@opalise.co.uk>
2019-04-24 08:59:28 +0100

    OLCS-23392 review permit feature toggle usage

--- Common/src/Common/FeatureToggle.php
+++ Common/src/Common/FeatureToggle.php
@@ -8,11 +8,8 @@ namespace Common;
 class FeatureToggle
 {
     const ADMIN_PERMITS = 'admin_permits';
-    const INTERNAL_ECMT = 'internal_ecmt';
     const INTERNAL_PERMITS = 'internal_permits';
-    const SELFSERVE_ECMT = 'ss_ecmt';
     const SELFSERVE_PERMITS = 'ss_permits';
-    const BACKEND_ECMT = 'back_ecmt';
     const BACKEND_PERMITS = 'back_permits';
 
     const INTERNAL_SURRENDER = 'internal_surrender';
