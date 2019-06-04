66eda1a89b962603cbabc2dd0937c3e6f870da28
James Hodgson <james.hodgson@dvsa.gov.uk>
2019-06-04 13:53:32 +0100

    Add formatter for internal licence link

---
+++ Common/src/Common/Service/Table/Formatter/InternalLicenceNumberLink.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Common\Service\Table\Formatter;
+
+/**
+ * Class LicenceNumberLink
+ *
+ * Takes a licence array and creates and outputs a link for that licence.
+ *
+ * @package Common\Service\Table\Formatter
+ */
+class InternalLicenceNumberLink implements FormatterInterface
+{
+    /**
+     * Return a the licence URL in a link format for a table.
+     *
+     * @param array $data The row data.
+     * @param array $column The column
+     * @param null $sm The service manager
+     *
+     * @return string
+     */
+    public static function format($data, $column = array(), $sm = null)
+    {
+        $urlHelper = $sm->get('Helper\Url');
+        $url = $urlHelper->fromRoute('lva-licence', array('licence' => $data['licence']['id']));
+
+        return '<a href="' . $url . '">' . $data['licence']['licNo'] . '</a>';
+    }
+}
