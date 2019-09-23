2b29f040f00cdb2534957300cdef953502dc9bda
Andy Newton <andy@vitri.ltd>
2019-09-23 14:44:19 +0100

    OLCS-25588 - admin stock add/edit changes

---
+++ Common/src/Common/Service/Data/ApplicationPathGroup.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Common\Service\Data;
+
+use Common\Service\Data\Interfaces\ListData;
+use Common\Service\Entity\Exceptions\UnexpectedResponseException;
+use Dvsa\Olcs\Transfer\Query\IrhpApplication\ApplicationPathGroupList;
+
+/**
+ * Class ApplicationPathGroup
+ *
+ *
+ * @package Common\Service\Data
+ */
+class ApplicationPathGroup extends AbstractDataService implements ListData
+{
+    /**
+     * Format data
+     *
+     * @param array $data Data
+     *
+     * @return array
+     */
+    public function formatData(array $data)
+    {
+        $optionData = [];
+        foreach ($data as $datum) {
+            $optionData[$datum['id']] = $datum['name'];
+        }
+
+        return $optionData;
+    }
+
+    /**
+     * Fetch list options
+     *
+     * @param string $category Category
+     * @param bool $useGroups Use groups
+     *
+     * @return array
+     * @throws UnexpectedResponseException
+     */
+    public function fetchListOptions($category, $useGroups = false)
+    {
+        $data = $this->fetchListData();
+
+        if (!$data) {
+            return [];
+        }
+
+        return $this->formatData($data);
+    }
+
+    /**
+     * Fetch list data
+     *
+     * @return array
+     * @throws UnexpectedResponseException
+     */
+    public function fetchListData()
+    {
+        if (is_null($this->getData('ApplicationPathGroup'))) {
+            $dtoData = ApplicationPathGroupList::create([]);
+            $response = $this->handleQuery($dtoData);
+
+            if (!$response->isOk()) {
+                throw new UnexpectedResponseException('unknown-error');
+            }
+
+            $this->setData('ApplicationPathGroup', false);
+            if (isset($response->getResult()['results'])) {
+                $this->setData('ApplicationPathGroup', $response->getResult()['results']);
+            }
+        }
+
+        return $this->getData('ApplicationPathGroup');
+    }
+}
---
+++ test/Common/src/Common/Service/Data/ApplicationPathGroupTest.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace CommonTest\Service\Data;
+
+use Common\Service\Data\ApplicationPathGroup;
+use Mockery as m;
+use Common\Service\Entity\Exceptions\UnexpectedResponseException;
+
+/**
+ * Class Application Path Group Test
+ * @package CommonTest\Service
+ */
+class ApplicationPathGroupTest extends AbstractDataServiceTestCase
+{
+    public function testFormatData()
+    {
+        $source = $this->getSingleSource();
+        $expected = $this->getSingleExpected();
+
+        $sut = new ApplicationPathGroup();
+
+        $this->assertEquals($expected, $sut->formatData($source));
+    }
+
+    /**
+     * @dataProvider provideFetchListOptions
+     * @param $input
+     * @param $expected
+     */
+    public function testFetchListOptions($input, $expected)
+    {
+        $sut = new ApplicationPathGroup();
+        $sut->setData('ApplicationPathGroup', $input);
+
+        $this->assertEquals($expected, $sut->fetchListOptions(''));
+    }
+
+    public function provideFetchListOptions()
+    {
+        return [
+            [$this->getSingleSource(), $this->getSingleExpected()],
+            [false, []]
+        ];
+    }
+
+    public function testFetchListData()
+    {
+        $results = ['results' => 'results'];
+        $mockTransferAnnotationBuilder = m::mock()
+            ->shouldReceive('createQuery')->once()
+            ->andReturn('query')
+            ->once()
+            ->getMock();
+
+        $mockResponse = m::mock()
+            ->shouldReceive('isOk')
+            ->andReturn(true)
+            ->once()
+            ->shouldReceive('getResult')
+            ->andReturn($results)
+            ->twice()
+            ->getMock();
+
+        $sut = new ApplicationPathGroup();
+        $this->mockHandleQuery($sut, $mockTransferAnnotationBuilder, $mockResponse);
+
+        $this->assertEquals($results['results'], $sut->fetchListData([]));
+    }
+
+    public function testFetchListDataWithException()
+    {
+        $this->expectException(UnexpectedResponseException::class);
+        $mockTransferAnnotationBuilder = m::mock()
+            ->shouldReceive('createQuery')->once()->andReturn('query')->getMock();
+
+        $mockResponse = m::mock()
+            ->shouldReceive('isOk')
+            ->andReturn(false)
+            ->once()
+            ->getMock();
+        $sut = new ApplicationPathGroup();
+        $this->mockHandleQuery($sut, $mockTransferAnnotationBuilder, $mockResponse);
+
+        $sut->fetchListData([]);
+    }
+
+    /**
+     * @return array
+     */
+    protected function getSingleExpected()
+    {
+        $expected = [
+            '1' => 'APG1',
+            '2' => 'APG2',
+            '3' => 'APG3',
+        ];
+        return $expected;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getSingleSource()
+    {
+        $source = [
+            ['id' => '1', 'name' => 'APG1'],
+            ['id' => '2', 'name' => 'APG2'],
+            ['id' => '3', 'name' => 'APG3'],
+        ];
+        return $source;
+    }
+}
