0c2ec81b30c7224692a8e2db765b2b09850d46fa
Shaun Hare <shaun.hare@dvsa.gov.uk>
2019-11-06 17:09:27 +0000

    added unsatisfactory fix into publication link

--- Common/src/Common/Service/Table/Formatter/PublicationNumber.php
+++ Common/src/Common/Service/Table/Formatter/PublicationNumber.php
@@ -37,7 +37,10 @@ class PublicationNumber implements FormatterInterface
         $link = sprintf($linkPattern, Escape::html($url), Escape::html($data['publicationNo']));
 
         if ($data['pubStatus']['id'] === 'pub_s_generated') {
-            $uriPattern = $sm->get('Config')['document_share']['uri_pattern'];
+            $osType = $data['document']['osType'];
+
+            $uriPattern = $sm->get('Config')[$osType.'_document_share']['uri_pattern'];
+
             $url = sprintf($uriPattern, $data['document']['identifier']);
             $linkPattern = '<a href="%s" data-file-url="%s" target="blank">%s</a>';
             $link = sprintf($linkPattern, Escape::html($url), Escape::html($url), Escape::html($data['publicationNo']));
