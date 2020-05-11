5806a1b42028a2c8790c239130e5ac4d6ab80a24
James Wragg <james.wragg@dvsa.gov.uk>
2020-05-11 15:36:16 +0100

    VOL-620: Store user last login timestamp

---
+++ Common/src/Common/Service/User/LastLoginService.php
@@ -0,0 +1,38 @@
+<?php
+namespace Common\Service\User;
+
+use Common\Service\Cqrs\Command\CommandSender;
+use Common\Service\Cqrs\Exception;
+use Dvsa\Olcs\Transfer\Command\User\UpdateUserLastLoginAt;
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
+use Zend\ServiceManager\ServiceLocatorAwareTrait;
+
+final class LastLoginService implements ServiceLocatorAwareInterface
+{
+    use ServiceLocatorAwareTrait;
+
+    /**
+     * @var CommandSender
+     */
+    private $commandSender;
+
+    public function __construct(CommandSender $commandSender)
+    {
+        $this->commandSender = $commandSender;
+    }
+
+    public function updateLastLogin($userName)
+    {
+        $command = UpdateUserLastLoginAt::create([
+            'id' => $userName
+        ]);
+
+        $response = $this->commandSender->send($command);
+
+        if (!$response->isOk()) {
+            //TODO: Replace with exception!
+            var_dump($response);
+            die($response);
+        }
+    }
+}
