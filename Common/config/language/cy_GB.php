dcb16777e412e9e2eb7d9600742e07df3d1d0de1
John Spellman <john.spellman@otc.gov.uk>
2020-08-03 15:03:59 +0100

    OLCS-27537_Select_permit_type_page_reworded_permit_descriptions_text

--- Common/config/language/cy_GB.php
+++ Common/config/language/cy_GB.php
@@ -2043,12 +2043,32 @@ return [
     'cookie-settings-saved-content' => 'Gall gwasanaethau\'r Llywodraeth osod cwcis ychwanegol ac, os felly, bydd ganddynt eu polisi cwcis a\'u baner eu hunain.',
 
     'selfserve-app-overview-status-cant-start' => 'Methu cychwyn eto',
+    
+    // Annual Bilateral description
 
-    'This permit can be used for each country you intend to travel to or transit through. You\'ll need a separate permit for each country.' => 'Gellir defnyddio\'r drwydded hon ar gyfer pob gwlad yr ydych yn bwriadu teithio iddi neu ei chludo drwyddi. Bydd arnoch angen trwydded ar wahân ar gyfer pob gwlad.',
+    'These permits are for travelling to or transiting through individual countries. A separate permit is needed for each country you intend to travel to or transit through. Each country will have a specific validity period for these permits.' => 'Mae\'r trwyddedau hyn ar gyfer teithio neu deithio drwy wledydd unigol. Mae angen trwydded ar wahân ar gyfer pob gwlad yr ydych yn bwriadu teithio iddi neu ei chludo drwyddi. Bydd gan bob gwlad gyfnod dilysrwydd penodol ar gyfer y trwyddedau hyn.',
+    
+    // Annual multilateral description
+
     'This is an annual permit that allows unlimited journeys to all EU and EEA countries. You will need one permit for each vehicle you use. This permit type can be transferred between vehicles.' => 'Mae hon yn hawlen flynyddol sy\'n caniatáu teithiau diderfyn i holl wledydd yr UE a\'r EEA. Bydd arnoch angen un hawlen ar gyfer pob cerbyd a ddefnyddiwch. Gellir trosglwyddo\'r math hwn o hawlen rhwng cerbydau.',
-    'This permit can be used for international road haulage journeys between ECMT member countries.' => 'Gellir defnyddio\'r hawlen hon ar gyfer teithiau cludo nwyddau rhyngwladol rhwng gwledydd sy\'n aelodau o\'r ECMT.',
-    'This permit can only be used to transit through EU countries to non-EU countries who are ECMT members, including Switzerland, or cross-trade between two EU countries. It is valid for 30 consecutive days from the start date you choose. Short-term permits are not valid for Austria.' => 'Gellir defnyddio\'r hawlen hon i symud trwy wledydd nad ydynt yn rhan o\'r UE sy\'n aelodau ECMT, gan gynnwys y Swistir, neu i draws-fasnachu rhwng dwy wlad yn yr UE. Mae\'n ddilys am 30 diwrnod yn olynol o\'r dyddiad dechrau a ddewiswch. Nid yw trwyddedau tymor byr yn ddilys ar gyfer Awstria.',
-    'This permit is for specialist removal companies to move household goods and business possessions between ECMT member countries except Cyprus. It is valid for one year from the date of issue.' => 'Mae\'r hawlen hon ar gyfer cwmnïau symud dodrefn arbenigol i symud nwyddau cartref ac eiddo busnes rhwng gwledydd sy\'n aelodau o\'r ECMT ac eithrio Cyprus. Mae\'n ddilys am flwyddyn o\'r dyddiad cyhoeddi.',
-    'This certificate is required for vehicles transporting goods using any type of ECMT permit. You will need to apply separately for each vehicle you intend to use.' => 'Mae angen y dystysgrif hon ar gyfer cerbydau sy\'n cludo nwyddau gan ddefnyddio unrhyw fath o drwydded ECMT. Bydd angen i chi wneud cais ar wahân am bob cerbyd rydych yn bwriadu ei ddefnyddio.',
-    'This certificate is required for trailers transporting goods using any type of ECMT permit. You will need to apply separately for each trailer you intend to use.' => 'Mae angen y dystysgrif hon ar gyfer trelars sy\'n cludo nwyddau gan ddefnyddio unrhyw fath o drwydded ECMT. Bydd angen i chi wneud cais ar wahân am bob trelar rydych chi\'n bwriadu eu defnyddio.',
+    
+    // ECMT Annual description
+ 
+    'These permits are valid for one calendar year. They are for transiting through EU or EEA countries to non-EU countries who are ECMT members (including Switzerland), or for cross-trade between two EU or EEA countries.' => 'Mae\'r trwyddedau hyn yn ddilys am un flwyddyn galendr. Maent ar gyfer trosglwyddo drwy wledydd yr UE neu\'r AEE i wledydd y tu allan i\'r UE sy\'n aelodau ECMT (gan gynnwys y Swistir), neu ar gyfer traws-fasnach rhwng dwy o wledydd yr UE neu AEE.',
+    
+    // ECMT short term description
+
+    'These permits are valid for 30 consecutive days from the start date you choose. They are for transiting through EU or EEA countries to non-EU countries who are ECMT members (including Switzerland), or for cross-trade between two EU or EEA countries. Short-term permits are not valid for Austria.' => 'Mae\'r trwyddedau hyn yn ddilys am 30 diwrnod yn olynol o\'r dyddiad cychwyn a ddewiswch. Maent ar gyfer trosglwyddo drwy wledydd yr UE neu\'r AEE i wledydd y tu allan i\'r UE sy\'n aelodau ECMT (gan gynnwys y Swistir), neu ar gyfer traws-fasnach rhwng dwy o wledydd yr UE neu AEE. Nid yw trwyddedau tymor byr yn ddilys ar gyfer Awstria.',
+    
+    // ECMT removal description
+
+    'Specialist removal companies can use these to move household goods and business possessions between ECMT member countries, except Cyprus. They require the use of the specialised equipment and staff. They are valid for one year from the date of issue.' => 'Gall cwmnïau symud arbenigol ddefnyddio\'r rhain i symud nwyddau cartref ac eiddo busnes rhwng aelod-wledydd ECMT, ac eithrio Cyprus. Maent yn gofyn am ddefnyddio\'r offer a\'r staff arbenigol. Maent yn ddilys am flwyddyn o\'r dyddiad cyhoeddi.',
+    
+    // Cert roadworthiness vehicle description
+
+    'Vehicles transporting goods using an ECMT or removals permit need these certificates. You will need to apply separately for each vehicle you intend to use. They are valid until the end of the vehicle’s MOT.' => 'Mae angen y tystysgrifau hyn ar gerbydau sy\'n cludo nwyddau gan ddefnyddio ECMT neu hawlen symud. Bydd angen i chi wneud cais ar wahân am bob cerbyd rydych chi\'n bwriadu ei ddefnyddio. Maent yn ddilys tan ddiwedd MOT y cerbyd.',
+    
+    // Cert roadworthiness trailer description
+
+    'Trailers transporting goods using any type of ECMT permit need these certificates. You will need to apply separately for each trailer you intend to use. They are valid until the end of the vehicle’s MOT.' => 'Mae angen y tystysgrifau hyn ar ôl-gerbydau sy\'n cludo nwyddau gan ddefnyddio unrhyw fath o drwydded ECMT. Bydd angen i chi wneud cais ar wahân am bob trelar rydych chi\'n bwriadu ei ddefnyddio. Maent yn ddilys tan ddiwedd MOT y cerbyd.',
 ];
