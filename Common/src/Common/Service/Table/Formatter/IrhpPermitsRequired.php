70d792407614a01b80c9a00edd51ebb3f1045adc
Ian Lindsay <ian@hemera-business-services.co.uk>
2020-08-05 12:34:35 +0100

    olcs-27422 permits required field always shows 1 for certificates

---
+++ Common/src/Common/Service/Table/Formatter/IrhpPermitsRequired.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Common\Service\Table\Formatter;
+
+use Common\RefData;
+use Common\Util\Escape;
+
+/**
+ * IRHP Permit Type formatter
+ */
+class IrhpPermitsRequired implements FormatterInterface
+{
+    const NAME = 'IrhpPermitsRequired';
+
+    /**
+     * Format
+     *
+     * Returns the number of IRHP permits required
+     *
+     * @param array $data
+     *
+     * @return string
+     */
+    public static function format($data)
+    {
+        $value = $data['permitsRequired'];
+
+        $certificateTypes = [
+            RefData::CERT_ROADWORTHINESS_TRAILER_PERMIT_TYPE_ID,
+            RefData::CERT_ROADWORTHINESS_VEHICLE_PERMIT_TYPE_ID,
+        ];
+
+        if (in_array($data['typeId'], $certificateTypes)) {
+            $value = 1;
+        }
+
+        return Escape::html($value);
+    }
+}
