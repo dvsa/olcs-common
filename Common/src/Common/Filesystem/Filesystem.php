774a6473440cd34cca5a297e9b82c30b0c3d22e6
James Hodgson <james.hodgson@dvsa.gov.uk>
2020-08-14 08:34:41 +0000

    VOL-748: Bump symfony versions to remove CVE

--- Common/src/Common/Filesystem/Filesystem.php
+++ Common/src/Common/Filesystem/Filesystem.php
@@ -3,7 +3,12 @@
 namespace Common\Filesystem;
 
 use Symfony\Component\Filesystem\Filesystem as BaseFileSystem;
-use Symfony\Component\Filesystem\LockHandler;
+use Symfony\Component\Lock\Exception\LockAcquiringException;
+use Symfony\Component\Lock\Exception\LockConflictedException;
+use Symfony\Component\Lock\LockFactory;
+use Symfony\Component\Lock\LockInterface;
+use Symfony\Component\Lock\Store\FlockStore;
+
 
 /**
  * Class Filesystem
@@ -11,15 +16,17 @@ use Symfony\Component\Filesystem\LockHandler;
  */
 class Filesystem extends BaseFileSystem
 {
+    const MAX_LOCK_ATTEMPTS = 3;
+
     /**
-     * @param $path
+     * @param string $path
      * @param string $prefix
      * @return string
      */
-    public function createTmpDir($path, $prefix = '')
+    public function createTmpDir(string $path, string $prefix = ''): string
     {
-        $lock = new LockHandler(hash('sha256', $path));
-        $lock->lock(true);
+        $lock = $this->getLock($path);
+        $this->acquireLock($lock);
 
         do {
             $dirname = $path . DIRECTORY_SEPARATOR . uniqid($prefix);
@@ -33,14 +40,14 @@ class Filesystem extends BaseFileSystem
     }
 
     /**
-     * @param $path
+     * @param string $path
      * @param string $prefix
      * @return string
      */
-    public function createTmpFile($path, $prefix = '')
+    public function createTmpFile(string $path, string $prefix = ''): string
     {
-        $lock = new LockHandler(hash('sha256', $path));
-        $lock->lock(true);
+        $lock = $this->getLock($path);
+        $this->acquireLock($lock);
 
         do {
             $filename = $path . DIRECTORY_SEPARATOR . uniqid($prefix);
@@ -51,5 +58,39 @@ class Filesystem extends BaseFileSystem
         $lock->release();
 
         return $filename;
+    }
+
+
+    /**
+     * @param LockInterface $lock
+     * @param bool $blocking
+     * @param int $maxAttempts
+     * @throws LockConflictedException | LockAcquiringException
+     */
+    private function acquireLock(LockInterface $lock, bool $blocking = true, int $maxAttempts = self::MAX_LOCK_ATTEMPTS): void
+    {
+        for ($currentAttempt = 1; $currentAttempt <= $maxAttempts; $currentAttempt++)
+        {
+            try {
+                $lock->acquire($blocking);
+                break;
+            } catch (LockConflictedException | LockAcquiringException $exception) {
+                if ($currentAttempt >= $maxAttempts) {
+                    throw $exception;
+                }
+                usleep(500);
+            }
+        }
+    }
+
+    /**
+     * @param string $path
+     * @return LockInterface
+     */
+    protected function getLock(string $path): LockInterface
+    {
+        $store = new FlockStore();
+        $factory = new LockFactory($store);
+        return $factory->createLock($path);
     }
 }
