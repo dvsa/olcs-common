d92d47e0d10870294500e80e5e2615b3c049dffc
Marcus Turner <marcus.turner@dvsa.gov.uk>
2021-08-19 09:08:37 +0100

    Housekeeping

--- Common/src/Common/Test/MocksServicesTrait.php
+++ Common/src/Common/Test/MocksServicesTrait.php
@@ -3,7 +3,7 @@
 namespace Common\Test;
 
 /**
- * @deprecated Please use \Olcs\TestHelpers\MockeryTestCase - this is a better place for these globally shared test classes
+ * @deprecated Please use \Olcs\TestHelpers\Service\MocksServicesTrait - this is a better place for these globally shared test classes
  */
 trait MocksServicesTrait
 {
