0b122a815c7eaa0e1294e814bef837aa20f67ac6
Marcus Turner <marcus.turner@dvsa.gov.uk>
2021-08-19 08:56:39 +0100

    Deprecate old test classes in favour of new ones in testhelpers

--- Common/src/Common/Test/MocksServicesTrait.php
+++ Common/src/Common/Test/MocksServicesTrait.php
@@ -2,99 +2,10 @@
 
 namespace Common\Test;
 
-use Common\Test\Builder\ServiceManagerBuilder;
-use Laminas\ServiceManager\AbstractPluginManager;
-use Laminas\ServiceManager\ServiceLocatorInterface;
-use Laminas\ServiceManager\ServiceManager;
-use Mockery\MockInterface;
-use Mockery as m;
-
+/**
+ * @deprecated Please use \Olcs\TestHelpers\MockeryTestCase - this is a better place for these globally shared test classes
+ */
 trait MocksServicesTrait
 {
-    /**
-     * @var ServiceManager
-     */
-    private $serviceManager;
-
-    /**
-     * @return ServiceManager
-     */
-    protected function serviceManager(): ServiceManager
-    {
-        assert(null !== $this->serviceManager, 'Expected service manager to be set. Hint: You may need to call `setUpServiceManager` before trying to get a service manager');
-        return $this->serviceManager;
-    }
-
-    /**
-     * @return ServiceManager
-     */
-    protected function setUpServiceManager(): ServiceManager
-    {
-        $this->serviceManager = new ServiceManager();
-        $this->serviceManager->setAllowOverride(true);
-        $services = $this->setUpDefaultServices($this->serviceManager);
-
-        // Maintain support for deprecated way of registering services via an array of services. Instead, services
-        // should be registered by calling the available setter methods on the ServiceManager instance.
-        if (is_array($services)) {
-            foreach ($services as $serviceName => $service) {
-                $this->serviceManager->setService($serviceName, $service);
-            }
-        }
-
-        // Set controller plugin manager to the main service manager so that all services can be resolved from the one
-        // service manager instance.
-        $this->serviceManager->setService('ControllerPluginManager', $this->serviceManager);
-
-        return $this->serviceManager;
-    }
-
-    /**
-     * @return ServiceManager
-     * @deprecated Please use MocksServicesTrait::setUpServiceManager instead.
-     */
-    protected function setUpServiceLocator(): ServiceManager
-    {
-        return $this->setUpServiceManager();
-    }
-
-    /**
-     * @param ServiceLocatorInterface $serviceLocator
-     * @return MockInterface|AbstractPluginManager
-     */
-    protected function setUpAbstractPluginManager(ServiceLocatorInterface $serviceLocator): MockInterface
-    {
-        $instance = m::mock(AbstractPluginManager::class);
-        $instance->shouldReceive('getServiceLocator')->andReturn($serviceLocator)->byDefault();
-        return $instance;
-    }
-
-    /**
-     * @param string $class
-     * @return MockInterface
-     */
-    protected function setUpMockService(string $class): MockInterface
-    {
-        $instance = m::mock($class);
-        $instance->shouldIgnoreMissing();
-        return $instance;
-    }
-
-    /**
-     * Sets up default services.
-     *
-     * @param ServiceManager $serviceManager
-     */
-    abstract protected function setUpDefaultServices(ServiceManager $serviceManager);
-
-    /**
-     * @param ServiceLocatorInterface $serviceLocator
-     * @param string $name
-     * @return MockInterface
-     * @deprecated Use $this->serviceManager()->get($name) instead
-     */
-    protected function resolveMockService(ServiceLocatorInterface $serviceLocator, string $name): MockInterface
-    {
-        return $serviceLocator->get($name);
-    }
+    use \Olcs\TestHelpers\Service\MocksServicesTrait;
 }
