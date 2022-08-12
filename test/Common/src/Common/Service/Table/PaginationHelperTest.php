23a0546b4fa4afdd3fd0098e557e82120ead5c23
Ian Lindsay <ian@hemera-business-services.co.uk>
2022-08-13 00:40:07 +0100

    VOL-3302 VOL-3204 amend templates for new GDS pagination styles

--- test/Common/src/Common/Service/Table/PaginationHelperTest.php
+++ test/Common/src/Common/Service/Table/PaginationHelperTest.php
@@ -28,15 +28,7 @@ class PaginationHelperTest extends MockeryTestCase
             $paginationHelper->setTranslator($mockTranslator);
         }
 
-        $options = $paginationHelper->getOptions();
-
-        $labels = array();
-
-        foreach ($options as $option) {
-            $labels[] = $option['label'];
-        }
-
-        $this->assertEquals($expected, $labels);
+        $this->assertEquals($expected, $paginationHelper->getOptions());
     }
 
     /**
@@ -52,35 +44,240 @@ class PaginationHelperTest extends MockeryTestCase
                 'total' => 10,
                 'limit' => 10,
                 'isSetTranslator' => false,
-                'expect' => ['1'],
+                'expect' => [
+                    'previous' => [],
+                    'links' => [
+                        0 => [
+                            'page' => 1,
+                            'label' => 1,
+                            'class' => PaginationHelper::CLASS_PAGINATION_ITEM_CURRENT,
+                            'ariaCurrent' => 'aria-current="page"',
+                        ],
+                    ],
+                    'next' => [],
+                ],
             ],
             [
                 'page' => 1,
                 'total' => 50,
                 'limit' => 10,
                 'isSetTranslator' => true,
-                'expect' => ['1', '2', '3', '4', '5', 'TRNSLT_NEXT'],
+                'expect' => [
+                    'previous' => [],
+                    'links' => [
+                        0 => [
+                            'page' => 1,
+                            'label' => 1,
+                            'class' => PaginationHelper::CLASS_PAGINATION_ITEM_CURRENT,
+                            'ariaCurrent' => 'aria-current="page"',
+                        ],
+                        1 => [
+                            'page' => 2,
+                            'label' => 2,
+                            'class' => '',
+                            'ariaCurrent' => '',
+                        ],
+                        2 => [
+                            'page' => 3,
+                            'label' => 3,
+                            'class' => '',
+                            'ariaCurrent' => '',
+                        ],
+                        3 => [
+                            'page' => 4,
+                            'label' => 4,
+                            'class' => '',
+                            'ariaCurrent' => '',
+                        ],
+                        4 => [
+                            'page' => 5,
+                            'label' => 5,
+                            'class' => '',
+                            'ariaCurrent' => '',
+                        ],
+                    ],
+                    'next' => [
+                        'label' => 'TRNSLT_NEXT',
+                        'page' => 2,
+                    ],
+                ],
             ],
             [
                 'page' => 2,
                 'total' => 50,
                 'limit' => 10,
                 'isSetTranslator' => true,
-                'expect' => ['TRNSLT_PREV', '1', '2', '3', '4', '5', 'TRNSLT_NEXT'],
+                'expect' => [
+                    'previous' => [
+                        'label' => 'TRNSLT_PREV',
+                        'page' => 1,
+                    ],
+                    'links' => [
+                        0 => [
+                            'page' => 1,
+                            'label' => 1,
+                            'class' => '',
+                            'ariaCurrent' => '',
+                        ],
+                        1 => [
+                            'page' => 2,
+                            'label' => 2,
+                            'class' => PaginationHelper::CLASS_PAGINATION_ITEM_CURRENT,
+                            'ariaCurrent' => 'aria-current="page"',
+                        ],
+                        2 => [
+                            'page' => 3,
+                            'label' => 3,
+                            'class' => '',
+                            'ariaCurrent' => '',
+                        ],
+                        3 => [
+                            'page' => 4,
+                            'label' => 4,
+                            'class' => '',
+                            'ariaCurrent' => '',
+                        ],
+                        4 => [
+                            'page' => 5,
+                            'label' => 5,
+                            'class' => '',
+                            'ariaCurrent' => '',
+                        ],
+                    ],
+                    'next' => [
+                        'label' => 'TRNSLT_NEXT',
+                        'page' => 3,
+                    ],
+                ],
             ],
             [
                 'page' => 20,
                 'total' => 1000,
                 'limit' => 10,
                 'isSetTranslator' => false,
-                'expect' => ['Previous', '1', '...', '18', '19', '20', '21', '22', '...', '100', 'Next'],
+                'expect' => [
+                    'previous' => [
+                        'label' => 'Previous',
+                        'page' => 19,
+                    ],
+                    'links' => [
+                        0 => [
+                            'page' => 1,
+                            'label' => 1,
+                            'class' => '',
+                            'ariaCurrent' => '',
+                        ],
+                        1 => [
+                            'page' => null,
+                            'label' => PaginationHelper::ELLIPSE,
+                            'class' => '',
+                            'ariaCurrent' => '',
+                        ],
+                        2 => [
+                            'page' => 18,
+                            'label' => 18,
+                            'class' => '',
+                            'ariaCurrent' => '',
+                        ],
+                        3 => [
+                            'page' => 19,
+                            'label' => 19,
+                            'class' => '',
+                            'ariaCurrent' => '',
+                        ],
+                        4 => [
+                            'page' => 20,
+                            'label' => 20,
+                            'class' => PaginationHelper::CLASS_PAGINATION_ITEM_CURRENT,
+                            'ariaCurrent' => 'aria-current="page"',
+                        ],
+                        5 => [
+                            'page' => 21,
+                            'label' => 21,
+                            'class' => '',
+                            'ariaCurrent' => '',
+                        ],
+                        6 => [
+                            'page' => 22,
+                            'label' => 22,
+                            'class' => '',
+                            'ariaCurrent' => '',
+                        ],
+                        7 => [
+                            'page' => null,
+                            'label' => PaginationHelper::ELLIPSE,
+                            'class' => '',
+                            'ariaCurrent' => '',
+                        ],
+                        8 => [
+                            'page' => 100,
+                            'label' => 100,
+                            'class' => '',
+                            'ariaCurrent' => '',
+                        ],
+                    ],
+                    'next' => [
+                        'label' => 'Next',
+                        'page' => 21,
+                    ],
+                ],
             ],
             [
                 'page' => 100,
                 'total' => 1000,
                 'limit' => 10,
                 'isSetTranslator' => true,
-                'expect' => ['TRNSLT_PREV', '1', '...', '96', '97', '98', '99', '100'],
+                'expect' => [
+                    'previous' => [
+                        'label' => 'TRNSLT_PREV',
+                        'page' => 99,
+                    ],
+                    'links' => [
+                        0 => [
+                            'page' => 1,
+                            'label' => 1,
+                            'class' => '',
+                            'ariaCurrent' => '',
+                        ],
+                        1 => [
+                            'page' => null,
+                            'label' => PaginationHelper::ELLIPSE,
+                            'class' => '',
+                            'ariaCurrent' => '',
+                        ],
+                        2 => [
+                            'page' => 96,
+                            'label' => 96,
+                            'class' => '',
+                            'ariaCurrent' => '',
+                        ],
+                        3 => [
+                            'page' => 97,
+                            'label' => 97,
+                            'class' => '',
+                            'ariaCurrent' => '',
+                        ],
+                        4 => [
+                            'page' => 98,
+                            'label' => 98,
+                            'class' => '',
+                            'ariaCurrent' => '',
+                        ],
+                        5 => [
+                            'page' => 99,
+                            'label' => 99,
+                            'class' => '',
+                            'ariaCurrent' => '',
+                        ],
+                        6 => [
+                            'page' => 100,
+                            'label' => 100,
+                            'class' => PaginationHelper::CLASS_PAGINATION_ITEM_CURRENT,
+                            'ariaCurrent' => 'aria-current="page"',
+                        ],
+                    ],
+                    'next' => [],
+                ],
             ],
         ];
     }
