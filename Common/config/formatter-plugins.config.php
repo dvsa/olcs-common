f7cdb0083b8b3d272a9a02495384cdbb38e6a702
Jamie Gordon <jamie.gordon@dvsa.gov.uk>
2023-11-30 11:57:35 +0000

    Feat: VOL-4336 - Table formatter for conversation list

--- Common/config/formatter-plugins.config.php
+++ Common/config/formatter-plugins.config.php
@@ -54,6 +54,8 @@ use Common\Service\Table\Formatter\HideIfClosedRadio;
 use Common\Service\Table\Formatter\InspectionRequestId;
 use Common\Service\Table\Formatter\InterimOcCheckbox;
 use Common\Service\Table\Formatter\InterimVehiclesCheckbox;
+use Common\Service\Table\Formatter\InternalLicenceConversationFactory;
+use Common\Service\Table\Formatter\InternalLicenceConversationLink;
 use Common\Service\Table\Formatter\InternalLicenceNumberLink;
 use Common\Service\Table\Formatter\InternalLicencePermitReference;
 use Common\Service\Table\Formatter\IrhpPermitApplicationRefLink;
@@ -335,6 +337,7 @@ return [
         InspectionRequestId::class => InspectionRequestIdFactory::class,
         InternalLicenceNumberLink::class => InternalLicenceNumberLinkFactory::class,
         InternalLicencePermitReference::class => InternalLicencePermitReferenceFactory::class,
+        InternalLicenceConversationLink::class => InternalLicenceConversationFactory::class,
         IrhpPermitApplicationRefLink::class => IrhpPermitApplicationRefLinkFactory::class,
         IrhpPermitNumberInternal::class => IrhpPermitNumberInternalFactory::class,
         IrhpPermitRangePermitNumber::class => IrhpPermitRangePermitNumberFactory::class,
