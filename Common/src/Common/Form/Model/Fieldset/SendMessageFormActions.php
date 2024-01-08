1590e18a4873265aeadad75b69747f5ee78f15b5
Saul Wilcox <saul.wilcox@dvsa.gov.uk>
2024-01-08 11:25:01 +0000

    Create fieldset

---
+++ Common/src/Common/Form/Model/Fieldset/SendMessageFormActions.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Common\Form\Model\Fieldset;
+
+use Laminas\Form\Annotation as Form;
+
+/**
+ * @codeCoverageIgnore Auto-generated file with no methods
+ * @Form\Name("form-actions")
+ * @Form\Attributes({"class":"govuk-button-group"})
+ */
+class SendMessageFormActions
+{
+    /**
+     * @Form\Attributes({
+     *     "data-module": "govuk-button",
+     *     "type": "submit",
+     *     "class": "govuk-button"
+     * })
+     * @Form\Options({"label": "Send Message"})
+     * @Form\Type("\Common\Form\Elements\InputFilters\ActionButton")
+     */
+    public $submit = null;
+}
