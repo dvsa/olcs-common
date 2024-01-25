27558a0d2b7cf49bf5a6061f15a1ac4662a8fa31
JoshuaLicense <JoshuaLicense@users.noreply.github.com>
2024-01-25 15:05:16 +0000

    fix: fix type error in `AbstractInputSearch` when `this->messages` is empty (#20)
    * fix: fix type error in `AbstractInputSearch` when `this->messages` is empty
    
    * !fixup

--- Common/src/Common/Form/Elements/Types/AbstractInputSearch.php
+++ Common/src/Common/Form/Elements/Types/AbstractInputSearch.php
@@ -28,7 +28,7 @@ abstract class AbstractInputSearch extends Fieldset
 
     public function getMessages(?string $elementName = null): array
     {
-        return is_array($this->messages) ? current($this->messages) : [];
+        return current($this->messages) ?: [];
     }
 
     abstract protected function addHint();
