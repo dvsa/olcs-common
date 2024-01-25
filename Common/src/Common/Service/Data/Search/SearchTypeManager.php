de853cd8f77acfb37b0545ef90fd90188c8f9081
Ian Lindsay <ian@hemera-business-services.co.uk>
2024-01-25 09:42:10 +0000

    update getRegisteredServices to work off service manager factory list

--- Common/src/Common/Service/Data/Search/SearchTypeManager.php
+++ Common/src/Common/Service/Data/Search/SearchTypeManager.php
@@ -4,7 +4,6 @@ namespace Common\Service\Data\Search;
 
 use Common\Data\Object\Search\SearchAbstract;
 use Laminas\ServiceManager\AbstractPluginManager;
-use Psr\Container\ContainerInterface;
 
 /**
  * Plugin manager for search data objects
@@ -24,27 +23,12 @@ class SearchTypeManager extends AbstractPluginManager
      */
     protected $autoAddInvokableClass = false;
 
-    private array $config;
-
-    public function __construct(ContainerInterface $container, $config)
-    {
-        $this->config = $config;
-        parent::__construct($container, $config);
-    }
-
     /**
-     * @todo this isn't great, but provides for backward compatibility with old code as we move to Laminas 3
-     *
-     * the old code (mis)used a debug method within the Laminas service manager to provide this info,
+     * previously we had code that (mis)used a debug method within the Laminas service manager to provide this info,
      * for now we're replicating that behaviour here
      */
     public function getRegisteredServices(): array
     {
-        $factories = $this->config['factories'] ?? [];
-        $invokables = $this->config['invokables'] ?? [];
-
-        return array_keys(
-            array_merge($factories, $invokables)
-        );
+        return array_keys($this->factories);
     }
 }
