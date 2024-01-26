54e314fff15fc338c6cc827c61ba8886acdede5e
JoshuaLicense <JoshuaLicense@users.noreply.github.com>
2024-01-26 16:28:49 +0000

    fix: keep `SearchPostcode` fieldset at the top of the `Address` fieldset

--- Common/src/Common/Form/Model/Fieldset/Address.php
+++ Common/src/Common/Form/Model/Fieldset/Address.php
@@ -32,6 +32,7 @@ class Address
      * })
      * @Form\Required(false)
      * @Form\Type("Common\Form\Elements\Types\PostcodeSearch")
+     * @Form\Flags({"priority": 100})
      */
     public $searchPostcode = null;
 
