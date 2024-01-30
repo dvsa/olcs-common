f8d584e73ca7ead2a1cc767ffeba241e9fc8d65a
JoshuaLicense <JoshuaLicense@users.noreply.github.com>
2024-01-30 09:34:06 +0000

    fix: fix casing of `ViewHelperManager` in call to service manager (#26)

--- Common/src/Common/Service/Table/Formatter/DataRetentionAssignedToFactory.php
+++ Common/src/Common/Service/Table/Formatter/DataRetentionAssignedToFactory.php
@@ -15,7 +15,7 @@ class DataRetentionAssignedToFactory implements FactoryInterface
      */
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
-        $viewHelperManager = $container->get('viewHelperManager');
+        $viewHelperManager = $container->get('ViewHelperManager');
         return new DataRetentionAssignedTo($viewHelperManager);
     }
 }
