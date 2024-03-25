f32a2dbafe1766cf34e44b0de99540ddc784cac8
wadedvsa <155439365+wadedvsa@users.noreply.github.com>
2024-03-25 10:51:10 +0000

    chore: Remove doc blocks

--- Common/src/Common/Data/Object/Search/Aggregations/Terms/DeletedStatus.php
+++ Common/src/Common/Data/Object/Search/Aggregations/Terms/DeletedStatus.php
@@ -4,26 +4,9 @@ declare(strict_types=1);
 
 namespace Common\Data\Object\Search\Aggregations\Terms;
 
-/**
- * Licence Type filter class.
- *
- * @package Common\Data\Object\Search\Filter
- * @author Craig Reasbeck <craig.reasbeck@valtech.co.uk>
- */
 class DeletedStatus extends TermsAbstract
 {
-    /**
-     * The human readable title of this filter. This may also be used in the front-end (not sure yet).
-     *
-     * @var string
-     */
     protected $title = 'search.form.filter.deleted-status';
-
-    /**
-     * The actual name of the field to ask for filter information for.
-     *
-     * @var string
-     */
     protected $key = 'personDeleted';
 
     public function getType(): string
