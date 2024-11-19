81ddfada2c5b2035e1b02aab16e1aa960aa67031
Ian Lindsay <ian@hemera-business-services.co.uk>
2024-11-19 03:35:28 +0000

    feat: VOL-4718 permission service prevents last operator admin being deleted

--- Common/src/Common/Rbac/Service/Permission.php
+++ Common/src/Common/Rbac/Service/Permission.php
@@ -46,8 +46,21 @@ class Permission
         return $this->authService->isGranted(RefData::PERMISSION_CAN_MANAGE_USER_SELFSERVE);
     }
 
-    public function canRemoveSelfserveUser(string $userId): bool
+    /**
+     * $role, is the role of the user being deleted, as opposed to the role of the user doing the deleting
+     */
+    public function canRemoveSelfserveUser(string $userId, string $role): bool
     {
+        /**
+         * we have to check the auth service to see if operator admins can be deleted,
+         * since this needs calculating at an organisation level
+         *
+         * what we're preventing here is the last operator admin being deleted
+         */
+        if ($role === RefData::ROLE_OPERATOR_ADMIN && !$this->authService->getIdentity()->getUserData()['canDeleteOperatorAdmin']) {
+            return false;
+        }
+
         return $this->canManageSelfserveUsers() && !$this->isSelf($userId);
     }
 }
