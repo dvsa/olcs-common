9f069d52f305c9f98bebe421395735e57eda486d
Ian Lindsay <6673081+ilindsay@users.noreply.github.com>
2026-05-14 15:37:01 +0100

    feat: swapped old-logging for laminas-log to monolog VOL-6099 (#293) (#298)

--- Common/src/Module.php
+++ Common/src/Module.php
@@ -114,9 +114,7 @@ class Module implements ConfigProviderInterface, ServiceProviderInterface
 
         $this->setLoggerUser($sm);
 
-        $identifier = $sm->get('LogProcessorManager')
-            ->get(\Olcs\Logging\Log\Processor\RequestId::class)
-            ->getIdentifier();
+        $identifier = $sm->get(\Olcs\Logging\Log\Processor\RequestId::class)->getIdentifier();
 
         $this->onFatalError($identifier);
 
@@ -305,7 +303,7 @@ class Module implements ConfigProviderInterface, ServiceProviderInterface
     private function setLoggerUser(ServiceManager $serviceManager): void
     {
         $authService = $serviceManager->get(\LmcRbacMvc\Service\AuthorizationService::class);
-        $serviceManager->get('LogProcessorManager')->get(\Olcs\Logging\Log\Processor\UserId::class)
+        $serviceManager->get(\Olcs\Logging\Log\Processor\UserId::class)
             ->setUserId($authService->getIdentity()->getUsername());
     }
 
