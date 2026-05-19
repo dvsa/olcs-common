466b30106eb7dd65ed92e75f0a4a3cc9dd4c95ae
Mohammed Ahmed <62504045+waheeduk@users.noreply.github.com>
2026-05-19 17:10:47 +0100

    Update Common/src/Common/Form/Model/Form/Lva/Fieldset/ConvictionsPenaltiesData.php
    Co-authored-by: Ian Lindsay <6673081+ilindsay@users.noreply.github.com>

--- Common/src/Common/Form/Model/Form/Lva/Fieldset/ConvictionsPenaltiesData.php
+++ Common/src/Common/Form/Model/Form/Lva/Fieldset/ConvictionsPenaltiesData.php
@@ -24,7 +24,7 @@ class ConvictionsPenaltiesData
      *     "label": "selfserve-app-subSection-previous-history-criminal-conviction-hasConv-hint",
      *     "legend-attributes": {"class": "form-element__label field"},
      *     "label_attributes": {"class": "form-control form-control--radio form-control--inline"},
-     *     "value_options": {"Y": "Yes", "N": "No"q},
+     *     "value_options": {"Y": "Yes", "N": "No"},
      *     "label_options": {"disable_html_escape": true},
      * })
      * @Form\Type("\Laminas\Form\Element\Radio")
